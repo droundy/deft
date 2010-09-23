1c95eea577578acb3af9e76dcf242f7f227ec87c
David Roundy <roundyd@physics.oregonstate.edu>
2010-09-23 16:30:15 -0400

    test for CPU use as well as memory use in the memory test.
    Tested-on: collins (Linux 2.6.32-5-amd64)

--- tests/memory.cpp
+++ tests/memory.cpp
@@ -21,16 +21,20 @@
 #include "utilities.h"
 
 int retval = 0;
+clock_t last_time = clock();
+double reference_time = 1;
 
-void check_peak(const char *name, double peakmin, double peakmax) {
+double check_peak(const char *name, double peakmin, double peakmax, double time_limit) {
   printf("\n************");
   for (unsigned i=0;i<strlen(name);i++) printf("*");
   printf("\n* Testing %s *\n", name);
   for (unsigned i=0;i<strlen(name);i++) printf("*");
   printf("************\n\n");
   
+  double cputime = (clock()-last_time)/double(CLOCKS_PER_SEC);
   double peak = peak_memory()/1024.0/1024;
   printf("Peak memory use is %g M\n", peak);
+  printf("CPU time is %g s (or %gx)\n", cputime, cputime/reference_time);
   if (peak < peakmin) {
     printf("FAIL: Peak memory use should be at least %g!\n", peakmin);
     retval++;
@@ -39,7 +43,13 @@ void check_peak(const char *name, double peakmin, double peakmax) {
     printf("FAIL: Peak memory use should be under %g!\n", peakmax);
     retval++;
   }
+  if (time_limit > 0 && cputime/reference_time > time_limit) {
+    printf("FAIL: CPU time used should be under %gx!\n", time_limit);
+    retval++;
+  }
   reset_peak_memory();
+  last_time = clock();
+  return cputime;
 }
 
 const double R = 2.7;
@@ -70,11 +80,17 @@ int main(int, char **argv) {
   //Lattice lat(Cartesian(1.4*rmax,0,0), Cartesian(0,1.4*rmax,0), Cartesian(0,0,1.4*rmax));
   GridDescription gd(lat, 0.2);
 
+  last_time = clock();
   Grid external_potential(gd);
+  // Do some pointless stuff so we can get some sort of gauge as to
+  // how fast this CPU is, for comparison with other tests.
+  external_potential.Set(incavity);
+  external_potential = external_potential.cwise().exp();
+  external_potential = 13*external_potential.cwise().log() + 3*external_potential.cwise().square();
   external_potential.Set(incavity);
   external_potential *= 1e9;
-  external_potential.epsNativeSlice("external.eps", Cartesian(2*rmax,0,0), Cartesian(0,2*rmax,0), Cartesian(-rmax,-rmax,0));
-  external_potential.epsRadial1d("external-radial.eps", 0, rmax, 1, R, "Good fun!");
+  reference_time = check_peak("Setting external potential.", 7, 8, 0);
+
   Grid constraint(gd);
   constraint.Set(notincavity);
   //Functional f1 = f0 + ExternalPotential(external_potential);
@@ -84,25 +100,25 @@ int main(int, char **argv) {
   Grid potential(gd, external_potential + 0.005*VectorXd::Ones(gd.NxNyNz));
   printf("Energy is %g\n", ff.integral(potential));
 
-  check_peak("Energy of 3D cavity", 83, 84);
+  check_peak("Energy of 3D cavity", 83, 84, 40);
 
   Grid mygrad(gd);
   mygrad.setZero();
   ff.integralgrad(potential, &mygrad);
   printf("Grad is: %g\n", mygrad.norm());
 
-  check_peak("Gradient of 3D cavity", 100, 105);
+  check_peak("Gradient of 3D cavity", 100, 105, 250);
 
   ff = constrain(constraint, (HardSpheresFast(R, kT) + IdealGas(kT) + ChemicalPotential(mu))(n));
   printf("Energy new way is %g\n", ff.integral(potential));
 
-  check_peak("Energy of 3D cavity (fast)", 69, 70);
+  check_peak("Energy of 3D cavity (fast)", 69, 70, 10);
 
   mygrad.setZero();
   ff.integralgrad(potential, &mygrad);
   printf("Grad optimized is: %g\n", mygrad.norm());
 
-  check_peak("Gradient of 3D cavity (fast)", 363, 364);
+  check_peak("Gradient of 3D cavity (fast)", 363, 364, 60);
 
   if (retval == 0) {
     printf("\n%s passes!\n", argv[0]);
