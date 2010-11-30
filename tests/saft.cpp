2d98d5640c217c73a69e0df5420c2c2d9ede3601
David Roundy <roundyd@physics.oregonstate.edu>
2010-11-30 14:33:22 -0500

    add test so SAFT won't change without my knowing.
    Tested-on: collins (Linux 2.6.32-5-amd64)

---
+++ tests/saft.cpp
@@ -0,0 +1,82 @@
+// Deft is a density functional package developed by the research
+// group of Professor David Roundy
+//
+// Copyright 2010 The Deft Authors
+//
+// Deft is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with deft; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+// Please see the file AUTHORS for a list of authors.
+
+#include <stdio.h>
+#include "Functionals.h"
+#include "utilities.h"
+
+int retval = 0;
+
+const double kT = 1e-3; // room temperature in Hartree, approximately
+
+void test_energy(const char *name, Functional f,
+                 double true_energy, double fraccuracy = 1e-15) {
+  printf("\n************");
+  for (unsigned i=0;i<strlen(name);i++) printf("*");
+  printf("\n* Testing %s *\n", name);
+  for (unsigned i=0;i<strlen(name);i++) printf("*");
+  printf("************\n\n");
+
+  Lattice lat(Cartesian(0,.5,.5), Cartesian(.5,0,.5), Cartesian(.5,.5,0));
+  int resolution = 20;
+  GridDescription gd(lat, resolution, resolution, resolution);
+  Grid density(gd);
+  density = 1e-3*(-500*r2(gd)).cwise().exp()
+    + 1e-7*VectorXd::Ones(gd.NxNyNz);
+  Grid eff_potential(gd, -kT*density.cwise().log());
+
+  retval += f.run_finite_difference_test(name, eff_potential);
+
+  double e = f.integral(eff_potential);
+  printf("Energy = %.16g\n", e);
+  printf("Fractional error = %g\n", (e - true_energy)/fabs(true_energy));
+  if (!(fabs((e - true_energy)/true_energy) < fraccuracy)) {
+    printf("FAIL: Error in the energy is too big!\n");
+    retval++;
+  }
+}
+
+int main(int, char **argv) {
+  Functional n = EffectivePotentialToDensity(kT);
+  test_energy("association",
+              AssociationSAFT(water_prop.lengthscale, kT,
+                              water_prop.epsilonAB, water_prop.kappaAB)(n),
+              -3.5474667542423e-10);
+  test_energy("dispersion",
+              DispersionSAFT(water_prop.lengthscale, kT,
+                             water_prop.epsilon_dispersion,
+                             water_prop.lambda_dispersion)(n),
+              -1.092099264896821e-08);
+  test_energy("SAFT slow",
+              SaftFluidSlow(water_prop.lengthscale, kT,
+                            water_prop.epsilonAB, water_prop.kappaAB,
+                            water_prop.epsilon_dispersion,
+                            water_prop.lambda_dispersion, 0),
+              -1.63833795808733e-08);
+  //test_energy("SAFT",
+  //            SaftFluid(water_prop.lengthscale, kT,
+  //                      water_prop.epsilonAB, water_prop.kappaAB,
+  //                      water_prop.epsilon_dispersion,
+  //                      water_prop.lambda_dispersion, 0),
+  //            1.0);
+
+  if (retval == 0) {
+    printf("\n%s passes!\n", argv[0]);
+  } else {
+    printf("\n%s fails %d tests!\n", argv[0], retval);
+    return retval;
+  }
+}
