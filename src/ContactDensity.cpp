5b672cb626530069655c2c0323680e67e404d2bf
David Roundy <roundyd@physics.oregonstate.edu>
2011-06-15 07:40:51 -0700

    implement plot of contact density vs filling fraction and free energy
    Tested-on: collins (Linux 2.6.32-5-amd64)

---
+++ src/ContactDensity.cpp
@@ -0,0 +1,131 @@
+// Deft is a density functional package developed by the research
+// group of Professor David Roundy
+//
+// Copyright 2010 The Deft Authors
+//
+// Deft is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with deft; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+// Please see the file AUTHORS for a list of authors.
+
+#include "ContactDensity.h"
+
+static Functional VectorThirdTerm(double radius) {
+  Functional R(radius, "R");
+  Functional n2 = ShellConvolve(radius);
+  Functional n2x = xShellConvolve(radius);
+  Functional n2y = yShellConvolve(radius);
+  Functional n2z = zShellConvolve(radius);
+  return n2*(sqr(n2) - 3*(sqr(n2x) + sqr(n2y) + sqr(n2z)));
+}
+
+static Functional TensorThirdTerm(double radius) {
+  Functional R(radius, "R");
+  Functional n2 = ShellConvolve(radius);
+  Functional n2x = xShellConvolve(radius);
+  Functional n2y = yShellConvolve(radius);
+  Functional n2z = zShellConvolve(radius);
+  Functional nTxx = xxShellConvolve(radius);
+  Functional nTyy = yyShellConvolve(radius);
+  Functional nTzz = zzShellConvolve(radius);
+  Functional nTxy = xyShellConvolve(radius);
+  Functional nTyz = yzShellConvolve(radius);
+  Functional nTzx = zxShellConvolve(radius);
+  Functional nTxz = nTzx, nTyx = nTxy, nTzy = nTyz;
+  /*
+  Functional trace_nT3 =
+    nTxx*nTxx*nTxx + nTxx*nTxy*nTyx + nTxx*nTxz*nTzx + // starting with nTxx
+    nTxy*nTyx*nTxx + nTxy*nTyy*nTyx + nTxy*nTyz*nTzx + // starting with nTxy
+    nTxz*nTzx*nTxx + nTxz*nTzy*nTyx + nTxz*nTzz*nTzx + // starting with nTxz
+
+    nTyx*nTxx*nTxy + nTyx*nTxy*nTyy + nTyx*nTxz*nTzy + // starting with nTyx
+    nTyy*nTyx*nTxy + nTyy*nTyy*nTyy + nTyy*nTyz*nTzy + // starting with nTyy
+    nTyz*nTzx*nTxy + nTyz*nTzy*nTyy + nTyz*nTzz*nTzy + // starting with nTyz
+
+    nTzx*nTxx*nTxz + nTzx*nTxy*nTyz + nTzx*nTxz*nTzz + // starting with nTzx
+    nTzy*nTyx*nTxz + nTzy*nTyy*nTyz + nTzy*nTyz*nTzz + // starting with nTzy
+    nTzz*nTzx*nTxz + nTzz*nTzy*nTyz + nTzz*nTzz*nTzz; // starting with nTzz
+  */
+  Functional trace_nT3 =
+    6*nTxy*nTyz*nTzx +
+    nTxx*(  sqr(nTxx) + 3*sqr(nTxy) + 3*sqr(nTzx)) +
+    nTyy*(3*sqr(nTxy) +   sqr(nTyy) + 3*sqr(nTyz)) +
+    nTzz*(3*sqr(nTzx) + 3*sqr(nTzy) +   sqr(nTzz));
+  return (n2*sqr(n2) - 3*n2*(sqr(n2x) + sqr(n2y) + sqr(n2z))
+          +
+          9*(sqr(n2x)*nTxx + sqr(n2y)*nTyy + sqr(n2z)*nTzz
+             + 2*(n2x*n2y*nTxy + n2y*n2z*nTyz + n2z*n2x*nTzx)
+             - 0.5*trace_nT3));
+}
+
+static Functional n3(double radius) {
+  return StepConvolve(radius);
+}
+
+static Functional n2(double radius) {
+  return ShellConvolve(radius);
+}
+
+static Functional n1(double radius) {
+  Functional R(radius, "R");
+  return ShellConvolve(radius)/(4*M_PI*R);
+}
+
+static Functional n0(double radius) {
+  Functional R(radius, "R");
+  return ShellConvolve(radius)/(4*M_PI*sqr(R));
+}
+
+Functional dWBNT_dn0(double radius) {
+  return log(1-n3(radius));
+}
+
+Functional dWBNT_dn1(double radius) {
+  return n2(radius)/(1-n3(radius));
+}
+
+Functional dWBNT_dn2(double radius) {
+  Functional n2x = xShellConvolve(radius);
+  Functional n2y = yShellConvolve(radius);
+  Functional n2z = zShellConvolve(radius);
+  return n1(radius)/(1-n3(radius)) +
+    (sqr(n2(radius)) - 3*(sqr(n2x) + sqr(n2y) + sqr(n2z)))*
+    (n3(radius) + sqr(1-n3(radius))*log(1-n3(radius)))/(12*M_PI*sqr(n3(radius))*sqr(1-n3(radius)));
+}
+
+Functional dWBNT_dn3(double radius) {
+  Functional R(radius, "R");
+  Functional vtt = VectorThirdTerm(radius);
+  Functional n3_ = n3(radius);
+  Functional omn3 = 1 - n3_;
+  Functional n2x = xShellConvolve(radius);
+  Functional n2y = yShellConvolve(radius);
+  Functional n2z = zShellConvolve(radius);
+  Functional nV22 = sqr(n2x) + sqr(n2y) + sqr(n2z);
+  Functional n22mnV22 = sqr(n2(radius)) - nV22;
+  return n0(radius)/omn3 +
+    R*n22mnV22/sqr(omn3) +
+    // vtt*(Functional(1)/(n3_*Pow(3)(omn3))
+    //      - Functional(1)/(sqr(n3_)*sqr(omn3))
+    //      - Functional(1)/(sqr(n3_)*omn3)
+    //      - 2*log(omn3)/Pow(3)(n3_));
+    vtt*(Functional(1)/sqr(omn3) + 2*log(omn3)/(35*M_PI*sqr(n3_)*sqr(omn3)));
+  Functional ttt = TensorThirdTerm(radius);
+}
+
+Functional dAdR_simplest(double radius) {
+  Functional R(radius, "R");
+  return dWBNT_dn3(radius)*n2(radius) +
+    dWBNT_dn2(radius)*(Functional(2)/R)*n2(radius) +
+    dWBNT_dn1(radius)*n0(radius);
+}
+
+Functional ContactDensitySimplest(double radius) {
+  return Functional(0.25)*dAdR_simplest(radius)/n2(radius);
+}
---
+++ src/ContactDensity.h
@@ -0,0 +1,15 @@
+// -*- mode: C++; -*-
+
+#pragma once
+
+#include "Functionals.h"
+
+Functional dWBNT_dn0(double radius);
+Functional dWBNT_dn1(double radius);
+Functional dWBNT_dn2(double radius);
+Functional dWBNT_dn3(double radius);
+
+Functional dWBNT_dnV1(double radius);
+Functional dWBNT_dnV2(double radius);
+
+Functional ContactDensitySimplest(double radius);
