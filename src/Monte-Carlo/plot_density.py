8fcaa4dfe4f0403dd87baab6413e9730d9b0e750
Chris <chris@ubuntu.(none)>
2011-09-19 07:59:42 -0700

    Change in simulation beggining

--- src/Monte-Carlo/plot_density.py
+++ src/Monte-Carlo/plot_density.py
@@ -6,6 +6,9 @@ data = numpy.loadtxt("density.dat")
 #print data[:,0]
 #print data[1]
 pylab.plot(data[:,0],data[:,1]*4*numpy.pi/3,"o-")
+pylab.plot(data[:,0],data[:,2]*4*numpy.pi/3,"o-",label="ConDensity")
+pylab.plot(data[:,0],data[:,3]*4*numpy.pi/3,"o-",label="CenConDensity")
 pylab.xlabel("radius")
 pylab.ylabel("filling fraction")
+pylab.legend()
 pylab.show()
