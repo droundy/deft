a4ac9d4abea318bf5f00c1b01e0e36eeb511860c
David Roundy <roundyd@physics.oregonstate.edu>
2011-10-24 15:33:52 -0700

    add new empty paper Tested everything.

---
+++ papers/fuzzy-fmt/figs/sphere.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --mem-per-cpu=2000
+# #SBATCH --mail-type ALL
+# #SBATCH --mail-user daveroundy@gmail.com
+#SBATCH --output sphere-%j.out
+
+set -ev
+
+hostname
+date
+
+time nice -19 papers/contact/figs/sphere.mkdat $DIAMETER $NUMBER $FUNCTIONAL
+
+date
