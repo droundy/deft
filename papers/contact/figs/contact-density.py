fdf5506ab9e7022d71941e49711706161d181072
David Roundy <roundyd@physics.oregonstate.edu>
2011-11-22 13:38:45 -0800

    a bit of work on contact paper Tested everything.

--- papers/contact/figs/contact-density.py
+++ papers/contact/figs/contact-density.py
@@ -23,7 +23,7 @@ pylab.plot(mcdata[:,0],mcdata[:,2]*4*numpy.pi/3,"r-",label="ConDensity")
 pylab.plot(mcdata[:,0],mcdata[:,3]*4*numpy.pi/3,"g-",label="CenConDensity")
 
 pylab.plot(dftdata[:,0],dftdata[:,3]*4*numpy.pi/3,"g+--",label="simple contact")
-pylab.plot(dftdata[:,0],dftdata[:,4]*4*numpy.pi/3,"gx--",label="Wu and Fu")
+pylab.plot(dftdata[:,0],dftdata[:,4]*4*numpy.pi/3,"gx--",label="Yu and Wu")
 pylab.plot(dftdata[:,0],dftdata[:,5]*4*numpy.pi/3,"ro--",label="DFT at sphere")
 
 pylab.xlabel("radius")
