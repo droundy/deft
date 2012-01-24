6fcd91a3031bc7545d505a3016611b80f3be387a
jeffschulte83@gmail.com <jeffschulte@crystal.physics.oregonstate.edu>
2012-01-24 11:52:47 -0800

    support more b.c. in monte-carlo Tested everything.

--- src/Monte-Carlo/monte-carlo.h
+++ src/Monte-Carlo/monte-carlo.h
@@ -10,13 +10,11 @@ void writeSpheres(Vector3d *spheres, int n, FILE * o);
 Vector3d move(Vector3d v, double x, double y, double z);
 void run(const double rad, const int N, const long times, const char *filename);
 bool overlap(Vector3d *spheres, Vector3d v, int n, double R, int s);
-bool touch(Vector3d *spheres, Vector3d v, int n, double R, double delta, int s);
+bool touch(Vector3d w, Vector3d v, double oShell);
 Vector3d move(Vector3d v, double R);
 Vector3d move(Vector3d v);
-bool overlap(Vector3d *spheres, Vector3d v, int n, double R, double rad, int s);
-bool overlap(Vector3d *spheres, Vector3d v, int n, double R, int s, double x, double y, double z);
 int shell(Vector3d v, int div, double *radius);
-double countOverLaps(Vector3d *spheres, int n, double R, double rad);
+double countOverLaps(Vector3d *spheres, int n, double R);
 
 inline double distance(Vector3d v1, Vector3d v2){
   return (v1 - v2).norm();
