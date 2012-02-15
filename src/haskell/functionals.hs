be92f83796ebb2f891e5b5d6f1fd0b637414ae0b
David Roundy <roundyd@physics.oregonstate.edu>
2012-02-15 11:50:19 -0800

    add saft water functional to SomeFunctionals.hs Tested everything on bingley.

--- src/haskell/functionals.hs
+++ src/haskell/functionals.hs
@@ -14,6 +14,8 @@ main =
        generateHeader (fmt wb_contact_at_sphere) (Just (r_var "R")) "ContactAtSphere"
      gen "src/YuWuContactFast.cpp" $
        generateHeader (fmt yuwu_contact) (Just (r_var "R")) "YuWuContact"
+     gen "src/SaftFluid2Fast.cpp" $
+       generateHeader saft_fluid (Just (r_var "R")) "SaftFluid2"
      gen "papers/contact/formulas.tex" $ unlines $ map definelatex
        [("phione", phi1),
         ("phitwo", phi2),
@@ -25,8 +27,6 @@ main =
         ("dwbdnone", dwbdn1),
         ("dwbdnonev", dwbdn1v_over_n2v),
         ("dwbdntwov", dwbdn2v_over_n2v)]
-     --writeFile "src/HardSphereGas2Fast.cpp" $
-     --  generateHeader (whitebear + idealgas + mu*n) ["R", "mu"] "HardSphereGasFast2"
 
 definelatex :: (String, Expression RealSpace) -> String
 definelatex (v,e) = "\\newcommand\\" ++ v ++ "{" ++ latex e ++ "}"
