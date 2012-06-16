d3b3ff1ac3f5f35058073bc0575f2192b84f06fc
David Roundy <roundyd@physics.oregonstate.edu>
2012-06-15 19:41:42 -0700

    replace surface-tension test with something nicer Tested everything on bones.

--- src/haskell/latex-functionals.hs
+++ src/haskell/latex-functionals.hs
@@ -4,6 +4,7 @@ import Latex
 import Expression ( (===), grad, joinFFTs, integrate )
 import System.Environment ( getArgs )
 import System.Process ( rawSystem )
+import System.Exit ( ExitCode(ExitSuccess) )
 import System.FilePath ( dropFileName )
 
 main :: IO ()
@@ -12,8 +13,10 @@ main =
      let pdf f x = if f `elem` todo
                    then do let texname = reverse (drop 3 $ reverse f) ++ "tex"
                            writeFile texname x
-                           rawSystem "pdflatex" ["-output-directory", dropFileName f, texname]
-                           return ()
+                           ec <- rawSystem "pdflatex" ["-output-directory", dropFileName f, texname]
+                           if ec /= ExitSuccess
+                              then fail $ f ++ " failed with exit code " ++ show ec
+                              else return ()
                    else return ()
      pdf "doc/WhiteBear.pdf" $ latexEasy $ "FHS" === whitebear
      pdf "doc/Association.pdf" $ latexEasy $ "Fassoc" === saft_association
@@ -22,4 +25,7 @@ main =
      let foo = "Fdisp" === integrate saft_dispersion
      pdf "doc/GradDispersion.pdf" $ latexEasy $ "grad" === (grad "x" foo)
      pdf "doc/JoinedGradDispersion.pdf" $ latexEasy $ "grad" === (joinFFTs $ grad "x" foo)
+     let assoc = "Fassoc" === integrate saft_association
+     pdf "doc/GradAssociation.pdf" $ latexEasy $ "grad" === (grad "x" assoc)
+     pdf "doc/JoinedGradAssociation.pdf" $ latexEasy $ "grad" === (joinFFTs $ grad "x" assoc)
      pdf "doc/JoinedGradHydrogen.pdf" $ latexEasy $ "grad" === (joinFFTs $ grad "x" (integrate (oneElectron hydrogenPotential)))
--- tests/surface-tension.cpp
+++ tests/surface-tension.cpp
@@ -19,217 +19,64 @@
 #include "Functionals.h"
 #include "LineMinimizer.h"
 #include "equation-of-state.h"
+#include "handymath.h"
 
-const double ngas = water_prop.vapor_density; // vapor density of water
-const double nliquid = water_prop.liquid_density; // density of liquid water
-const double mu = -water_prop.kT*log(ngas);
-const double Veff_liquid = -water_prop.kT*log(nliquid);
-
-// Here we set up the lattice.
-Lattice lat(Cartesian(0.2,0,0), Cartesian(0,0.2,0), Cartesian(0,0,20));
-GridDescription gd(lat, 1, 1, 200);
-
-// And the functional...
-const double interaction_energy_scale = 2e-4;
-Functional attraction = GaussianPolynomial(-interaction_energy_scale/nliquid/nliquid/2, 0.5, 2);
-Functional repulsion = GaussianPolynomial(interaction_energy_scale/nliquid/nliquid/nliquid/nliquid/4, 0.125, 4);
-Functional f0 = ChemicalPotential(mu) + attraction + repulsion;
-Functional n = EffectivePotentialToDensity();
-Functional f = IdealGasOfVeff + f0(n);
-
-Grid external_potential(gd);
-Grid potential(gd);
-
-Functional ff;
-
-int test_minimizer(const char *name, Minimizer *min, int numiters, double fraccuracy=1e-3) {
-  clock_t start = clock();
-  printf("\n************");
-  for (unsigned i=0;i<strlen(name);i++) printf("*");
-  printf("\n* Testing %s *\n", name);
-  for (unsigned i=0;i<strlen(name);i++) printf("*");
-  printf("************\n\n");
-
-  potential = Veff_liquid*VectorXd::Ones(gd.NxNyNz);
-  for (int i=0;i<gd.NxNyNz/2;i++) potential[i] = mu;
-
-  for (int i=0;i<numiters && min->improve_energy(false);i++) {
-    fflush(stdout);
-  }
-  min->print_info();
-  //min->minimize(f, gd);
-  //for (int i=0;i<numiters && min->improve_energy(false);i++) {
-  //  //printf("Without external potential...\n");
-  //  fflush(stdout);
-  //}
-  //min->print_info();  
-
-  const double Einterface_with_external = ff.integral(water_prop.kT, potential);
-  const double Einterface = f.integral(water_prop.kT, potential);
-  double Ninterface = 0;
-  {
-    Grid density(gd, EffectivePotentialToDensity()(water_prop.kT, gd, potential));
-    for (int i=0;i<gd.NxNyNz;i++) Ninterface += density[i]*gd.dvolume;
-  }
-  printf("Minimization took %g seconds.\n", (clock() - double(start))/CLOCKS_PER_SEC);
-  start = clock();
-
-  Grid gas(gd, mu*VectorXd::Ones(gd.NxNyNz));
-  min->minimize(f, gd, &gas);
-  // The following sometimes fails with NaNs if it's run too long, for
-  // reasons I don't understand.
-  for (int i=0;i<numiters && min->improve_energy(false);i++) {
-    //printf("GAS\n");
-    fflush(stdout);
-  }
-  const double Egas = f.integral(water_prop.kT, gas);
-  double Ngas = 0;
-  {
-    Grid density(gd, EffectivePotentialToDensity()(water_prop.kT, gd, gas));
-    for (int i=0;i<gd.NxNyNz;i++) Ngas += density[i]*gd.dvolume;
-  }
-  min->print_info();
-  printf("gas energy is %g\n", f.integral(water_prop.kT, gas));
-  printf("Minimization took %g seconds.\n", (clock() - double(start))/CLOCKS_PER_SEC);
-  start = clock();
-
-  double minpot = 1e100;
-  for (int i=0;i<gd.NxNyNz;i++)
-    if (potential[i] < minpot) minpot = potential[i];
-  Grid liquid(gd, Veff_liquid*VectorXd::Ones(gd.NxNyNz));
-  min->minimize(f, gd, &liquid);
-  for (int i=0;i<numiters && min->improve_energy(false);i++) {
-    //printf("LIQUID\n");
-    fflush(stdout);
-  }
-  min->print_info();
-  printf("Minimization took %g seconds.\n", (clock() - double(start))/CLOCKS_PER_SEC);
-  const double Eliquid = f.integral(water_prop.kT, liquid);
-  double Nliquid = 0;
-  {
-    Grid density(gd, EffectivePotentialToDensity()(water_prop.kT, gd, liquid));
-    for (int i=0;i<gd.NxNyNz;i++) Nliquid += density[i]*gd.dvolume;
-  }
-
-  double retval = 0;
-  printf("\n\n");
-  const double Einterface_with_external_true = -1.00700771531599e-05;
-  printf("interface energy is %.15g (vs. %.15g)\n", Einterface, Einterface_with_external);
-  if (Einterface_with_external < Einterface_with_external_true) {
-    printf("FAIL: Einterface_with_external is too low: %.17g (should be %.17g)\n",
-	   Einterface_with_external, Einterface_with_external_true);
-    retval++;
-  }
-  printf("fractional Einterface error = %g\n",
-         (Einterface_with_external - Einterface_with_external_true)/fabs(Einterface_with_external_true));
-  if (fabs((Einterface_with_external - Einterface_with_external_true)/Einterface_with_external_true) > fraccuracy) {
-    printf("FAIL: Einterface_with_external is too inaccurate...\n");
-    retval++;
-  }
-
-  printf("gas energy is %.15g\n", Egas);
-  const double Egas_true = -2.53378305461179e-05;
-  if (Egas < Egas_true) {
-    printf("FAIL: Egas is too low: %.16g\n", Egas);
-    retval++;
-  }
-
-  printf("liquid energy is %.15g\n", Eliquid);
-  const double Eliquid_true = -2.53378305461181e-05;
-  if (Eliquid < Eliquid_true) {
-    printf("FAIL: Eliquid is too low: %.16g\n", Eliquid);
-    retval++;
-  }
-  printf("Ninterface/liquid/gas = %g/%g/%g\n", Ninterface, Nliquid, Ngas);
-  const double X = Ninterface/Nliquid; // Fraction of volume effectively filled with liquid.
-  printf("X is %g\n", X);
-  
-  const double true_surface_tension = 1.208891768e-05;
-
-  const double surface_tension = (Einterface - Eliquid*X - Egas*(1-X))/2/0.2/0.2;
-  printf("surface tension is %.10g\n", surface_tension);
-
-
-  printf("fractional surface tension error = %g\n",
-         (surface_tension - true_surface_tension)/fabs(true_surface_tension));
-  if (fabs((surface_tension - true_surface_tension)/true_surface_tension) > fraccuracy) {
-    printf("FAIL:\n");
-    printf("Error in the surface tension is too big!\n");
-    retval++;
-  }
-  return retval;
+static void took(const char *name) {
+  static clock_t last_time = clock();
+  clock_t t = clock();
+  printf("%s took %g seconds\n", name, (t-last_time)/double(CLOCKS_PER_SEC));
+  last_time = t;
 }
 
-double forcer(Cartesian r) {
-  const double z = r.dot(Cartesian(0,0,1));
-  return 0.0001/nliquid*exp(-(z-5.0)*(z-5)/2);
-}
+const double nv = 1e-7, nl = 1e-3;
+Functional n0 = Gaussian(1);
+// The following is the simplest interesting functional
+Functional f = /*IdealGasOfVeff + */ 1e6*sqr(n0 - Functional(water_prop.liquid_density))*sqr(n0 - Functional(water_prop.vapor_density));
 
 int main(int, char **argv) {
-  external_potential.Set(forcer);
-  ff = IdealGasOfVeff + (f0 + ExternalPotential(external_potential))(n);
-
-  Grid test_density(gd, EffectivePotentialToDensity()(water_prop.kT, gd, -1e-4*(-2*r2(gd)).cwise().exp()
-                                                      + mu*VectorXd::Ones(gd.NxNyNz)));
-
-  // The following is for debugging our simple liquid...
-  //printf("mu is %g\n", mu);
-  //printf("Veff_liquid is %g\n", Veff_liquid);
-  //printf("%12s\t%12s\t%12s\n", "Veff", "n", "energy");
-  //for (double v=0.5*Veff_liquid; v<mu+5*kT; v*=1.05) {
-  //  printf("%12g\t%12g\t%12g\n", v, EffectivePotentialToDensity(kT)(v), f(v));
-  //}
-
   int retval = 0;
-
-  potential = +1e-4*((-10*r2(gd)).cwise().exp()) + 1.14*mu*VectorXd::Ones(gd.NxNyNz);
-  retval += ff.run_finite_difference_test("simple liquid", water_prop.kT, potential);
-  fflush(stdout);
-
-  retval += attraction.run_finite_difference_test("quadratic", water_prop.kT, test_density);
-  fflush(stdout);
-  retval += repulsion.run_finite_difference_test("repulsive", water_prop.kT, test_density);
-  fflush(stdout);
-
-  {
-    // We need a small initial step for PreconditionedDownhill in
-    // order to avoid jumping over the barrier from the liquid global
-    // minimum to the vapor local minimum.
-    Minimizer pd = PreconditionedDownhill(ff, gd, water_prop.kT, &potential, 1e-9);
-    potential.setZero();
-    retval += test_minimizer("PreconditionedDownhill", &pd, 4001, 10); // ignore accuracy for now
-
-    Grid density(gd, EffectivePotentialToDensity()(water_prop.kT, gd, potential));
-    //density.epsNativeSlice("PreconditionedDownhill.eps", Cartesian(0,0,20), Cartesian(0.1,0,0),
-    //                       Cartesian(0,0,0));
-
-    retval += attraction.run_finite_difference_test("quadratic", water_prop.kT, density);
-    retval += repulsion.run_finite_difference_test("repulsive", water_prop.kT, density);
+  const double kB = 3.16681539628059e-6; // Boltzmann's constant in Hartree/Kelvin
+  LiquidProperties prop = water_prop;
+  prop.lengthscale = 2;
+  double criten = 0;
+  for (double n = prop.vapor_density; n < prop.liquid_density; n *= 1.1) {
+    if (f(prop.kT, n) > criten) {
+      criten = f(prop.kT, n);
+      prop.critical_density = n;
+    }
+    // printf("f(%g) = %g\n", n, f(prop.kT, n));
   }
-
-  Minimizer psd = PreconditionedSteepestDescent(ff, gd, water_prop.kT, &potential, QuadraticLineMinimizer);
-  potential.setZero();
-  // I'm not sure why PreconditionedSteepestDescent is failing this badly!
-  //retval += test_minimizer("PreconditionedSteepestDescent", &psd, 138000, 1e-2);
-
-  {
-    Minimizer pcg = PreconditionedConjugateGradient(ff, gd, water_prop.kT, &potential, QuadraticLineMinimizer);
-    potential.setZero();
-    retval += test_minimizer("PreconditionedConjugateGradient", &pcg, 6000, 3e-5);
-
-    Grid density(gd, EffectivePotentialToDensity()(water_prop.kT, gd, potential));
-    density.epsNativeSlice("PreconditionedConjugateGradient.eps",
-                           Cartesian(0,0,40), Cartesian(0.1,0,0), Cartesian(0,0,0));
+  printf("Critical density: %g\n", prop.critical_density);
+  f = OfEffectivePotential(f);
+  saturated_liquid_properties(f, &prop);
+  prop.critical_density = 1;
+  took("Finding bulk densities");
+  if (fabs(prop.liquid_density-water_prop.liquid_density) > 1e-9*water_prop.liquid_density) {
+    printf("FAIL: bad liquid density:  %g (vs. %g)\n", prop.liquid_density, water_prop.liquid_density);
+    retval++;
   }
-
-  Minimizer cg = ConjugateGradient(ff, gd, water_prop.kT, &potential, QuadraticLineMinimizer);
-  potential.setZero();
-  retval += test_minimizer("ConjugateGradient", &cg, 3000, 3e-5);
-
-  if (retval == 0) {
-    printf("\n%s passes!\n", argv[0]);
+  if (fabs(prop.vapor_density-water_prop.vapor_density) > 1e-9*water_prop.vapor_density) {
+    printf("FAIL: bad vapor density:  %g (vs. %g)\n", prop.vapor_density, water_prop.vapor_density);
+    retval++;
+  }
+  // Here we set up an unused lattice.
+  Lattice lat(Cartesian(0.2,0,0), Cartesian(0,0.2,0), Cartesian(0,0,20));
+  GridDescription gd(lat, 1, 1, 200);
+  Grid foo(gd);
+  Minimizer min = MaxIter(500, Precision(1e-14, ConjugateGradient(f, gd, prop.kT, &foo, QuadraticLineMinimizer)));
+  const bool amverbose = false;
+  const double st = surface_tension(min, f, prop, amverbose);
+  took("Finding surface tension");
+  const double true_st = 4.08612e-5;
+  printf("surface tension is %.15g (vs %.15g) (%g fractionally)\n", st, true_st, fabs(st-true_st)/true_st);
+  if (fabs(st - true_st) > 1e-3*true_st) {
+    printf("FAIL: bad surface tension: %.15g (vs %.15g) (%g fractionally)\n", st, true_st, fabs(st-true_st)/true_st);
+    retval++;
+  }
+  if (retval > 0) {
+    printf("%s failed %d tests!\n", argv[0], retval);
   } else {
-    printf("\n%s fails %d tests!\n", argv[0], retval);
-    return retval;
+    printf("%s passed!\n", argv[0]);
   }
+  return retval;
 }
