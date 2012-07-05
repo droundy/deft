ecb3f8667afcf5f2eb28a7b1ce4902f3fda1fdd1
David Roundy <roundyd@physics.oregonstate.edu>
2012-07-05 11:41:12 -0700

    a few minor changes in contact paper (incl. resizing figures) Tested everything on node08.

--- papers/contact/figs/plot-walls.py
+++ papers/contact/figs/plot-walls.py
@@ -41,11 +41,14 @@ n_plt.legend(loc=1, bbox_to_anchor=[1.0, 1.0], ncol=2).get_frame().set_alpha(0.5
 pylab.xlim(0,12)
 n = len(mcdata[:,0])
 if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.45) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.35)):
-    pylab.ylim(0.1,0.8)
+    pylab.ylim(0.1,2.8)
 if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.35) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.25)):
-    pylab.ylim(0.17,0.43)
+    pylab.ylim(0.17,1.19)
 if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.15) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.05)):
-    pylab.ylim(0.04,0.19)
+    pylab.ylim(0.04,0.16)
+    n_plt.yaxis.set_ticks(pylab.arange(0.0, 0.15, 0.02))
+    n_plt.legend(loc='lower right', ncol=2).get_frame().set_alpha(0.5)
+pylab.ylim(ymin=0)
 pylab.xlabel("position")
 #pylab.legend(loc='lower left', ncol=2).get_frame().set_alpha(0.5)
 
@@ -64,15 +67,16 @@ A_plt.plot(wbm2data[start_here:stop_here,0],wbm2data[start_here:stop_here,5],"m-
            markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
 A_plt.plot(dftdata[:,0],dftdata[:,7],"rx--",markevery=me,label="Gross",
            markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
-A_plt.legend(loc=1, ncol=1).get_frame().set_alpha(0.5)
+A_plt.legend(loc='lower right', ncol=1).get_frame().set_alpha(0.5)
 pylab.xlim(0,12)
 #matplotlib.ticks(arange(0.5, 1.5, 3.5))
-# if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.45) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.35)):
-#     pylab.ylim(2.7,4.8)
-# if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.35) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.25)):
-#     pylab.ylim(1.0,4.20)
-# if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.15) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.05)):
-#     pylab.ylim(0.95,1.70)
+if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.45) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.35)):
+    pylab.ylim(2.7,4.8)
+if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.35) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.25)):
+    pylab.ylim(1.0,4.20)
+if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.15) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.05)):
+    pylab.ylim(0.95,1.50)
+pylab.ylim(ymin=0)
 
 S_plt = pylab.subplot(3,1,2)
 S_plt.plot(mcdata[:,0]+mcoffset,mcdata[:,3+2*off]/n0mc,"b-",label="$g_\sigma^S$ MC")
@@ -81,14 +85,15 @@ S_plt.plot(dftdata[start_here:stop_here,0],dftdata[start_here:stop_here,3],
 S_plt.plot(wbm2data[start_here:stop_here,0],wbm2data[start_here:stop_here,3],"m--",markevery=me,label="$g_\sigma^S$ (mark II)",
            markerfacecolor='none',markeredgecolor='green', markeredgewidth=1)
 S_plt.plot(dftdata[:,0],dftdata[:,4],"rx--",markevery=me,label="Yu and Wu")
-S_plt.legend(loc=1, bbox_to_anchor=[1.0, 1.0], ncol=1).get_frame().set_alpha(0.5)
+S_plt.legend(loc='lower right', ncol=1).get_frame().set_alpha(0.5)
 pylab.xlim(0,12)
-# if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.45) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.35)):
-#     pylab.ylim(2.3,5.50)
-# if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.35) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.25)):
-#     pylab.ylim(1.50,3.60)
-# if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.15) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.05)):
-#     pylab.ylim(1.10,1.60)
+if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.45) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.35)):
+    pylab.ylim(2.3,9.0)
+if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.35) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.25)):
+    pylab.ylim(1.50,3.70)
+if ((mcdata[int(n/2),1]*4*numpy.pi/3<0.15) & (mcdata[int(n/2),1]*4*numpy.pi/3>0.05)):
+    pylab.ylim(1.10,1.60)
+pylab.ylim(ymin=0)
 
 pylab.ylabel("filling fraction")
 
