b0bc223ece1c30801df8cc4b3356ab06b1ea6bb6
jeffschulte83@gmail.com <jeffschulte@node09.quipu>
2012-08-28 10:58:35 -0700

    Added data and another monte-carlo bug fix Tested everything on node09.

--- src/Monte-Carlo/monte-carlo.cpp
+++ src/Monte-Carlo/monte-carlo.cpp
@@ -296,105 +296,123 @@ int main(int argc, char *argv[]){
   for (int i=0;i<N;i++){max_move_counter[i]=0;}
   int * move_counter = new int [N];
   for (int i=0;i<N;i++){move_counter[i]=0;}
+
+  clock_t output_period = CLOCKS_PER_SEC*60; // start at outputting every minute
+  clock_t max_output_period = CLOCKS_PER_SEC*60*60; // top out at one hour interval
+  clock_t last_output = clock(); // when we last output data
   for (long j=0; j<iterations; j++){
-    if (num_timed++ > num_to_time) {
-      clock_t now = clock();
-      secs_per_iteration = (now - double(start))/CLOCKS_PER_SEC/num_to_time;
-      printf("took %g microseconds per iteration\n", 1000000*secs_per_iteration);
+	  num_timed = num_timed + 1;
+    if (num_timed > num_to_time) {
       num_timed = 0;
+      ///////////////////////////////////////////start of print.dat
+      const clock_t now = clock();
+      secs_per_iteration = (now - double(start))/CLOCKS_PER_SEC/num_to_time;
+      if (secs_per_iteration*num_to_time < 1) {
+        printf("took %g microseconds per iteration\n", 1000000*secs_per_iteration);
+        num_to_time *= 2;
+      } else {
+        // Set the number of iterations to time to a minute, so we
+        // won't check *too* many times.
+        num_to_time = long(60/secs_per_iteration);
+      }
       start = now;
-      // after the first timing, just time things once per percent (as
-      // often as we print the % complete messages)
-      
-      
-       ///////////////////////////////////////////start of print.dat
-    
-
-
-    int hours_passed = 0;
-    hours_passed = floor(clock()/10000/100/60/60 + 0.5);  // rounds to the nearest hour
-    if (hours_passed >= hours_now){
-      printf("Saved Data after %d hours(s) \n", hours_passed);      
-      hours_now = hours_passed + 1;
-      if (!flat_div){
-        for(long i=0; i<div; i++){
+      if (now > last_output + output_period) {
+        last_output = now;
+        if (output_period < max_output_period/2) {
+          output_period *= 2;
+        } else if (output_period < max_output_period) {
+          output_period = max_output_period;
+        }
+        {
+          double secs_done = double(now)/CLOCKS_PER_SEC;
+          long mins_done = secs_done / 60;
+          long hours_done = mins_done / 60;
+          mins_done = mins_done % 60;
+          if (hours_done > 50) {
+            printf("Saved data after %ld hours\n", hours_done);
+          } else if (mins_done < 1) {
+            printf("Saved data after %.1f seconds\n", secs_done);
+          } else if (hours_done < 1) {
+            printf("Saved data after %ld minutes\n", mins_done);
+          } else if (hours_done < 2) {
+            printf("Saved data after %ld minutes\n", mins_done);
+          } else {
+            printf("Saved data after %ld hours, %ld minutes\n", hours_done, mins_done);
+          }
+        }
+        if (!flat_div){
+          for(long i=0; i<div; i++){
             double rmax = radius[i+1];
             double rmin = radius[i];
             density[i]=shells[i]/(((4/3.*M_PI*rmax*rmax*rmax)-(4/3.*M_PI*rmin*rmin*rmin)))/((j+1)/double(N));
             n0[i]=shellsArea[i]/(((4/3.*M_PI*rmax*rmax*rmax)-(4/3.*M_PI*rmin*rmin*rmin)))/((j+1)/double(N))/(4*M_PI*R*R);
             nA[i]=shellsDoubleArea[i]/(((4/3.*M_PI*rmax*rmax*rmax)-(4/3.*M_PI*rmin*rmin*rmin)))/((j+1)/double(N))
-                  /(4*M_PI*2*R*2*R);
+              /(4*M_PI*2*R*2*R);
+          }
+        } else {
+          for(long i=0; i<div; i++){
+            density[i]=shells[i]/(lenx*leny*lenz/div)/((j+1)/double(N));
+            n0[i]=shellsArea[i]/(lenx*leny*lenz/div)/((j+1)/double(N))/(4*M_PI*R*R);
+            nA[i]=shellsDoubleArea[i]/(lenx*leny*lenz/div)/((j+1)/double(N))/(4*M_PI*2*R*2*R);
+          }
         }
-      } else {
         for(long i=0; i<div; i++){
-          density[i]=shells[i]/(lenx*leny*lenz/div)/((j+1)/double(N));
-          n0[i]=shellsArea[i]/(lenx*leny*lenz/div)/((j+1)/double(N))/(4*M_PI*R*R);
-          nA[i]=shellsDoubleArea[i]/(lenx*leny*lenz/div)/((j+1)/double(N))/(4*M_PI*2*R*2*R);
-        }
-        }
-      for(long i=0; i<div; i++){
-        SconDensity[i]=((SconShells[i]+0.0)/shells[i])/((4/3.*M_PI*oShellArray[0]*8*oShellArray[0]*oShellArray[0]-4/3.*M_PI*8*R*R*R));
-        ScenConDensity[i]=4*M_PI*R*R*((ScenConShells[i]+0.0)/shellsArea[i])/((4/3.*M_PI*8*oShellArray[0]*oShellArray[0]*oShellArray[0]-4/3.*M_PI*8*R*R*R));
+          SconDensity[i]=((SconShells[i]+0.0)/shells[i])/((4/3.*M_PI*oShellArray[0]*8*oShellArray[0]*oShellArray[0]-4/3.*M_PI*8*R*R*R));
+          ScenConDensity[i]=4*M_PI*R*R*((ScenConShells[i]+0.0)/shellsArea[i])/((4/3.*M_PI*8*oShellArray[0]*oShellArray[0]*oShellArray[0]-4/3.*M_PI*8*R*R*R));
           MconDensity[i]=((MconShells[i]+0.0)/shells[i])/((4/3.*M_PI*oShellArray[1]*8*oShellArray[1]*oShellArray[1]-4/3.*M_PI*8*R*R*R));
           McenConDensity[i]=4*M_PI*R*R*((McenConShells[i]+0.0)/shellsArea[i])/((4/3.*M_PI*8*oShellArray[1]*oShellArray[1]*oShellArray[1]-4/3.*M_PI*8*R*R*R));
           LconDensity[i]=((LconShells[i]+0.0)/shells[i])/((4/3.*M_PI*oShellArray[2]*8*oShellArray[2]*oShellArray[2]-4/3.*M_PI*8*R*R*R));
           LcenConDensity[i]=4*M_PI*R*R*((LcenConShells[i]+0.0)/shellsArea[i])/((4/3.*M_PI*8*oShellArray[2]*oShellArray[2]*oShellArray[2]-4/3.*M_PI*8*R*R*R));
           GconDensity[i]=((GconShells[i]+0.0)/shells[i])/((4/3.*M_PI*oShellArray[3]*8*oShellArray[3]*oShellArray[3]-4/3.*M_PI*8*R*R*R));
           GcenConDensity[i]=4*M_PI*R*R*((GcenConShells[i]+0.0)/shellsArea[i])/((4/3.*M_PI*8*oShellArray[3]*oShellArray[3]*oShellArray[3]-4/3.*M_PI*8*R*R*R));
-      }
-      
+        }
+        
         //FILE *out = fopen((const char *)outfilename,"w");
-      FILE *out = fopen((const char *)outfilename,"w");
-      if (out == NULL) {
-        printf("Error creating file %s\n", outfilename);
+        FILE *out = fopen((const char *)outfilename,"w");
+        if (out == NULL) {
+          printf("Error creating file %s\n", outfilename);
           return 1;
-      }
-      if (flat_div){
-        fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n", 0.5*(sections[0]+sections[1]), density[0],
-            SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
-            LconDensity[0], LcenConDensity[0], GconDensity[0], GcenConDensity[0], n0[0], nA[0]);
-      } else if (spherical_inner_wall) {
-        fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n", radius[0], 0.0,
-            SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
+        }
+        if (flat_div){
+          fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n", 0.5*(sections[0]+sections[1]), density[0],
+                  SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
+                  LconDensity[0], LcenConDensity[0], GconDensity[0], GcenConDensity[0], n0[0], nA[0]);
+        } else if (spherical_inner_wall) {
+          fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n", radius[0], 0.0,
+                  SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
                   LconDensity[0], LcenConDensity[0], GconDensity[0], GcenConDensity[0], n0[0], nA[0]);
           fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n", 0.5*(radius[0]+radius[1]), density[0],
                   SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
                   LconDensity[0], LcenConDensity[0], GconDensity[0], GcenConDensity[0], n0[0], nA[0]);
-      } else {
-        fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n" , 0.0, density[0],
-            SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
-            LconDensity[0], LcenConDensity[0], GconDensity[0], GcenConDensity[0], n0[0], nA[0]);
+        } else {
+          fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n" , 0.0, density[0],
+                  SconDensity[0], ScenConDensity[0], MconDensity[0], McenConDensity[0],
+                  LconDensity[0], LcenConDensity[0], GconDensity[0], GcenConDensity[0], n0[0], nA[0]);
         }
-
-      long divtoprint = div;
-      if (!spherical_outer_wall) divtoprint = div - 1;
-      if (!flat_div) {
-        for(long i=1; i<divtoprint; i++){
-          fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n",
-              0.5*(radius[i]+radius[i+1]), density[i],
-              SconDensity[i], ScenConDensity[i], MconDensity[i], McenConDensity[i],
-              LconDensity[i], LcenConDensity[i], GconDensity[i], GcenConDensity[i], n0[i], nA[i]);
+        
+        long divtoprint = div;
+        if (!spherical_outer_wall) divtoprint = div - 1;
+        if (!flat_div) {
+          for(long i=1; i<divtoprint; i++){
+            fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n",
+                    0.5*(radius[i]+radius[i+1]), density[i],
+                    SconDensity[i], ScenConDensity[i], MconDensity[i], McenConDensity[i],
+                    LconDensity[i], LcenConDensity[i], GconDensity[i], GcenConDensity[i], n0[i], nA[i]);
+          }
+        } else {
+          for(long i=1; i<div; i++){
+            fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n",
+                    0.5*(sections[i]+sections[i+1]), density[i],
+                    SconDensity[i], ScenConDensity[i], MconDensity[i], McenConDensity[i],
+                    LconDensity[i], LcenConDensity[i], GconDensity[i], GcenConDensity[i], n0[i], nA[i]);
+          }
         }
-      } else {
-        for(long i=1; i<div; i++){
-          fprintf(out, "%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n",
-              0.5*(sections[i]+sections[i+1]), density[i],
-              SconDensity[i], ScenConDensity[i], MconDensity[i], McenConDensity[i],
-              LconDensity[i], LcenConDensity[i], GconDensity[i], GcenConDensity[i], n0[i], nA[i]);
-        }
+        fflush(stdout);
+        fclose(out);
       }
-      
-      fflush(stdout);
-      fclose(out);
-      
-    
+      ///////////////////////////////////////////end of print.dat
     }
-    
-    
-    
-    ///////////////////////////////////////////end of print.dat
-      
-    }
+	
     // only write out the sphere positions after they've all had a
     // chance to move
     if (workingmoves%N == 0) {
@@ -904,8 +922,6 @@ Vector3d halfwayBetween(Vector3d w, Vector3d v, double oShell){
     if (distance(v,w+latx-laty-latz) < 2*oShell) return fixPeriodic((v+w+latx-laty-latz)/2);
     if (distance(v,w-latx-laty-latz) < 2*oShell) return fixPeriodic((v+w-latx-laty-latz)/2);
   }
-  printf("BUGHHHH@!:\n");
-  exit(1);
   return (w+v)/2;
 }
 
