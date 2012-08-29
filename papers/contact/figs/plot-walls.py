a9216fec0cec4889231fbf465c992ab062a873c8
jeffschulte83@gmail.com <jeffschulte@node09.quipu>
2012-08-29 11:26:34 -0700

    Got rid of a space in FMT and made plotting more uniform and pretty Tested everything on node09.

--- papers/contact/figs/plot-walls.py
+++ papers/contact/figs/plot-walls.py
@@ -40,8 +40,8 @@ n_plt.plot(dftdata[:,0]+offset,dftdata[:,1]*4*numpy.pi/3,"m--",label='$n$ DFT')
 #n_plt.plot(wbm2data[:,0]+offset,wbm2data[:,1]*4*numpy.pi/3,"m--",label='$n$ Mark II')
 #n_plt.plot(dftdata[:,0]+offset,n0*4*numpy.pi/3,"g--",label="$n_0$")
 #n_plt.plot(dftdata[:,0]+offset,nA*4*numpy.pi/3,"g-.",label="$n_A$")
-n_plt.plot(mcdata[:,0]+mcoffset,n0mc*4*numpy.pi/3,"m-",label="$n_0$ MC")
-n_plt.plot(mcdata[:,0]+mcoffset,nAmc*4*numpy.pi/3,"c-",label="$n_A$ MC")
+n_plt.plot(dftdata[:,0]+offset,n0*4*numpy.pi/3,"c-.",label="$n_0$")
+n_plt.plot(dftdata[:,0]+offset,nA*4*numpy.pi/3,"m--",label="$n_A$")
 n_plt.legend(loc=1, bbox_to_anchor=[1.0, 1.0], ncol=2).get_frame().set_alpha(0.5)
 n_plt.yaxis.set_major_locator(pylab.MaxNLocator(6,steps=[1,5,10],prune='upper'))
 pylab.ylim(ymin=0)
@@ -59,14 +59,14 @@ stop_here = int(dft_len - 1/dft_dr)
 print stop_here
 start_here = int(2.5/dft_dr)
 off = 1
-me = 30
+me = 55
 
 A_plt = pylab.subplot(3,1,1)
 A_plt.axvline(x=0, color='k', linestyle=':')
 A_plt.plot(mcdata[:,0]+mcoffset,mcdata[:,2+2*off]/nAmc,"r-",label="$g_\sigma^A$ MC")
-A_plt.plot(dftdata[start_here:stop_here,0]+offset,dftdata[start_here:stop_here,5],"r+-",markevery=me,label="$g_\sigma^A$ (White Bear)")
-A_plt.plot(wbm2data[start_here:stop_here,0]+offset,wbm2data[start_here:stop_here,5],"rx-",markevery=me,label="$g_\sigma^A$ (mark II)",
-           markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
+A_plt.plot(dftdata[start_here:stop_here,0]+offset,dftdata[start_here:stop_here,5],"ro-",markevery=me,label="$g_\sigma^A$ (White Bear)")
+#A_plt.plot(wbm2data[start_here:stop_here,0]+offset,wbm2data[start_here:stop_here,5],"rx-",markevery=me,label="$g_\sigma^A$ (mark II)",
+#           markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
 A_plt.plot(dftdata[:,0]+offset,dftdata[:,7],"rx--",markevery=me,label="Gross",
            markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
 A_plt.legend(loc='lower right', ncol=1).get_frame().set_alpha(0.5)
@@ -82,9 +82,9 @@ S_plt = pylab.subplot(3,1,2)
 S_plt.axvline(x=0, color='k', linestyle=':')
 S_plt.plot(mcdata[:,0]+mcoffset,mcdata[:,3+2*off]/n0mc,"g-",label="$g_\sigma^S$ MC")
 S_plt.plot(dftdata[start_here:stop_here,0]+offset,dftdata[start_here:stop_here,3],
-           "g+--",markevery=me,label="$g_\sigma^S$ (White Bear)")
-S_plt.plot(wbm2data[start_here:stop_here,0]+offset,wbm2data[start_here:stop_here,3],"g--",markevery=me,label="$g_\sigma^S$ (mark II)",
-           markerfacecolor='none',markeredgecolor='green', markeredgewidth=1)
+           "go--",markevery=me,label="$g_\sigma^S$ (White Bear)")
+#S_plt.plot(wbm2data[start_here:stop_here,0]+offset,wbm2data[start_here:stop_here,3],"g--",markevery=me,label="$g_\sigma^S$ (mark II)",
+#           markerfacecolor='none',markeredgecolor='green', markeredgewidth=1)
 S_plt.plot(dftdata[:,0]+offset,dftdata[:,4],"gx--",markevery=me,label="Yu and Wu")
 S_plt.legend(loc='lower right', ncol=1).get_frame().set_alpha(0.5)
 S_plt.yaxis.set_major_locator(pylab.MaxNLocator(5,integer=True,prune='upper'))
