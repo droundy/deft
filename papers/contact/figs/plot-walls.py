f2cbdd99828c674830b31e46bda12d3f206efc4f
David Roundy <roundyd@physics.oregonstate.edu>
2012-08-29 17:02:55 -0700

    improve figure captions and change to diameter units (partly) Tests skipped!

--- papers/contact/figs/plot-walls.py
+++ papers/contact/figs/plot-walls.py
@@ -2,6 +2,7 @@
 
 # We need the following two lines in order for matplotlib to work
 # without access to an X server.
+from __future__ import division
 import matplotlib
 matplotlib.use('Agg')
 
@@ -22,8 +23,8 @@ dft_dr = dftdata[2,0] - dftdata[1,0]
 mcdata = numpy.insert(mcdata,0,0,0)
 mcdata[0,0]=-10
 
-mcoffset = 10
-offset = -3
+mcoffset = 10/2
+offset = -3/2
 n0 = dftdata[:,6]
 nA = dftdata[:,8]
 nAmc = mcdata[:,11]
@@ -34,19 +35,19 @@ pylab.subplots_adjust(hspace=0.001)
 
 n_plt = pylab.subplot(3,1,3)
 #n_plt.axvline(x=3, color='k', linestyle=':')
-n_plt.plot(mcdata[:,0]+mcoffset,mcdata[:,1]*4*numpy.pi/3,"b-",label='$n$ MC')
-n_plt.plot(dftdata[:,0]+offset,dftdata[:,1]*4*numpy.pi/3,"m--",label='$n$ DFT')
-#n_plt.plot(wbtdata[:,0]+offset,wbtdata[:,1]*4*numpy.pi/3,"c-.",label='$n$ WBT')
-#n_plt.plot(wbm2data[:,0]+offset,wbm2data[:,1]*4*numpy.pi/3,"m--",label='$n$ Mark II')
-#n_plt.plot(dftdata[:,0]+offset,n0*4*numpy.pi/3,"g--",label="$n_0$")
-#n_plt.plot(dftdata[:,0]+offset,nA*4*numpy.pi/3,"g-.",label="$n_A$")
-n_plt.plot(dftdata[:,0]+offset,n0*4*numpy.pi/3,"c-.",label="$n_0$")
-n_plt.plot(dftdata[:,0]+offset,nA*4*numpy.pi/3,"m--",label="$n_A$")
+n_plt.plot(mcdata[:,0]/2+mcoffset,mcdata[:,1]*4*numpy.pi/3,"b-",label='$n$ MC')
+n_plt.plot(dftdata[:,0]/2+offset,dftdata[:,1]*4*numpy.pi/3,"m--",label='$n$ DFT')
+#n_plt.plot(wbtdata[:,0]/2+offset,wbtdata[:,1]*4*numpy.pi/3,"c-.",label='$n$ WBT')
+#n_plt.plot(wbm2data[:,0]/2+offset,wbm2data[:,1]*4*numpy.pi/3,"m--",label='$n$ Mark II')
+#n_plt.plot(dftdata[:,0]/2+offset,n0*4*numpy.pi/3,"g--",label="$n_0$")
+#n_plt.plot(dftdata[:,0]/2+offset,nA*4*numpy.pi/3,"g-.",label="$n_A$")
+n_plt.plot(dftdata[:,0]/2+offset,n0*4*numpy.pi/3,"c-.",label="$n_0$")
+n_plt.plot(dftdata[:,0]/2+offset,nA*4*numpy.pi/3,"m--",label="$n_A$")
 n_plt.legend(loc=1, bbox_to_anchor=[1.0, 1.0], ncol=2).get_frame().set_alpha(0.5)
 n_plt.yaxis.set_major_locator(pylab.MaxNLocator(6,steps=[1,5,10],prune='upper'))
 pylab.ylim(ymin=0)
-pylab.xlim(-1,9)
-pylab.xlabel("$r/R$")
+pylab.xlim(-1/2,9/2)
+pylab.xlabel("$r/\sigma$")
 pylab.ylabel("filling fraction")
 n_plt.axvline(x=0, color='k', linestyle=':')
 
@@ -63,32 +64,32 @@ me = 55
 
 A_plt = pylab.subplot(3,1,1)
 A_plt.axvline(x=0, color='k', linestyle=':')
-A_plt.plot(mcdata[:,0]+mcoffset,mcdata[:,2+2*off]/nAmc,"r-",label="$g_\sigma^A$ MC")
-A_plt.plot(dftdata[start_here:stop_here,0]+offset,dftdata[start_here:stop_here,5],"ro-",markevery=me,label="$g_\sigma^A$ (White Bear)")
-#A_plt.plot(wbm2data[start_here:stop_here,0]+offset,wbm2data[start_here:stop_here,5],"rx-",markevery=me,label="$g_\sigma^A$ (mark II)",
+A_plt.plot(mcdata[:,0]/2+mcoffset,mcdata[:,2+2*off]/nAmc,"r-",label="$g_\sigma^A$ MC")
+A_plt.plot(dftdata[start_here:stop_here,0]/2+offset,dftdata[start_here:stop_here,5],"ro-",markevery=me,label="$g_\sigma^A$ (White Bear)")
+#A_plt.plot(wbm2data[start_here:stop_here,0]/2+offset,wbm2data[start_here:stop_here,5],"rx-",markevery=me,label="$g_\sigma^A$ (mark II)",
 #           markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
-A_plt.plot(dftdata[:,0]+offset,dftdata[:,7],"rx--",markevery=me,label="Gross",
+A_plt.plot(dftdata[:,0]/2+offset,dftdata[:,7],"rx--",markevery=me,label="Gross",
            markerfacecolor='none',markeredgecolor='red', markeredgewidth=1)
 A_plt.legend(loc='lower right', ncol=1).get_frame().set_alpha(0.5)
 A_plt.yaxis.set_major_locator(pylab.MaxNLocator(integer=True,prune='upper'))
 pylab.ylim(ymin=0)
 #pylab.ylim(ymax=(dftdata[20,5]+1))
 pylab.ylabel("$g^A$")
-pylab.xlim(-1,9)
+pylab.xlim(-1/2,9/2)
 
 n0mc[0]=1
 mcdata[0,10]=1
 S_plt = pylab.subplot(3,1,2)
 S_plt.axvline(x=0, color='k', linestyle=':')
-S_plt.plot(mcdata[:,0]+mcoffset,mcdata[:,3+2*off]/n0mc,"g-",label="$g_\sigma^S$ MC")
-S_plt.plot(dftdata[start_here:stop_here,0]+offset,dftdata[start_here:stop_here,3],
+S_plt.plot(mcdata[:,0]/2+mcoffset,mcdata[:,3+2*off]/n0mc,"g-",label="$g_\sigma^S$ MC")
+S_plt.plot(dftdata[start_here:stop_here,0]/2+offset,dftdata[start_here:stop_here,3],
            "go--",markevery=me,label="$g_\sigma^S$ (White Bear)")
-#S_plt.plot(wbm2data[start_here:stop_here,0]+offset,wbm2data[start_here:stop_here,3],"g--",markevery=me,label="$g_\sigma^S$ (mark II)",
+#S_plt.plot(wbm2data[start_here:stop_here,0]/2+offset,wbm2data[start_here:stop_here,3],"g--",markevery=me,label="$g_\sigma^S$ (mark II)",
 #           markerfacecolor='none',markeredgecolor='green', markeredgewidth=1)
-S_plt.plot(dftdata[:,0]+offset,dftdata[:,4],"gx--",markevery=me,label="Yu and Wu")
+S_plt.plot(dftdata[:,0]/2+offset,dftdata[:,4],"gx--",markevery=me,label="Yu and Wu")
 S_plt.legend(loc='lower right', ncol=1).get_frame().set_alpha(0.5)
 S_plt.yaxis.set_major_locator(pylab.MaxNLocator(5,integer=True,prune='upper'))
-pylab.xlim(-1,9)
+pylab.xlim(-1/2,9/2)
 pylab.ylim(ymin=0)
 pylab.ylabel("$g^S$")
 
