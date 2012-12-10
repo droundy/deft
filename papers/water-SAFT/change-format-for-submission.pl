3a4a783a0638f7370d5f86302992573572eb0052
David Roundy <roundyd@physics.oregonstate.edu>
2012-12-10 13:17:07 -0800

    automagically generate paper for submission in required format Tests skipped!

---
+++ papers/water-SAFT/change-format-for-submission.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+
+open(SUBMIT, ">submit/paper.tex") or die "couldn't make submission paper $!";
+
+open(PAPER, "paper.tex") or die "oops $!";
+my $aminfigure = 0;
+while (<PAPER>) {
+  s/twocolumn/preprint/;
+  if ($aminfigure) {
+    if (/end{figure}/) {
+      $aminfigure = 0;
+    }
+  } else {
+    if (/begin{figure}/) {
+      $aminfigure = 1;
+    } elsif (/end{document}/) {
+      break;
+    } else {
+      print SUBMIT $_;
+    }
+  }
+}
+close(PAPER);
+
+open(PAPER, "paper.tex") or die "oops $!";
+$aminfigure = 0;
+while (<PAPER>) {
+  s/twocolumn/preprint/;
+  if ($aminfigure) {
+    if (/end{figure}/) {
+      $aminfigure = 0;
+    }
+    print SUBMIT $_;
+  } else {
+    if (/begin{figure}/) {
+      $aminfigure = 1;
+      print SUBMIT $_;
+    }
+  }
+}
+close(PAPER);
+
+print SUBMIT "\\end{document}\n";
