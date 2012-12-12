2fffbdfb29a584b22e233d974a6269be32552071
David Roundy <roundyd@physics.oregonstate.edu>
2012-12-12 12:29:07 -0800

    more changes for resubmission of contact Tested everything on kipu.

--- papers/contact/change-format-for-submission.pl
+++ papers/contact/change-format-for-submission.pl
@@ -4,41 +4,12 @@
 open(SUBMIT, ">submit/paper.tex") or die "couldn't make submission paper $!";
 
 open(PAPER, "paper.tex") or die "oops $!";
-my $aminfigure = 0;
 while (<PAPER>) {
-  s/twocolumn/preprint/;
-  if ($aminfigure) {
-    if (/end{figure}/ or /end{figure\*/) {
-      $aminfigure = 0;
-    }
-  } else {
-    if (/begin{figure}/ or /begin{figure\*/) {
-      $aminfigure = 1;
-    } elsif (/end{document}/) {
-      break;
-    } else {
-      print SUBMIT $_;
-    }
-  }
-}
-close(PAPER);
-
-open(PAPER, "paper.tex") or die "oops $!";
-$aminfigure = 0;
-while (<PAPER>) {
-  s/twocolumn/preprint/;
-  if ($aminfigure) {
-    if (/end{figure}/ or /end{figure\*/) {
-      $aminfigure = 0;
-    }
-    print SUBMIT $_;
-  } else {
-    if (/begin{figure}/ or /begin{figure\*/) {
-      $aminfigure = 1;
-      print SUBMIT $_;
-    }
-  }
+  #s/twocolumn/preprint/;
+  s/figs\///;
+  print SUBMIT $_;
 }
 close(PAPER);
 
 print SUBMIT "\\end{document}\n";
+close(SUBMIT);
