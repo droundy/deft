dd1a68fe8795653b38fc84eb5bdfc12743018484
Paho Lurie-Gregg <paho@paho-deb.hsd1.or.comcast.net>
2013-02-20 06:44:28 -0800

    removed temporary pair-monte-carlo-scripts and replaced with one to batch all runs

--- pair-mc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./pair-monte-carlo 589 500000000000 .01 papers/pair-correlation/figs/mc/wallsMC-pair-0.30 periodxy 20 wallz 20 flatdiv
-
-#./pair-monte-carlo 5 50000 .01 papers/pair-correlation/figs/mc/wallsMC-pair-0.30 periodxy 20 wallz 20 flatdiv
