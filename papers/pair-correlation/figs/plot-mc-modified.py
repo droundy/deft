ce34f2e20d68106f14fdad66c88361a6821603a0
Paho Lurie-Gregg <paho@paholg.com>
2013-06-25 09:37:13 -0700

    Fixed a bug in a couple plotting scripts in pair-correlation. Tests skipped

--- papers/pair-correlation/figs/plot-mc-modified.py
+++ papers/pair-correlation/figs/plot-mc-modified.py
@@ -11,10 +11,13 @@ from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.widgets import Slider, RadioButtons
 #from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+colors = ['k', 'b', 'r', 'g']
+plots = ['mc', 'this-work', 'fischer', 'gross']
+numplots = 4
 z0 = 0.95
 theta = numpy.pi/2
 ff = 0.3
-
+dx = 0.1
 if len(sys.argv) != 4:
     print("Usage:  " + sys.argv[0] + " ff z0 theta/pi")
     exit(1)
@@ -28,17 +31,22 @@ def plot1d():
   global g2, ax
   zvals = len(g2[0][0,:])
   rvals = len(g2[0][:,0])
-  if (abs(theta) < numpy.arctan(1/2)):
-    z1, r1 = (zmax-zmax/zvals), (zmax-zmax/zvals)*numpy.tan(theta)
+  if theta < numpy.arctan(1/2):
+    z1 = zmax
+    r1 = (z1-z0)*numpy.tan(theta)
+  elif theta < numpy.pi - numpy.arctan(1/2):
+    r1 = rmax
+    z1 = r1/numpy.tan(theta) + z0
   else:
-    z1, r1 = (rmax-rmax/rvals)/numpy.tan(theta), (rmax-rmax/rvals)
-
+    z1 = -zmax
+    r1 = (z1-z0)*numpy.tan(theta)
   rlen = numpy.sqrt((z1-z0)**2 + r1**2)
   z0coord = zvals*(z0-0.05)/zmax
   z1coord = zvals*(z1)/zmax
   r0coord = 0
   r1coord = rvals*(r1)//rmax
   y, x = numpy.linspace(z0coord, z1coord, num), numpy.linspace(r0coord, r1coord, num)
+  ax[4].set_title('$z_0 = %g$, $ff = %g$, $\\theta = %g$' %(z0, ff, theta))
 
   i = 0
   while i < numplots:
@@ -47,24 +55,20 @@ def plot1d():
     gslice[i].set_data(numpy.linspace(0, rlen, num), zi)
     i += 1
 
-colors = ['k', 'b', 'r', 'g']
-plots = ['mc', 'this-work', 'fischer', 'gross']
-numplots = 4
-
 def plot():
   global ax, CS
-  i=0
-  ax[i].collections = []
-  g2[i] = read_walls(ff, z0, plots[i])
-  CS = ax[i].contourf(Z, R, g2[i], levels, cmap=cmap, extend='both')
-  CS2 = ax[i].contourf(Z, -R, g2[i], levels, cmap=cmap, extend='both')
-  CS.cmap.set_over('k')
-  CS2.cmap.set_over('k')
-  ax[i].set_title('%s, $z_0 = %g$, $ff = %g$ $theta = %g\pi$' %(plots[i], z0, ff, theta/numpy.pi))
-  i = 1
+  i = 0
   while i < numplots:
     ax[i].collections = []
     g2[i] = read_walls(ff, z0, plots[i])
+
+    rmax = len(g2[i][:,0])*dx
+    zmax = len(g2[i][0,:])*dx
+
+    r = numpy.arange(0, rmax, dx)
+    z = numpy.arange(0, zmax, dx)
+    Z, R = numpy.meshgrid(z, r)
+
     CS = ax[i].contourf(Z, R, g2[i], levels, cmap=cmap, extend='both')
     CS2 = ax[i].contourf(Z, -R, g2[i], levels, cmap=cmap, extend='both')
     CS.cmap.set_over('k')
@@ -73,10 +77,6 @@ def plot():
     i += 1
   plot1d()
   pylab.draw()
-
-
-zmax = 20
-rmax = 10
 
 def read_walls(ff, z0, fun):
   if fun == 'mc':
@@ -118,13 +118,13 @@ levels = numpy.linspace(0, 4, 49)
 g2 = [0]*numplots
 g2[0] = read_walls(ff, z0, 'mc')
 
-zbins = len(g2[0][0,:])
-rbins = len(g2[0][:,0])
-dr = rmax/rbins
-dz = zmax/zbins
+rmax = len(g2[0][:,0])*dx
+zmax = len(g2[0][0,:])*dx
 
-r = numpy.arange(0, rmax, dr)
-z = numpy.arange(0, zmax, dz)
+print rmax, zmax
+
+r = numpy.arange(0, rmax, dx)
+z = numpy.arange(0, zmax, dx)
 Z, R = numpy.meshgrid(z, r)
 
 #fig = pylab.figure(1)
@@ -147,6 +147,8 @@ ax[4] = pylab.axes([left+hspace+width, top-height, width*2+hspace, height])
 i = 0
 while i < numplots:
   ax[i].set_aspect('equal')
+  ax[i].set_xlim(0, 20)
+  ax[i].set_ylim(-10,10)
   i += 1
 
 num = 1000
