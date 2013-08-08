7605b362b21fb9995d053c02542a24c7230cf6ea
Paho Lurie-Gregg <paho@paholg.com>
2013-08-07 19:59:28 -0700

    Added Sokolowski to various plotting scripts in pair-correlation. Added plot-path-and-2d-alternate-axis.py that uses x and z for the x-axis, instead of path length. Some other plotting script modifications. Tested everything on paho-deb.

--- papers/pair-correlation/figs/plot-path-and-2d-alternate-axis.py
+++ papers/pair-correlation/figs/plot-path-and-2d-alternate-axis.py
@@ -4,8 +4,8 @@ from __future__ import division
 import matplotlib, sys
 if len(sys.argv) < 3 or sys.argv[2] != "show":
   matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import pylab, numpy, scipy.ndimage
+from pylab import *
+import scipy.ndimage
 import os.path
 import math
 
@@ -51,10 +51,12 @@ def read_walls_path(ff,z0,fun):
   if (os.path.isfile(filename) == False):
     # Just use walls data if we do not have the MC (need to be careful!)
     filename = "figs/walls/wallsWB-path-%s-pair-%1.2f-%1.2f.dat" %('this-work', ff, z0)
-  data=numpy.loadtxt(filename)
+  data = loadtxt(filename)
   if fun == 'mc':
     data[:,0]-=4.995
-  return data[:,0:2]
+  else:
+    data[:,2]-=3
+  return data[:,0:4]
 
 def read_walls(ff, z0, fun):
   if fun == 'mc':
@@ -68,21 +70,36 @@ def read_walls(ff, z0, fun):
     # Just use walls data if we do not have the MC (need to be careful!)
     filename = "figs/walls/wallsWB-%s-pair-%1.2f-%1.2f.dat" %('this-work', ff, z0)
     pylab.title("Using this work instead of MC!")
-  data = numpy.loadtxt(filename)
+  data = loadtxt(filename)
   return data
 
-# First plot
+ymax = 4
 
-pylab.figure(figsize=(10,5))
+fig = figure(figsize=(10,5))
 
-ax = pylab.subplot(1,2,2)
+xplot = fig.add_subplot(1,2,2)
+zplot = xplot.twiny()
+#zplot = fig.add_subplot(1,3,3, sharey=xplot)
+twod_plot = fig.add_subplot(1,2,1)
+
+xplot.set_xlim(6, -4)
+xplot.set_xticks([6, 4, 2, 0])
+zplot.set_xlim(-4, 6)
+zplot.set_xticks([0, 2, 4, 6])
+xplot.set_ylim(0, 3.5)
+
+twod_plot.set_xlim(-0.5, 1.5*ymax)
+twod_plot.set_ylim(-ymax, ymax)
+
+fig.subplots_adjust(hspace=0.001)
+
 for i in range(len(plots)):
     g2_path = read_walls_path(ff, z0, plots[i])
     if able_to_read_file == False:
-        matplotlib.pyplot.plot(numpy.arange(0,10,1), [0]*10, 'k')
-        matplotlib.pyplot.suptitle('!!!!WARNING!!!!! There is data missing from this plot!', fontsize=25)
+        plot(numpy.arange(0,10,1), [0]*10, 'k')
+        suptitle('!!!!WARNING!!!!! There is data missing from this plot!', fontsize=25)
         savedfilename = "figs/pair-correlation-path-" + str(int(ff*10)) + ".pdf"
-        pylab.savefig(savedfilename)
+        savefig(savedfilename)
         exit(0)
     # FIXME:  once we have sufficient data, we will want to remove this smoothing...
     old = 1.0*g2_path[:,1]
@@ -90,78 +107,96 @@ for i in range(len(plots)):
     # averaged[1:] += old[:len(averaged)-1]
     # averaged[:len(averaged)-1] += old[1:]
     # averaged /= 3;
-    pylab.plot(g2_path[:,0],averaged, label=titles[i], color=colors[i])
-    #pylab.plot(g2_path[:,0],g2_path[:,1], label=titles[i], color=colors[i])
+
+    # Find x-portion:
+    z_beg = g2_path[0,2]
+    coord = 0
+    z_final = z_beg
+    while z_final == z_beg:
+      coord += 1
+      z_final = g2_path[coord,2]
+
+    xplot.plot(g2_path[:coord,3],averaged[:coord], label=titles[i], color=colors[i])
+    zplot.plot(g2_path[coord-1:,2],averaged[coord-1:], label=titles[i], color=colors[i])
 
 g2nice = read_walls_path(ff, z0, 'this-work')
-def g2pathfunction(x):
-    return numpy.interp(x, g2nice[:,0], g2nice[:,1])
+p = 90
+
+def g2pathfunction_x(x):
+    return interp(x, flipud(g2nice[:,3]), flipud(g2nice[:,1]))
+def g2pathfunction_z(z):
+    return interp(z, g2nice[:,2], g2nice[:,1])
 
 rA = 3.9
 rE = 4.0
 rpath = 2.005
-xBoff = 10 - rpath
-xDoff = 10 - rpath + numpy.pi*rpath/2
-xAoff = xBoff - rA + rpath
-xEoff = xDoff + rE - rpath
-pylab.axvline(xBoff, color='k')
-pylab.axvline(xDoff, color='k')
+xAoff = 3.8
+xBoff = 2.0
+zCoff = 1.0
+zDoff = 2.0
+zEoff = 3.8
+
+zplot.axvline(x=2, color='k')
+zplot.axvline(x=0, color='k')
 
 hw = 4 # headwidth of arrows
 
-pylab.annotate('$A$', xy=(xAoff, g2pathfunction(xAoff)), xytext=(xAoff-1,1.3),
+xplot.annotate('$A$', xy=(xAoff, g2pathfunction_x(xAoff)),
+               xytext=(xAoff+1,1.3),
                arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$B$', xy=(xBoff,g2pathfunction(xBoff)), xytext=(xBoff-1,g2pathfunction(xBoff)-0.2),
+xplot.annotate('$B$', xy=(xBoff,g2pathfunction_x(xBoff)),
+               xytext=(xBoff+1, g2pathfunction_x(xBoff)-0.2),
                arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$C$', xy=(0.5*(xBoff+xDoff),g2pathfunction(0.5*(xBoff+xDoff))),
-               xytext=(0.5*(xBoff+xDoff)+0.2,g2pathfunction(0.5*(xBoff+xDoff))-0.5),
+zplot.annotate('$C$', xy=(zCoff,g2pathfunction_z(zCoff)),
+               xytext=(zCoff,g2pathfunction_z(zCoff)-0.5),
                arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$D$', xy=(xDoff,g2pathfunction(xDoff)), xytext=(xDoff+1,g2pathfunction(xDoff)-0.2),
+zplot.annotate('$D$', xy=(zDoff,g2pathfunction_z(zDoff)),
+               xytext=(zDoff+1,g2pathfunction_z(zDoff)-0.2),
                arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$E$', xy=(xEoff,g2pathfunction(xEoff)), xytext=(xEoff+0.7,1.3),
+zplot.annotate('$E$', xy=(zEoff,g2pathfunction_z(zEoff)),
+               xytext=(zEoff+0.7,1.3),
                arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
 
 
-ax.set_xticks((xBoff - 4,
-               xBoff - 3,
-               xBoff - 2,
-               xBoff - 1,
-               xBoff,
-               xDoff,
-               xDoff + 1,
-               xDoff + 2,
-               xDoff + 3,
-               xDoff + 4))
-ax.set_xticklabels((6, 5, 4, 3, 2, 2, 3, 4, 5, 6))
+# ax.set_xticks((xBoff - 4,
+#                xBoff - 3,
+#                xBoff - 2,
+#                xBoff - 1,
+#                xBoff,
+#                xDoff,
+#                xDoff + 1,
+#                xDoff + 2,
+#                xDoff + 3,
+#                xDoff + 4))
+# ax.set_xticklabels((6, 5, 4, 3, 2, 2, 3, 4, 5, 6))
 
-pylab.ylim(0)
-pylab.xlim(xAoff - rpath,xEoff + rpath)
-pylab.xlabel('$|\mathbf{r}_{12}|$')
-pylab.ylabel(r'$g^{(2)}(\left< 0,0,0\right>,\mathbf{r}_2)$')
-pylab.legend(loc='best')
+# pylab.ylim(0)
+# pylab.xlim(xAoff - rpath,xEoff + rpath)
+xlabel('$|\mathbf{r}_{12}|$')
+ylabel(r'$g^{(2)}(\left< 0,0,0\right>,\mathbf{r}_2)$')
+zplot.legend(loc='best')
 
-pylab.subplot(1,2,1).set_aspect('equal')
+twod_plot.set_aspect('equal')
 g2mc = read_walls(ff, z0, 'mc')
 rbins = round(2*rmax/dx)
 zposbins = round(zmax/dx)
 znegbins = round(-zmin/dx)
 zbins = zposbins + znegbins
-g22 = numpy.zeros((rbins, zbins))
+g22 = zeros((rbins, zbins))
 g22[rbins/2:rbins, znegbins:zbins] = g2mc[:rbins/2,:zposbins]
-g22[:rbins/2, znegbins:zbins] = numpy.flipud(g2mc[:rbins/2,:zposbins])
+g22[:rbins/2, znegbins:zbins] = flipud(g2mc[:rbins/2,:zposbins])
 g2mc = g22
 gmax = g2mc.max()
 dx = 0.1
 
-r = numpy.arange(0-rmax, rmax, dx)
-z = numpy.arange(zmin, zmax, dx)
-Z, R = numpy.meshgrid(z, r)
+r = arange(0-rmax, rmax, dx)
+z = arange(zmin, zmax, dx)
+Z, R = meshgrid(z, r)
 
-levels = numpy.linspace(0, gmax, gmax*100)
+levels = linspace(0, gmax, gmax*100)
 xlo = 0.85/gmax
 xhi = 1.15/gmax
 xhier = (1 + xhi)/2.0
-ymax = 4
 
 cdict = {'red':   [(0.0,  0.0, 0.0),
                    (xlo,  1.0, 1.0),
@@ -185,35 +220,34 @@ cdict = {'red':   [(0.0,  0.0, 0.0),
                    (1.0,  0.0, 0.0)]}
 cmap = matplotlib.colors.LinearSegmentedColormap('mine', cdict)
 
-CS = pylab.pcolor(Z, R, g2mc, vmax=gmax, vmin=0, cmap=cmap)
+CS = pcolor(Z, R, g2mc, vmax=gmax, vmin=0, cmap=cmap)
 
-myticks = numpy.arange(0, numpy.floor(2.0*gmax)/2 + 0.1, 0.5)
-pylab.colorbar(CS, extend='neither', ticks=myticks)
-pylab.ylabel('$x_2$');
-pylab.xlabel('$z_2$');
+myticks = arange(0, floor(2.0*gmax)/2 + 0.1, 0.5)
+colorbar(CS, extend='neither', ticks=myticks)
+ylabel('$x_2$');
+xlabel('$z_2$');
 
 xs = [0, 0]
 ys = [ymax, rpath]
-dtheta = pylab.pi/80
-for theta in pylab.arange(0, pylab.pi/2 + dtheta/2, dtheta):
-    xs.append(rpath*pylab.sin(theta))
-    ys.append(rpath*pylab.cos(theta))
+dtheta = pi/80
+for theta in arange(0, pi/2 + dtheta/2, dtheta):
+    xs.append(rpath*sin(theta))
+    ys.append(rpath*cos(theta))
 xs.append(2*ymax)
 
 ys.append(0)
-pylab.plot(xs, ys, 'w-', linewidth=2)
-pylab.plot(xs, ys, 'k--', linewidth=2)
+plot(xs, ys, 'w-', linewidth=2)
+plot(xs, ys, 'k--', linewidth=2)
 
-pylab.annotate('$A$', xy=(0,rA), xytext=(1,3), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$B$', xy=(0,rpath), xytext=(1,2.5), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$C$', xy=(rpath/pylab.sqrt(2.0),rpath/pylab.sqrt(2.0)), xytext=(2.3,2.0), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$D$', xy=(rpath,0), xytext=(3,1), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
-pylab.annotate('$E$', xy=(rE,0), xytext=(5,1), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
+annotate('$A$', xy=(0,rA), xytext=(1,3), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
+annotate('$B$', xy=(0,rpath), xytext=(1,2.5), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
+annotate('$C$', xy=(rpath/sqrt(2.0),rpath/sqrt(2.0)), xytext=(2.3,2.0), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
+annotate('$D$', xy=(rpath,0), xytext=(3,1), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
+annotate('$E$', xy=(rE,0), xytext=(5,1), arrowprops=dict(shrink=0.01, width=1, headwidth=hw))
 
-pylab.xlim(-0.5, 1.5*ymax)
-pylab.ylim(-ymax, ymax)
-
-pylab.title(r'$g^{(2)}(\left< 0,0,0\right>, \left<x_2, 0, z_2\right>)$ at $\eta = %g$' % ff)
-pylab.savefig("figs/pair-correlation-pretty-%d.pdf" % (int(ff*10)))
-pylab.show()
+xplot.set_xlabel('$x$')
+zplot.set_xlabel('$z$')
+title(r'$g^{(2)}(\left< 0,0,0\right>, \left<x_2, 0, z_2\right>)$ at $\eta = %g$' % ff)
+savefig("figs/pair-correlation-pretty-alt-%d.pdf" % (int(ff*10)))
+show()
 
