00528a2e1477b00da078ddf254cc8cd1bcdfee23
Paho Lurie-Gregg <paho@paholg.com>
2013-09-05 18:37:37 -0700

    Cleaned up vector3d.h. Added random struct and rotation class. Tested on paho-deb. Took 0:20.47.

--- src/vector3d.h
+++ src/vector3d.h
@@ -1,185 +1,301 @@
 #include <math.h>
+#include "MersenneTwister.h"
 
 #pragma once
 
+struct random {
+  static void seed(unsigned long seedval) { my_mtrand = MTRand(seedval); }
+  static double ran() {return my_mtrand.randExc(); }
+private:
+  static MTRand my_mtrand;
+};
+
 class vector3d {
  public:
+  double x;
+  double y;
+  double z;
+
   vector3d() {
-    coords[0] = coords[1] = coords[2] = 0; }
-  vector3d(const double x, const double y, const double z) {
-    coords[0] = x; coords[1] = y; coords[2] = z; }
+    x = y = z = 0; }
+  vector3d(const double newx, const double newy, const double newz) {
+    x = newx; y = newy; z = newz; }
   vector3d(const vector3d &v) {
-    coords[0] = v.coords[0]; coords[1] = v.coords[1]; coords[2] = v.coords[2];}
-
-  double x() const { return coords[0]; }
-  double y() const { return coords[1]; }
-  double z() const { return coords[2]; }
-
-  double x(const double newx) { coords[0] = newx; return coords[0]; }
-  double y(const double newy) { coords[1] = newy; return coords[1]; }
-  double z(const double newz) { coords[2] = newz; return coords[2]; }
+    x = v.x; y = v.y; z = v.z;}
 
   vector3d operator=(const vector3d &v) {
-    coords[0] = v.coords[0]; coords[1] = v.coords[1]; coords[2] = v.coords[2];
+    x = v.x; y = v.y; z = v.z;
     return *this; }
 
   vector3d operator+(const vector3d &v) const {
-    return vector3d(coords[0]+v.coords[0], coords[1]+v.coords[1], coords[2]+v.coords[2]); }
+    return vector3d(x+v.x, y+v.y, z+v.z); }
   vector3d operator+=(const vector3d &v) {
-    coords[0] += v.coords[0]; coords[1] += v.coords[1]; coords[2] += v.coords[2];
+    x += v.x; y += v.y; z += v.z;
     return *this; }
 
   vector3d operator-(const vector3d &v) const {
-    return vector3d(coords[0]-v.coords[0], coords[1]-v.coords[1], coords[2]-v.coords[2]); }
+    return vector3d(x-v.x, y-v.y, z-v.z); }
   vector3d operator-=(const vector3d &v) {
-    coords[0] -= v.coords[0]; coords[1] -= v.coords[1]; coords[2] -= v.coords[2];
+    x -= v.x; y -= v.y; z -= v.z;
     return *this; }
 
   vector3d operator*(const double scalar) const {
-    return vector3d(scalar*coords[0], scalar*coords[1], scalar*coords[2]); }
+    return vector3d(scalar*x, scalar*y, scalar*z); }
   vector3d operator*=(const double scalar) {
-    coords[0] *= scalar; coords[1] *= scalar; coords[2] *= scalar; return *this; }
+    x *= scalar; y *= scalar; z *= scalar;
+    return *this; }
 
   vector3d operator/(const double scalar) const {
-    return vector3d(coords[0]/scalar, coords[1]/scalar, coords[2]/scalar); }
+    return vector3d(x/scalar, y/scalar, z/scalar); }
   vector3d operator/=(const double scalar) {
-    coords[0] /= scalar; coords[1] /= scalar; coords[2] /= scalar; return *this; }
+    x /= scalar; y /= scalar; z /= scalar;
+    return *this; }
 
   bool operator ==(const vector3d &v) const {
-    return ((coords[0] == v.coords[0]) && (coords[1] == v.coords[1]) &&
-            (coords[2] == v.coords[2])); }
+    return ((x == v.x) && (y == v.y) &&
+            (z == v.z)); }
   bool operator !=(const vector3d &v) const {
     return !(*this == v); }
 
-  double &operator[](const unsigned int i) { return coords[i]; }
-  const double operator[](const unsigned int i) const { return coords[i]; }
+  double &operator[](const unsigned int i) {
+    switch(i) {
+    case 0: return x;
+    case 1: return y;
+    case 2: return z;
+    }
+  }
+  const double operator[](const unsigned int i) const {
+    switch(i) {
+    case 0: return x;
+    case 1: return y;
+    case 2: return z;
+    }
+  }
 
   double dot(const vector3d &v) const {
-    return coords[0]*v.coords[0] + coords[1]*v.coords[1] + coords[2]*v.coords[2]; }
+    return x*v.x + y*v.y + z*v.z; }
   vector3d cross(const vector3d &v) const {
-    return vector3d(coords[1]*v.coords[2] - coords[2]*v.coords[1],
-                   coords[0]*v.coords[2] - coords[2]*v.coords[0],
-                   coords[0]*v.coords[1] - coords[1]*v.coords[2]); }
+    return vector3d(y*v.z - z*v.y, x*v.z - z*v.x, x*v.y - y*v.z); }
 
   double norm() const {
-    return sqrt(coords[0]*coords[0] + coords[1]*coords[1] + coords[2]*coords[2]); }
+    return sqrt(x*x + y*y + z*z); }
   double normsquared() const {
-    return coords[0]*coords[0] + coords[1]*coords[1] + coords[2]*coords[2]; }
+    return x*x + y*y + z*z; }
   vector3d normalized() const {
     return *this/this->norm(); }
 
   void tostr(char str[]) const {
-    sprintf(str, "(%6.2f, %6.2f, %6.2f)", coords[0], coords[1], coords[2]);
+    sprintf(str, "(%6.2f, %6.2f, %6.2f)", x, y, z);
   }
 
- private:
-  double coords[3];
+  static vector3d ran(double scale) {
+    double x, y, r2;
+    do {
+      x = 2*random::ran() - 1;
+      y = 2*random::ran() - 1;
+      r2 = x*x + y*y;
+    } while(r2 >= 1 || r2 == 0);
+    double fac = sqrt(-2*log(r2)/r2);
+    vector3d out(x*fac, y*fac, 0);
+    do {
+      x = 2*random::ran() - 1;
+      y = 2*random::ran() - 1;
+      r2 = x*x + y*y;
+    } while(r2 >= 1 || r2 == 0);
+    fac = sqrt(-2*log(r2)/r2);
+    out[2]=x*fac;
+    return out;
+  }
 };
 
 const vector3d operator*(const double scalar, const vector3d &v) {
   return v*scalar; }
 
+class rotation {
+ public:
+  double w;
+  double x;
+  double y;
+  double z;
+
+  rotation() { w = 1.0; x = y = z = 0; }
+  rotation(const rotation &q) { w = q.w; x = q.x; y = q.y; z = q.z; }
+
+  rotation operator=(const rotation &q) {
+    w = q.w; x = q.x; y = q.y; z = q.z;
+    return *this; }
+
+  rotation operator*(const rotation &q) const {
+    return rotation(w*q.w - x*q.x - y*q.y - z*q.z,
+                    w*q.x + x*q.w + y*q.z - z*q.y,
+                    w*q.y - x*q.z + y*q.w + z*q.x,
+                    w*q.z + x*q.y - y*q.x + z*q.w).normalized(); }
+  rotation operator*=(const rotation &q) {
+    *this = (*this)*q; return *this; }
+
+  bool operator ==(const rotation &q) const {
+    return ((w == q.w) && (x == q.x) &&
+            (y == q.y) && (z == q.z)); }
+  bool operator !=(const rotation &q) const {
+    return !(*this == q); }
+
+  vector3d rotate_vector(const vector3d &v) {
+    const rotation product = (*this)*rotation(0, v)*this->conj();
+    return vector3d(product.x, product.y, product.z); }
+
+  void tostr(char str[]) const {
+    const double theta = 2*acos(w);
+    const double fac = 1.0/sin(theta/2.0);
+    sprintf(str, "[%6.2f, (%6.2f, %6.2f, %6.2f)]", theta, x*fac, y*fac, z*fac); }
+
+  static rotation ran() {
+    double x, y, z, r2;
+    const double theta = 2.0*M_PI*random::ran();
+    do {
+      x = 2*random::ran() - 1;
+      y = 2*random::ran() - 1;
+      z = 2*random::ran() - 1;
+      r2 = x*x + y*y + z*z;
+    } while(r2 >= 1 || r2 == 0);
+    const rotation rot(cos(theta/2), vector3d(x, y, z)*sin(theta/2)/sqrt(r2));
+    return rot.normalized();
+  }
+
+  static rotation ran(double angwidth) {
+    double x, y, z, r2, sintheta_over2;
+    do {
+      do {
+        x = 2*random::ran() - 1;
+        y = 2*random::ran() - 1;
+        r2 = x*x + y*y;
+      } while (r2 >= 1 || r2 == 0);
+    const double fac = sqrt(-2*log(r2)/r2);
+    sintheta_over2 = fac*x*angwidth;
+    } while (sintheta_over2 <= -1 or sintheta_over2 >= 1);
+    const double costheta_over2 = sqrt(1 - sintheta_over2*sintheta_over2);
+    do {
+      x = 2*random::ran() - 1;
+      y = 2*random::ran() - 1;
+      z = 2*random::ran() - 1;
+      r2 = x*x + y*y + z*z;
+    } while(r2 >= 1 || r2 == 0);
+    const double vfac = sintheta_over2/sqrt(r2);
+    const rotation rot(costheta_over2, x*vfac, y*vfac, z*vfac);
+    return rot.normalized();
+  }
+
+ private:
+  rotation(const double neww, const double newx, const double newy, const double newz) {
+    w = neww; x = newx; y = newy; z = newz; }
+  rotation(const double neww, const vector3d &v) {
+    w = neww; x = v.x; y = v.y; z = v.z; }
+
+  rotation operator/(const double scalar) const {
+    return rotation(w/scalar, x/scalar, y/scalar, z/scalar); }
+  rotation operator/=(const double scalar) {
+    w/=scalar; x/=scalar; y/=scalar; z/=scalar;
+    return *this; }
+
+  rotation conj() const {
+    return rotation(w, -x, -y, -z); }
+
+  rotation normalized() const {
+    return *this/sqrt(w*w + x*x + y*y + z*z);
+  }
+};
 
 class quaternion {
  public:
-  quaternion() {
-    coords[0] = 1.0; coords[1] = coords[2] = coords[3] = 0; }
-  quaternion(const double w, const double x, const double y, const double z) {
-    coords[0] = w; coords[1] = x; coords[2] = y; coords[3] = z; }
+  double w;
+  double x;
+  double y;
+  double z;
+
+  quaternion() { w = 1.0; x = y = z = 0; }
+  quaternion(const double neww, const double newx, const double newy, const double newz) {
+    w = neww; x = newx; y = newy; z = newz; }
   quaternion(const quaternion &q) {
-    coords[0] = q.coords[0]; coords[1] = q.coords[1];
-    coords[2] = q.coords[2]; coords[3] = q.coords[3]; }
-  quaternion(const double w, const vector3d &v) {
-    coords[0] = w; coords[1] = v[0]; coords[2] = v[1]; coords[3] = v[2]; }
-
-  double w() const { return coords[0]; }
-  double x() const { return coords[1]; }
-  double y() const { return coords[2]; }
-  double z() const { return coords[3]; }
-
-  double w(const double neww) { coords[0] = neww; return coords[0]; }
-  double x(const double newx) { coords[1] = newx; return coords[1]; }
-  double y(const double newy) { coords[2] = newy; return coords[2]; }
-  double z(const double newz) { coords[3] = newz; return coords[3]; }
+    w = q.w; x = q.x; y = q.y; z = q.z; }
+  quaternion(const double neww, const vector3d &v) {
+    w = neww; x = v.x; y = v.y; z = v.z; }
 
   quaternion operator=(const quaternion &q) {
-    coords[0] = q.coords[0]; coords[1] = q.coords[1];
-    coords[2] = q.coords[2]; coords[3] = q.coords[3];
+    w = q.w; x = q.x; y = q.y; z = q.z;
     return *this; }
 
   quaternion operator+(const quaternion &q) const {
-    return quaternion(coords[0]+q.coords[0], coords[1]+q.coords[1],
-                      coords[2]+q.coords[2], coords[3]+q.coords[3]); }
+    return quaternion(w+q.w, x+q.x, y+q.y, z+q.z); }
   quaternion operator+=(const quaternion &q) {
-    coords[0] += q.coords[0]; coords[1] += q.coords[1];
-    coords[2] += q.coords[2]; coords[3] += q.coords[3];
+    w += q.w; x += q.x; y += q.y; z += q.z;
     return *this; }
 
   quaternion operator-(const quaternion &q) const {
-    return quaternion(coords[0]-q.coords[0], coords[1]-q.coords[1],
-                      coords[2]-q.coords[2], coords[3]-q.coords[3]); }
+    return quaternion(w-q.w, x-q.x, y-q.y, z-q.z); }
   quaternion operator-=(const quaternion &q) {
-    coords[0] -= q.coords[0]; coords[1] -= q.coords[1];
-    coords[2] -= q.coords[2]; coords[3] -= q.coords[3];
+    w -= q.w; x -= q.x; y -= q.y; z -= q.z;
     return *this; }
 
   quaternion operator*(const double scalar) const {
-    return quaternion(scalar*coords[0], scalar*coords[1],
-                      scalar*coords[2], scalar*coords[3]); }
+    return quaternion(scalar*w, scalar*x,
+                      scalar*y, scalar*z); }
   quaternion operator*=(const double scalar) {
-    coords[0] *= scalar; coords[1] *= scalar; coords[2] *= scalar; coords[3] *= scalar;
+    w *= scalar; x *= scalar; y *= scalar; z *= scalar;
     return *this; }
 
   quaternion operator*(const quaternion &q) const {
-    return quaternion(coords[0]*q.coords[0] - coords[1]*q.coords[1] -
-                      coords[2]*q.coords[2] - coords[3]*q.coords[3],
-                      coords[0]*q.coords[1] + coords[1]*q.coords[0] +
-                      coords[2]*q.coords[3] - coords[3]*q.coords[2],
-                      coords[0]*q.coords[2] - coords[1]*q.coords[3] +
-                      coords[2]*q.coords[0] + coords[3]*q.coords[1],
-                      coords[0]*q.coords[3] + coords[1]*q.coords[2] -
-                      coords[2]*q.coords[1] + coords[3]*q.coords[0]); }
+    return quaternion(w*q.w - x*q.x - y*q.y - z*q.z,
+                      w*q.x + x*q.w + y*q.z - z*q.y,
+                      w*q.y - x*q.z + y*q.w + z*q.x,
+                      w*q.z + x*q.y - y*q.x + z*q.w); }
   quaternion operator*=(const quaternion &q) {
     *this = (*this)*q; return *this; }
 
   quaternion operator/(const double scalar) const {
-    return quaternion(coords[0]/scalar, coords[1]/scalar,
-                      coords[2]/scalar, coords[3]/scalar); }
+    return quaternion(w/scalar, x/scalar,
+                      y/scalar, z/scalar); }
   quaternion operator/=(const double scalar) {
-    coords[0]/=scalar; coords[1]/=scalar; coords[2]/=scalar; coords[3]/=scalar;
+    w/=scalar; x/=scalar; y/=scalar; z/=scalar;
     return *this; }
 
   bool operator ==(const quaternion &q) const {
-    return ((coords[0] == q.coords[0]) && (coords[1] == q.coords[1]) &&
-            (coords[2] == q.coords[2]) && (coords[3] == q.coords[3])); }
+    return ((w == q.w) && (x == q.x) &&
+            (y == q.y) && (z == q.z)); }
   bool operator !=(const quaternion &q) const {
     return !(*this == q); }
 
-  double &operator[](const unsigned int i) { return coords[i]; }
-  const double &operator[](const unsigned int i) const { return coords[i]; }
+  double &operator[](const unsigned int i) {
+    switch(i) {
+    case 0: return w;
+    case 1: return x;
+    case 2: return y;
+    case 3: return z;
+    }
+  }
+  const double operator[](const unsigned int i) const {
+    switch(i) {
+    case 0: return w;
+    case 1: return x;
+    case 2: return y;
+    case 3: return z;
+    }
+  }
 
   quaternion conj() const {
-    return quaternion(coords[0], -coords[1], -coords[2], -coords[3]); }
+    return quaternion(w, -x, -y, -z); }
 
   double norm() const {
-    return sqrt(coords[0]*coords[0] + coords[1]*coords[1] +
-                coords[2]*coords[2] + coords[3]*coords[3]); }
+    return sqrt(w*w + x*x + y*y + z*z); }
   double normsquared() const {
-    return coords[0]*coords[0] + coords[1]*coords[1] +
-                coords[2]*coords[2] + coords[3]*coords[3]; }
+    return w*w + x*x + y*y + z*z; }
   quaternion normalized_vector() const {
-    const double norm = sqrt(coords[1]*coords[1] + coords[2]*coords[2] +
-                             coords[3]*coords[3]);
-    return quaternion(coords[0], coords[1]/norm, coords[2]/norm, coords[3]/norm); }
+    const double norm = sqrt(x*x + y*y + z*z);
+    return quaternion(w, x/norm, y/norm, z/norm); }
   quaternion normalized() const {
     return *this/this->norm();
   }
 
   void tostr(char str[]) const {
-    sprintf(str, "[%6.2f, (%6.2f, %6.2f, %6.2f)]", coords[0], coords[1], coords[2], coords[3]); }
-
- private:
-  double coords[4]; // coords[0] = scalar part, rest is vector part
+    sprintf(str, "[%6.2f, (%6.2f, %6.2f, %6.2f)]", w, x, y, z); }
 };
 
 const quaternion operator*(const double scalar, const quaternion &q) {
