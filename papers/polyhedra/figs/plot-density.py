f4b71eae88782de252ca5e2e3dd7d2ec0a4b5610
Paho Lurie-Gregg <paho@paholg.com>
2013-09-19 18:09:02 -0700

    Added order parameter to polyhedra-monte-carlo, some small plot changes Tested on paho-deb. Took 4:41.97.

--- papers/polyhedra/figs/plot-density.py
+++ papers/polyhedra/figs/plot-density.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from __future__ import division
 import matplotlib, sys, os, argparse
-import common
+import read
 
 parser = argparse.ArgumentParser(description='Plot density of polyhedra.')
 parser.add_argument('ff', metavar='ff', type=float, help='filling fraction')
@@ -15,6 +15,7 @@ parser.add_argument('-p', '--periodic', action='store_true',
                     help='will use periodic cell - defaults to walls otherwise')
 parser.add_argument('--hide', action='store_true',
                     help='will just save the plot and won\'t display it')
+parser.add_argument('-z', action='store_true', help='only plot the z-dimension')
 
 args = parser.parse_args()
 
@@ -28,7 +29,7 @@ else:
   celltype = 'walls'
 
 if args.N == 0:
-  N = common.get_N("figs/mc/%s-%4.2f-density-%s" %(celltype, ff, polyhedron))
+  N = read.get_N("figs/mc/%s-%4.2f-density-%s" %(celltype, ff, polyhedron))
   if N == 0:
     exit(1)
 else: N = args.N
@@ -37,27 +38,37 @@ if args.hide:
   matplotlib.use('Agg')
 from pylab import *
 
-e, densities = common.read_mc_density(ff, polyhedron, N, celltype)
+e, densities = read.read_mc_density(ff, polyhedron, N, celltype)
 if e == 0:
   exit(1)
-length = common.read_mc_dimensions(ff, polyhedron, N, celltype)
+length = read.read_mc_dimensions(ff, polyhedron, N, celltype)
 print 'cell shape: ', length
 dims = ['x', 'y', 'z']
 colors = ['r', 'b', 'k']
-for i in xrange(3):
-  coord = e[i]
-  density = densities[i]
-  mid = length[i]/2
-  print("Integral %c: %g" %(dims[i], sum(density)/len(density)*length[0]*length[1]*length[2]))
-  plot(coord[coord<mid], density[coord<mid], label="$%c$" %dims[i], color=colors[i], linestyle='-')
-  plot(length[i]-coord[coord>mid], density[coord>mid], color=colors[i], linestyle='--')
+if args.z:
+  coord = e[2]
+  density=densities[2]
+  plot(coord, density, label="$%c$" %dims[2], color=colors[2], linestyle='-')
+else:
+  for i in xrange(3):
+    coord = e[i]
+    density = densities[i]
+    mid = length[i]/2
+    print("Integral %c: %g" %(dims[i], sum(density)/len(density)*length[0]*length[1]*length[2]))
+    plot(coord[coord<mid], density[coord<mid], label="$%c$" %dims[i], color=colors[i], linestyle='-')
+    plot(length[i]-coord[coord>mid], density[coord>mid], color=colors[i], linestyle='--')
 
 
 if polyhedron == 'cube' and celltype == 'walls':
   axvline(x = 0.5, linestyle=':')
   axvline(x = sqrt(2)/2, linestyle=':')
   axvline(x = sqrt(3)/2, linestyle=':')
-legend(loc='best')
+  if args.z:
+    axvline(x = length[2]-0.5, linestyle=':')
+    axvline(x = length[2]-sqrt(2)/2, linestyle=':')
+    axvline(x = length[2]-sqrt(3)/2, linestyle=':')
+if not args.z:
+  legend(loc='best')
 
 #xlim(0, 4)
 title("$%s,$ $density,$ $%s,$ $\\eta = %04.2f$, $N = %i$." %(celltype, polyhedron, ff, N))
