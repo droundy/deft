86265d2dc7c17a6b63854eeaefb903fcc762f7e4
Michael Perlin <perlinm@MAPHost.MAPDomain>
2014-04-26 22:23:42 -0700

    modified run-monte-carlo so that it is actually usable Tested on MAPHost. Took 0:48.97.

--- papers/square-well-liquid/figs/run-monte-carlo.py
+++ papers/square-well-liquid/figs/run-monte-carlo.py
@@ -1,69 +1,69 @@
 #!/usr/bin/python2
-import os, sys, argparse, socket
+
+import os, sys, socket
 import subprocess as sp
 
-import arguments
-from info import *
+if len(sys.argv) != 5:
+    print 'useage: %s ww ff N version' % sys.argv[0]
+    exit(1)
 
-# parse arguments
-parser = argparse.ArgumentParser(
-  description='Run monte carlo simulation(s) using sbatch.',
-  parents = [arguments.parser])
+ww = float(sys.argv[1])
 
-parser.add_argument(
-		'-initialize', metavar='INT', type=int, default=1000000,
-		help='Number of iterations to run for initialization')
+ff = float(sys.argv[2])
 
-parser.add_argument(
-		'-iterations', metavar='INT', type=int, default=1000000000000,
-		help='Number of simulation iterations')
+N = int(sys.argv[3])
 
-args = parser.parse_args()
+version = str(sys.argv[4])
+
+cores = 2 if socket.gethostname() == 'MAPHost' else 8
+
+initialize = 10000
+iterations = 10000
+
+# define some directories
+figdir = os.path.dirname(os.path.realpath(__file__))
+swdir = os.path.dirname(figdir)
+projectdir = os.path.realpath(swdir+'../../..')
+jobdir = swdir+'/jobs'
+datadir = swdir+'/data/'
+simname = 'square-well-monte-carlo'
 
 # build monte carlo code
-exitStatus = sp.call(["scons","-C",projectdir,simname])
+exitStatus = sp.call(["scons","-j%i"%cores,"-C",projectdir,simname])
 if exitStatus != 0:
     print "Build failed"
     exit(exitStatus)
 
-# store all sets to run
-for ww in args.ww:
-    for ff in args.ff:
-        for N in args.N:
-            for kT in args.kT:
-                memory = N # fixme: better guess
-                jobname = 'periodic-ww%04.2f-ff%04.2f-N%i' % (ww, ff, N)
-                if kT != 0: jobname += '-kT%g' %kT
-                if args.flat: jobname += '-flat'
-                elif args.nw: jobname += '-nw'
-                basename = "%s/%s" %(jobdir, jobname)
-                scriptname = basename + '.sh'
-                outname = basename + '.out'
-                errname = basename + '.err'
+# write script to run simulation
+memory = N # fixme: better guess
+jobname = 'periodic-ww%02.0f-ff%02.0f-N%i-%s' \
+  % (ww*100, ff*100, N, version)
+basename = "%s/%s" %(jobdir, jobname)
+scriptname = basename + '.sh'
+outname = basename + '.out'
+errname = basename + '.err'
 
-                command = "time nice -19 %s/%s" %(projectdir, simname)
+command = "time nice -19 %s/%s" %(projectdir, simname)
 
-                script = open(scriptname,'w')
-                script.write("#!/bin/bash\n")
-                script.write("#SBATCH --mem-per-cpu=%i\n" % memory)
-                script.write("#SBATCH --output %s\n" % outname)
-                script.write("#SBATCH --error %s\n\n" % errname)
-                script.write("echo \"Starting job with ID: %s, "
-                             "Estimated memory use: %i MB.\"\n\n"
-                             %(jobname,memory))
-                script.write("cd %s\n" %projectdir)
-                script.write(command)
-                for (arg,val) in [ ("ww",ww), ("ff",ff), ("N",N), ("kT",kT),
-                                   ("walls",args.walls),
-                                   ("initialize",args.initialize),
-                                   ("iterations",args.iterations) ]:
-                    script.write(" \\\n --" + arg + "=" + str(val))
-                if args.flat: script.write(" \\\n --flat")
-                elif args.nw: script.write(" \\\n --nw")
-                script.close()
+script = open(scriptname,'w')
+script.write("#!/bin/bash\n")
+script.write("#SBATCH --mem-per-cpu=%i\n" % memory)
+script.write("#SBATCH --output %s\n" % outname)
+script.write("#SBATCH --error %s\n\n" % errname)
+script.write("echo \"Starting job with ID: %s, "
+             "Estimated memory use: %i MB.\"\n\n" %(jobname,memory))
+script.write("cd %s\n" %projectdir)
+script.write(command)
+for (arg,val) in [ ("ww",ww), ("ff",ff), ("N",N),
+                   ("initialize",initialize),
+                   ("iterations",iterations) ]:
+    script.write(" \\\n --%s %s" %(arg,str(val)))
+script.write(" \\\n --%s"%version)
+script.close()
 
-                if socket.gethostname() == 'MAPHost':
-                    #sp.Popen(["bash", scriptname])
-                    None
-                else:
-                    sp.Popen(["sbatch", "-J", jobname, scriptname])
+# start simulation
+if socket.gethostname() == 'MAPHost':
+#    sp.Popen(["bash", scriptname])
+    None
+else:
+    sp.Popen(["sbatch", "-J", jobname, scriptname])
