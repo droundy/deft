1315d5c880763893434fbbce76b945d5ddcd2e16
Eric Krebs <krebse@knightley.physics.oregonstate.edu>
2014-09-15 10:38:55 -0700

    Changed variable names to match those in the soft spheres paper. Tested on knightley. Took 0:22.01.

--- papers/fuzzy-fmt/figs/potential-plot.py
+++ papers/fuzzy-fmt/figs/potential-plot.py
@@ -13,21 +13,21 @@ import styles
 
 kT = 1.0
 
-sigma = ( 2.0/(1 + 6*np.sqrt(np.log(2)*kT)) )**(1.0/6)
-a = sigma/np.sqrt(np.pi)/(6*np.log(2) + np.sqrt(np.log(2)/kT))
+alpha = ( 2.0/(1 + 6*np.sqrt(np.log(2)*kT)) )**(1.0/6)
+Xi = alpha/np.sqrt(np.pi)/(6*np.log(2) + np.sqrt(np.log(2)/kT))
 
-rmin = 0.9*sigma
+rmin = 0.9*alpha
 
 r = np.arange(rmin, 2**(1.0/6), 0.01)
 plt.plot(r, (4/r**12 - 4/r**6 + 1)/36, label='WCA')
 
-r = np.arange(rmin, 1.1*sigma, 0.01)
-Verf = -kT*np.log(0.5*(erf ((r-sigma)/a)+1))
+r = np.arange(rmin, 1.1*alpha, 0.01)
+Verf = -kT*np.log(0.5*(erf ((r-alpha)/Xi)+1))
 plt.plot(r, Verf, '-', label='erf')
 
-plt.axvline(sigma)
+plt.axvline(alpha)
 
-plt.xlim(rmin, 1.1*sigma)
+plt.xlim(rmin, 1.1*alpha)
 plt.ylabel('$V$')
 plt.xlabel('$r$')
 plt.legend(loc='best')
