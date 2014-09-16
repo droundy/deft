34865c43cef219bcb2ee38bce0329cc2f8b79f14
David Roundy <roundyd@physics.oregonstate.edu>
2014-09-16 10:33:03 -0700

    beautify fprime plot Tested on bennet. Took 0:19.89.

--- papers/fuzzy-fmt/figs/w2-comparison.py
+++ papers/fuzzy-fmt/figs/w2-comparison.py
@@ -16,25 +16,29 @@ import styles
 kT = 1.0
 
 alpha, Xi, diameter = wca_erf.parameters(kT)
-rmin = 0.7*alpha
+rmin = alpha - 3*Xi
+rmax = alpha + 3*Xi
+if rmax < diameter:
+    rmax = diameter
 
-r = np.arange(rmin, diameter, 0.001)
+r = np.arange(rmin, diameter, 0.01*Xi)
 
 figscale=1.4
 plt.figure(figsize=(4*figscale,3*figscale))
 
-plt.axvline(diameter)
+plt.axvline(diameter, color='k', linestyle=':')
 
 fp_wca = np.exp(-wca_erf.V(r)/kT)*(-wca_erf.Vprime(r)/kT)
 plt.plot(r, fp_wca, label="$f'$ WCA")
 
-r = np.arange(rmin, diameter, 0.01)
+r = np.arange(rmin, rmax, 0.01*Xi)
 fp_erf = np.exp(-(r-alpha)**2/Xi**2)/Xi/np.sqrt(np.pi)
 plt.plot(r, fp_erf, '-', label="$f'$ erf")
 
-plt.axvline(alpha)
+plt.axvline(alpha, color='k', linestyle=':')
 
-plt.xlim(rmin, diameter)
+plt.title(r"$f'$ with $kT/\epsilon=%g$" % kT)
+plt.xlim(rmin, rmax)
 plt.ylabel("$f'$")
 plt.xlabel('$r$')
 plt.legend(loc='best')
