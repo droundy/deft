5b21726ddce0f150aef760fea6efe5a7c7a66afa
Eric Krebs <krebse@knightley.physics.oregonstate.edu>
2014-09-18 14:19:34 -0700

    Used reduced density and added remark for reduced temperature in argon soft-spheres experiment plots. Tested on knightley. Took 0:19.39.

--- papers/fuzzy-fmt/figs/radial_argon_plots.py
+++ papers/fuzzy-fmt/figs/radial_argon_plots.py
@@ -8,17 +8,25 @@ if 'show' not in sys.argv:
   matplotlib.use('Agg')
 from pylab import *
 
+sigma = 0.3506 #nm
+
 figure()
 data = loadtxt('figs/YarnellArgon85K.dat')
-plot(data[:,0],data[:,1])
+n =0.02125 # Angstrom^-3
+nsig_3 = n*(sigma*10)**3
+plot(data[:,0],data[:,1]*nsig_3)
 savefig('figs/Argon-vapor_pressure-85K.pdf')
 
 figure()
 data2 = loadtxt('figs/EggertArgon0.6GPaNN.dat')
-plot(data2[:,0],data2[:,1])
+n = 24.23 #atoms/nm^3
+nsig_3 = n*sigma**3
+plot(data2[:,0],data2[:,1]*nsig_3)
 savefig('figs/Argon-0_6GPa-RT.pdf')
 
 figure()
 data3 = loadtxt('figs/EggertArgon1.1GPaNN.dat')
-plot(data3[:,0],data3[:,1])
+n = 27.74 #atoms/nm^3
+nsig_3 = n*sigma**3
+plot(data3[:,0],(data3[:,1]-1)*nsig_3)
 savefig('figs/Argon-1_1GPa-RT.pdf')
