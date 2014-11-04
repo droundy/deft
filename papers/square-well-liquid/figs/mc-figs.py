ee581399c9b78b53d54c06b806bf4ae8f5cdcc23
David Roundy <roundyd@physics.oregonstate.edu>
2014-11-04 09:26:55 -0800

    yet more tup changes to keep up with Michael this time Tested on bennet. Took 0:19.39.

--- papers/square-well-liquid/figs/mc-figs.py
+++ papers/square-well-liquid/figs/mc-figs.py
@@ -11,7 +11,7 @@ methods = ["nw", "wang_landau", "gaussian", "flat", "walkers", "kT2", "kT1"]
 
 inputs = ["../data/periodic-ww%04.2f-ff%04.2f-N%i-%s-%s.dat"
           % (ww, ff, N, method.replace(' ',''), postfix)
-          for postfix in ['g', 'E', 'lnw', 'lnw-init', 'rt-init', 'E-init']
+          for postfix in ['g', 'E', 'lnw', 'rt']
           for method in methods] + ['styles.pyc']
 
 cmd = 'cd .. && python figs/plot-dos.py %f %f %d "%s"' % (ww, ff, N, methods)
@@ -26,4 +26,8 @@ print ': %s |> %s |> %s' % (string.join(inputs), cmd, string.join(outputs))
 cmd = 'cd .. && python figs/plot-uhc.py %f %f %d "%s"' % (ww, ff, N, methods)
 outputs = ["periodic-ww%02.0f-ff%02.0f-N%i-u.pdf" % (ww*100, ff*100, N),
            "periodic-ww%02.0f-ff%02.0f-N%i-hc.pdf" % (ww*100, ff*100, N)]
+print ': %s |> %s |> %s' % (string.join(inputs), cmd, string.join(outputs))
+
+cmd = 'cd .. && python figs/plot-round-trips.py %f %f %d "%s"' % (ww, ff, N, methods)
+outputs = ["periodic-ww%02.0f-ff%02.0f-N%i-rt.pdf" % (ww*100, ff*100, N)]
 print ': %s |> %s |> %s' % (string.join(inputs), cmd, string.join(outputs))
