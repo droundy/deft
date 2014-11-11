65192eaf02eff9ec66754e99f6d305b0e492ea32
David Roundy <roundyd@physics.oregonstate.edu>
2014-11-11 12:17:03 -0800

    maybe improve memory heuristic for sw mc and slurm Tested on bennet. Took 0:43.62.

--- papers/square-well-liquid/run-monte-carlo.py
+++ papers/square-well-liquid/run-monte-carlo.py
@@ -40,7 +40,7 @@ if exitStatus != 0:
     exit(exitStatus)
 
 for version in versions:
-    memory = N*N # fixme: better guess
+    memory = 20*N # fixme: better guess
     jobname = 'periodic-ww%04.2f-ff%04.2f-N%i-%s' %(ww, ff, N, version)
     basename = "%s/%s" %(jobdir, jobname)
     scriptname = basename + '.sh'
