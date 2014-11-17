8e6c7e9edb97476fdf7ecfc21be59ea5ea42a74a
David Roundy <roundyd@physics.oregonstate.edu>
2014-11-17 07:10:34 -0800

    make SW scaling plot actually plot something Tested on darcy. Took 35:15.56.

--- papers/square-well-liquid/figs/plot-scaling.py
+++ papers/square-well-liquid/figs/plot-scaling.py
@@ -3,7 +3,7 @@ import matplotlib, sys
 if 'show' not in sys.argv:
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import numpy, glob
+import numpy, glob, re
 import styles
 
 if len(sys.argv) not in [4,5]:
@@ -23,14 +23,38 @@ versions = eval(sys.argv[3])
 
 # input: "data/periodic-ww*-ff*-N*-*-rt.dat"
 
+N_regex = re.compile(r'-N([0-9]+)')
+initialization_iters_regex = re.compile(r'# iterations:\s+([0-9]+)')
+
 plt.title('Scaling for $\lambda=%g$, $\eta=%g$' % (ww, ff))
 for version in versions:
     for filename in glob.glob("data/periodic-ww%04.2f-ff%04.2f-N*-%s-rt.dat" % (ww, ff, version)):
         data = numpy.loadtxt(filename)
-        plt.plot([1,2], [3,4], label=filename)
 
-plt.xlabel('$U/N\epsilon$')
-plt.ylabel('Round trips')
+        N = int(N_regex.findall(filename)[0])
+
+        with open(filename, 'r') as content_file:
+            content = content_file.read()
+
+        init_iters = int(initialization_iters_regex.findall(content)[0])
+        print 'N is', N, 'version is', version, 'iters', init_iters
+
+        if N == 6:
+            plt.semilogy(N, init_iters, styles.color[version]+'.', label=version)
+        else:
+            plt.semilogy(N, init_iters, styles.color[version]+'.')
+
+        e_hist = numpy.loadtxt(
+            "data/periodic-ww%04.2f-ff%04.2f-N%i-%s-E.dat" % (ww, ff, N, version))
+        lnw = numpy.loadtxt(
+            "data/periodic-ww%04.2f-ff%04.2f-N%i-%s-lnw.dat" % (ww, ff, N, version))
+        energy = -e_hist[:,0]/N
+        log10w = lnw[e_hist[:,0].astype(int),1]*numpy.log10(numpy.exp(1))
+        log10_dos = numpy.log10(e_hist[:,1]) - log10w
+        log10_dos -= log10_dos.max()
+
+plt.xlabel('$N$')
+plt.ylabel('initialization iterations')
 plt.legend(loc='best').get_frame().set_alpha(0.25)
 plt.tight_layout(pad=0.2)
 plt.savefig("figs/periodic-ww%02.0f-ff%02.0f-scaling.pdf" % (ww*100, ff*100))
