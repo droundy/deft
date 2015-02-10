2427e57e6cf943112a35e2e2b355054052aa2867
Patrick Kreitzberg <kreitzpa@elliot.physics.oregonstate.edu>
2015-02-10 11:47:25 -0800

    Data for homogeneous and FCC lattice Tested on elliot. Took 0:33.23.

--- papers/fuzzy-fmt/figs/showSpheres.py
+++ papers/fuzzy-fmt/figs/showSpheres.py
@@ -15,22 +15,32 @@ allSpheres = genfromtxt(sys.argv[1])
 #container.size=(6,6,6)
 #container = sphere(opacity=.1, color=color.red)
 #container.radius=(3)
-
+#wallR=box(pos=(15,15,15),size=(30,30,30),opacity=.5)
 balls = []
 #for spheres in allSpheres:
 spheres = allSpheres # reshape(spheres, (-1, 3))
 #   rate(100)
+
+a=0
+s=30/7
+print 30/7
 for i in range(len(spheres)):
-   if i >= len(balls):
-      balls.append(sphere(pos=spheres[i]))
-   else:
-      balls[i].pos = spheres[i]
-   for j in range(len(spheres)):
-     if i != j:
-         d = sqrt((spheres[j][0]-spheres[i][0])**2+(spheres[j][1]-spheres[i][1])**2+(spheres[j][2]-spheres[i][2])**2)
-         if d < 2:
-           print 'distance is', i, j, d
-           balls[i].color = color.yellow
+    if(a <= spheres[i][0]<s and a <=spheres[i][1]<s and a <=spheres[i][2]<s):
+        if i >= len(balls):
+            balls.append(sphere(pos=spheres[i]))
+            
+        else:
+            balls[i].pos = spheres[i]
+            balls[i].radius=0.1
+        for j in range(len(spheres)):
+            if i != j:
+                d = sqrt((spheres[j][0]-spheres[i][0])**2+(spheres[j][1]-spheres[i][1])**2+(spheres[j][2]-spheres[i][2])**2)
+                if d < 1:
+                    print 'distance is', i, j, d
+                    balls[i].color = color.yellow
+for i in range(len(balls)):       
+    balls[i].radius=1
+
 
 #The rest is stuff that Jeff added for investigating a bug with the mc-walls data.  More info about walls!
 countOutside = 0
