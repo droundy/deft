0e81bb4c6388e299354ffd2dc958b8dddc1c5af4
David Roundy <roundyd@physics.oregonstate.edu>
2015-02-16 14:29:00 -0800

    fix horizontal axis in radial_argon_plots.py Tested on bennet. Took 10:36.61.

--- papers/fuzzy-fmt/figs/radial_argon_plots.py
+++ papers/fuzzy-fmt/figs/radial_argon_plots.py
@@ -19,7 +19,7 @@ plot(data[:,0]/(sigma*10),data[:,1])
 data_mc1=loadtxt('figs/mc_testp_wca-0.8389-0.7100.dat.gradial')
 plot((1/sigma_over_R)*data_mc1[:,0],data_mc1[:,1])
 data_dft1 = loadtxt('figs/radial-lj-0.7100-0.84.dat')
-plot(data_dft1[:,0]/2,data_dft1[:,1])
+plot(data_dft1[:,0]/sigma_over_R,data_dft1[:,1])
 xlabel(r'$r/\sigma$')
 savefig('figs/Argon-vapor_pressure-85K.pdf')
 
@@ -31,19 +31,20 @@ plot(data2[:,0]/sigma,data2[:,1])
 data_mc2 = loadtxt('figs/mc_testp_wca-0.9570-2.4800.dat.gradial')
 plot((1/sigma_over_R)*data_mc2[:,0], data_mc2[:,1])
 data_dft2 = loadtxt('figs/radial-lj-2.4800-0.96.dat')
-plot(data_dft2[:,0]/2,data_dft2[:,1])
+plot(data_dft2[:,0]/sigma_over_R,data_dft2[:,1])
 xlabel(r'$r/\sigma$')
 savefig('figs/Argon-0_6GPa-RT.pdf')
 
 figure()
 data3 = loadtxt('figs/EggertArgon1.1GPaRAW.dat')
 n = 27.74 #atoms/nm^3
+
 nsig_3 = n*sigma**3
 plot(data3[:,0]/sigma,(data3[:,1]))
 data_mc3 = loadtxt('figs/mc_testp_wca-1.0950-2.4800.dat.gradial')
 plot((1/sigma_over_R)*data_mc3[:,0], data_mc3[:,1])
 data_dft3 = loadtxt('figs/radial-lj-2.4800-1.09.dat')
-plot(data_dft3[:,0]/2,data_dft3[:,1])
+plot(data_dft3[:,0]/sigma_over_R,data_dft3[:,1])
 xlabel(r'$r/\sigma$')
 savefig('figs/Argon-1_1GPa-RT.pdf')
 
@@ -55,7 +56,7 @@ data_mc4 = loadtxt('figs/mc_testp_wca-0.5844-1.2350.dat.gradial')
 plot(data4[:,0]/(sigma*10),data4[:,1])
 plot((1/sigma_over_R)*data_mc4[:,0], data_mc4[:,1])
 data_dft4 = loadtxt('figs/radial-lj-1.2350-0.58.dat')
-plot(data_dft4[:,0]/2,data_dft4[:,1]*1.7)
+plot(data_dft4[:,0]/sigma_over_R,data_dft4[:,1]*1.7)
 xlabel(r'$r/\sigma$')
 
 
