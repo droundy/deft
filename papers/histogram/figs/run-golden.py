d7545ecfd54a0d544f10a2e5032cf80d5a6348a9
David Roundy <roundyd@physics.oregonstate.edu>
2015-03-13 16:59:55 -0700

    make run-golden accept a filling-fraction argument Tested on bennet. Took 0:33.35.

--- papers/histogram/figs/run-golden.py
+++ papers/histogram/figs/run-golden.py
@@ -1,8 +1,8 @@
 from __future__ import division
 import os, numpy, sys
 
-if len(sys.argv) != 2:
-    print "usage:  python %s N" % sys.argv[0]
+if len(sys.argv) != 3:
+    print "usage:  python %s N ff" % sys.argv[0]
     exit(1)
 
 paperdir = 'papers/histogram'
@@ -13,11 +13,12 @@ os.system('fac square-well-monte-carlo')
 def run_golden(N, eta, ww, method):
     filename = 'golden-%s-%d-%.4f-%.4f' % (method, N, eta, ww)
     iterations = 10000000000
-    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --N %d --filename_suffix golden --min_T 0.1 --%s --ww %g --iterations %d  > %s.out 2>&1 &" %
+    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --N %d --filename_suffix golden --min_T 0.1 --min_samples 10000 --%s --ww %g --iterations %d  > %s.out 2>&1 &" %
            (filename, N, method, ww, iterations, paperdir+'/data/'+filename))
     print(cmd)
     os.system(cmd)
 
 num = int(sys.argv[1])
+ff = float(sys.argv[2])
 
-run_golden(num, 0.3, 1.3, 'tmmc')
+run_golden(num, ff, 1.3, 'tmmc')
