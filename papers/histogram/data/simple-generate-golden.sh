92194497fdc43cac0f0fdb7c56209943db95241d
David Roundy <roundyd@physics.oregonstate.edu>
2015-03-16 08:23:07 -0700

    a bunch more tmmc-golden data from running all weekend Tested on darcy. Took 0:51.59.

---
+++ papers/histogram/data/simple-generate-golden.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -ev
+
+for N in `seq 21 30`; do
+    echo Working on N=$N
+    nice -19 ./square-well-monte-carlo --N $N --filename_suffix "golden" --min_T 0.1 --min_samples 10000 --tmmc --ff 0.3 --ww 1.3 --iterations 100000000
+done
