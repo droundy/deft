53f36f77ecc330ec20ea9410e3e8522042dd6554
David Roundy <roundyd@physics.oregonstate.edu>
2015-03-25 05:33:24 -0700

    push --tags and resume using git describe for version-identifier Tested on darcy. Took 12:50.10.

--- src/generate-version-identifier.py
+++ src/generate-version-identifier.py
@@ -3,12 +3,7 @@
 import subprocess, os
 
 try:
-    #name = subprocess.check_output(['git','describe','--dirty']).decode(encoding='UTF-8')[:-1]
-    # the above returns an error on quipu
-    # while adding try/except at least lets scons build with the above, it won't get us git version info (returning name = 'unknown')
-    # we might want to use the following instead, which properly gets git version info on at least two machines (namely quipu and MPerlin's computer)
-    # add '--short' before 'HEAD' for a shorter snippet, if desired
-    name = subprocess.check_output(['git','rev-parse','HEAD']).decode(encoding='UTF-8')[:-1]
+    name = subprocess.check_output(['git','describe','--dirty']).decode(encoding='UTF-8')[:-1]
 except subprocess.CalledProcessError:
     name = 'unknown'
 
