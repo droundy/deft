f8b3eee2c0ff6665ad66c44f6ffdb15e9efc81b5
Brenden Vischer <vischerb@quipu.physics.oregonstate.edu>
2015-04-07 11:42:09 -0700

    Committing the simple change in the plot-uhc code in the square well directory that I had been working on for the free energies.

--- papers/square-well-liquid/figs/plot-uhc.py
+++ papers/square-well-liquid/figs/plot-uhc.py
@@ -52,12 +52,17 @@ plt.title('Specific internal energy for $\lambda=%g$, $\eta=%g$, and $N=%i$' % (
 fig_hc, ax_hc = sci_fig('hc')
 plt.title('Specific heat capacity for $\lambda=%g$, $\eta=%g$, and $N=%i$' % (ww, ff, N))
 
+fig_f, ax_f = sci_fig('f')
+plt.title('Specific free energy for $\lambda=%g$, $\eta=%g$, and $N=%i$' % (ww, ff, N))
+
 # make dictionaries which we can index by method name
 U = {} # internal energy
 CV = {} # heat capacity
+F = {} # free energy
+
 
 for version in versions:
-    # energy histogram file; indexed by [-energy,counts]
+    # energy histogram file; indexed by [-energy,counts]z
     e_hist = numpy.loadtxt(
         "data/periodic-ww%04.2f-ff%04.2f-N%i-%s-E.dat" % (ww, ff, N, version), ndmin=2)
     # weight histogram file; indexed by [-energy,ln(weight)]
@@ -71,42 +76,61 @@ for version in versions:
     Z = numpy.zeros(len(T_range)) # partition function
     U[version] = numpy.zeros(len(T_range)) # internal energy
     CV[version] = numpy.zeros(len(T_range)) # heat capacity
+    F[version] = numpy.zeros(len(T_range)) # free energy
+
     for i in range(len(T_range)):
-        ln_dos_boltz = ln_dos - energy/T_range[i]
-        dos_boltz = numpy.exp(ln_dos_boltz - ln_dos_boltz.max())
-        Z[i] = sum(dos_boltz)
-        U[version][i] = sum(energy*dos_boltz)/Z[i]
-        CV[version][i] = sum((energy/T_range[i])**2*dos_boltz)/Z[i] - \
-                         (sum(energy/T_range[i]*dos_boltz)/Z[i])**2
+       # ln_dos_boltz = ln_dos - energy/T_range[i]
+        ln_dos_boltz = ln_dos - energy/T_range[i] #                added
+        maxA = ln_dos_boltz.max()
+        dos_boltz = numpy.exp(ln_dos_boltz - maxA)#           by
+        Z[i] = sum(dos_boltz) #                                       brenden
+        F[version][i] = -T_range[i]*maxA - T_range[i]*numpy.log(Z[i])#       for reasons
+       # dos_boltz = numpy.exp(ln_dos_boltz - ln_dos_boltz.max())
+       # Z[i] = sum(dos_boltz)
+       # U[version][i] = sum(energy*dos_boltz)/Z[i]
+       # CV[version][i] = sum((energy/T_range[i])**2*dos_boltz)/Z[i] - \
+        #                 (sum(energy/T_range[i]*dos_boltz)/Z[i])**2
+       # F[version][i] = energy.min() +  T_range[i]*numpy.log(sum(numpy.exp(dos_boltz -(energy - energy.min())/T_range[i])))
 
-    plt.figure('u')
-    plt.plot(T_range,U[version]/N,styles.plot(version),label=styles.title(version))
 
-    plt.figure('hc')
-    plt.plot(T_range,CV[version]/N,styles.plot(version),label=styles.title(version))
+#    plt.figure('u')
+#    plt.plot(T_range,U[version]/N,styles.plot(version),label=styles.title(version))
 
-plt.figure('u')
+#    plt.figure('hc')
+#    plt.plot(T_range,CV[version]/N,styles.plot(version),label=styles.title(version))
+
+    plt.figure('f')
+    plt.plot(T_range,F[version]/N,styles.plot(version),label=styles.title(version))
+
+#plt.figure('u')
+#plt.xlabel('$kT/\epsilon$')
+#plt.ylabel('$U/N\epsilon$')
+#plt.legend(loc='best')
+#plt.tight_layout(pad=0.2)
+#plt.savefig("figs/periodic-ww%02.0f-ff%02.0f-N%i-u.pdf" % (ww*100, ff*100, N))
+
+#plt.figure('hc')
+#plt.ylim(0)
+#plt.xlabel('$kT/\epsilon$')
+#plt.ylabel('$C_V/Nk$')
+#plt.legend(loc='best')
+#plt.tight_layout(pad=0.2)
+#plt.savefig("figs/periodic-ww%02.0f-ff%02.0f-N%i-hc.pdf" % (ww*100, ff*100, N))
+
+plt.figure('f')
 plt.xlabel('$kT/\epsilon$')
-plt.ylabel('$U/N\epsilon$')
+plt.ylabel('$F/N\epsilon$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0.2)
-plt.savefig("figs/periodic-ww%02.0f-ff%02.0f-N%i-u.pdf" % (ww*100, ff*100, N))
+plt.savefig("figs/periodic-ww%02.0f-ff%02.0f-N%i-f.pdf" %(ww*100, ff*100, N))
 
-plt.figure('hc')
-plt.ylim(0)
-plt.xlabel('$kT/\epsilon$')
-plt.ylabel('$C_V/Nk$')
-plt.legend(loc='best')
-plt.tight_layout(pad=0.2)
-plt.savefig("figs/periodic-ww%02.0f-ff%02.0f-N%i-hc.pdf" % (ww*100, ff*100, N))
-
-error_data = open("figs/error-table-ww%02.0f-ff%02.0f-%i.dat" % (ww*100, ff*100, N), "w")
-error_data.write("# method u_error cv_error\n")
-for version in versions:
-    u_error = max(abs(U[version][Tmin_i:] - U[reference_method][Tmin_i:]))/N
-    cv_error = max(abs(CV[version][Tmin_i:] - CV[reference_method][Tmin_i:]))/N
-    error_data.write("%s %g %g\n" % (version, u_error, cv_error))
-error_data.close()
+#error_data = open("figs/error-table-ww%02.0f-ff%02.0f-%i.dat" % (ww*100, ff*100, N), "w")
+#error_data.write("# method u_error cv_error\n")
+#for version in versions:
+#    u_error = max(abs(U[version][Tmin_i:] - U[reference_method][Tmin_i:]))/N
+#    cv_error = max(abs(CV[version][Tmin_i:] - CV[reference_method][Tmin_i:]))/N
+#    error_data.write("%s %g %g\n" % (version, u_error, cv_error))
+#error_data.close()
 
 if 'show' in sys.argv:
     plt.show()
