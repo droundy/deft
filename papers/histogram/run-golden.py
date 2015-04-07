187943ef155f48855efe3620a37c11ab064eeae2
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-07 13:17:12 -0700

    changes to run-golden script Tested on MAPHost. Took 0:36.13.

--- papers/histogram/run-golden.py
+++ papers/histogram/run-golden.py
@@ -1,24 +1,33 @@
+#!/usr/bin/env python2
 from __future__ import division
-import os, numpy, sys
+import os, numpy, sys, re
 
-if len(sys.argv) != 3:
-    print "usage:  python %s N ff" % sys.argv[0]
+if len(sys.argv) != 4:
+    print "usage:  python %s ww ff N" % sys.argv[0]
     exit(1)
 
 paperdir = 'papers/histogram'
 
-# always remember to build the executable before running it
+# switch to deft project directory and build SWMC
+filepath = os.path.abspath(__file__)
+deft_dir = re.sub('deft/.*','deft',filepath)
+paper_dir = re.sub('histogram/.*','histogram',filepath)
+data_dir = paper_dir+'/data'
 os.system('fac square-well-monte-carlo')
 
-def run_golden(N, eta, ww, method):
-    filename = 'golden-%s-%d-%.4f-%.4f' % (method, N, eta, ww)
+def run_golden(ww, ff, N, min_T, method):
+    filename = 'golden-%s-N%d-ff%.0f-ww%.0f' % (method, N, ff*100, ww*100)
     iterations = 10000000000
-    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --N %d --filename_suffix golden --min_T 0.1 --min_samples 10000 --%s --ww %g --iterations %d  > %s.out 2>&1 &" %
-           (filename, N, method, ww, iterations, paperdir+'/data/'+filename))
+    min_samples = 10000
+    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --ww %g --ff %g --N %d --min_T %g --%s --iterations %d --min_samples %d --filename_suffix golden > %s.out 2>&1 &" %
+           (filename, ww, ff, N, min_T, method, iterations, min_samples, data_dir+'/'+filename))
     print(cmd)
     os.system(cmd)
 
-num = int(sys.argv[1])
+ww = float(sys.argv[1])
 ff = float(sys.argv[2])
+N = int(sys.argv[3])
+min_T = 0.2
+method = 'tmmc'
 
-run_golden(num, ff, 1.3, 'tmmc')
+run_golden(ww, ff, N, min_T, method)
