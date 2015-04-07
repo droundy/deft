e5db624770e4ba46cbbd4b8dd50a9806f1fbc8fb
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-07 15:23:43 -0700

    added scripts to generate default data Tested on MAPHost. Took 1:20.15.

--- papers/histogram/run-golden.py
+++ papers/histogram/run-golden.py
@@ -2,11 +2,9 @@
 from __future__ import division
 import os, numpy, sys, re
 
-if len(sys.argv) != 4:
-    print "usage:  python %s ww ff N" % sys.argv[0]
+if len(sys.argv) != 5:
+    print "usage:  python2 %s ww ff min_T N" % sys.argv[0]
     exit(1)
-
-paperdir = 'papers/histogram'
 
 # switch to deft project directory and build SWMC
 filepath = os.path.abspath(__file__)
@@ -16,19 +14,20 @@ data_dir = paper_dir+'/data'
 os.chdir(deft_dir)
 os.system('fac square-well-monte-carlo')
 
-def run_golden(ww, ff, N, min_T, method):
-    filename = 'golden-%s-N%d-ff%.0f-ww%.0f' % (method, N, ff*100, ww*100)
+def run_golden(ww, ff, min_T, N, method):
+    out_fname = 'golden-%s-N%d-ff%.0f-ww%.0f' % (method, N, ff*100, ww*100)
     iterations = 10000000000
     min_samples = 10000
-    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --ww %g --ff %g --N %d --min_T %g --%s --iterations %d --min_samples %d --filename_suffix golden > %s.out 2>&1 &" %
-           (filename, ww, ff, N, min_T, method, iterations, min_samples, data_dir+'/'+filename))
+    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --ww %g --ff %g --min_T %g --N %d --%s --iterations %d --min_samples %d --filename_suffix golden > %s.out 2>&1 &"
+           % (out_fname, ww, ff, min_T, N, method, iterations, min_samples,
+              data_dir+'/'+out_fname))
     print(cmd)
     os.system(cmd)
 
 ww = float(sys.argv[1])
 ff = float(sys.argv[2])
-N = int(sys.argv[3])
-min_T = 0.2
+min_T = float(sys.argv[3])
+N = int(sys.argv[4])
 method = 'tmmc'
 
-run_golden(ww, ff, N, min_T, method)
+run_golden(ww, ff, min_T, N, method)
