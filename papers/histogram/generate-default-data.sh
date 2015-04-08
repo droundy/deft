5ab29185fc50364762bf432d589e8bc58065cee9
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-07 19:54:48 -0700

    fix bug in output file metadata

--- papers/histogram/generate-default-data.sh
+++ papers/histogram/generate-default-data.sh
@@ -5,11 +5,12 @@
 ff=0.3
 ww=1.3
 min_T=0.2
-method=simple_flat
-# method is one of: simple_flat wang_landau tmmc oetmmc
 
 echo using method: $method
-for N in `seq 5 20` 40 60 80 100 120 140 160 180 200 250 300; do
-  echo working on N=$N
-  python2 run-default.py $ww $ff $min_T $N $method
+for N in `seq 5 30`; do
+  echo '  working on N='$N
+  for method in simple_flat wang_landau tmmc oetmmc; do
+    echo working on $method
+    python2 run-default.py $ww $ff $min_T $N $method
+  done
 done
