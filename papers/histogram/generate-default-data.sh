36acdb305eef140bb8c13f96b167c1b757ba4466
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-10 09:50:40 -0700

    adding lots of data

--- papers/histogram/generate-default-data.sh
+++ papers/histogram/generate-default-data.sh
@@ -6,10 +6,9 @@ ff=0.3
 ww=1.3
 min_T=0.2
 
-echo using method: $method
-for N in `seq 5 30`; do
+for N in 70 80 90 100; do
   echo '  working on N='$N
-  for method in simple_flat wang_landau tmmc oetmmc; do
+  for method in simple_flat tmmc oetmmc; do
     echo working on $method
     python2 run-default.py $ww $ff $min_T $N $method
   done
