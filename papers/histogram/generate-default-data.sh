79ba70edbf2c6a002b224cb8d732c1bc7c2e662b
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-10 19:17:50 -0700

    added seeds to default data running scripts

--- papers/histogram/generate-default-data.sh
+++ papers/histogram/generate-default-data.sh
@@ -6,11 +6,13 @@ ff=0.3
 ww=1.3
 min_T=0.2
 
-for N in 55 60 70 80 90 100; do
-  echo '  working on N='$N
-  for method in simple_flat tmmc oetmmc; do
-  #for method in cfw wang_landau; do
-    echo working on $method
-    python2 run-default.py $ww $ff $min_T $N $method
+for N in `seq 5 30`; do
+  for seed in `seq 0 4`; do
+    echo '  working on N='$N
+    for method in simple_flat tmmc oetmmc; do
+      #for method in cfw wang_landau; do
+      echo working on $method
+      python2 run-default.py $ww $ff $min_T $N $method $seed
+    done
   done
 done
--- papers/histogram/run-default.py
+++ papers/histogram/run-default.py
@@ -3,7 +3,7 @@ from __future__ import division
 import os, numpy, sys, re
 
 if len(sys.argv) not in [6,7]:
-    print "usage:  python2 %s ww ff min_T N method [method_option]" % sys.argv[0]
+    print "usage:  python2 %s ww ff min_T N method [seed]" % sys.argv[0]
     exit(1)
 
 # switch to deft project directory and build SWMC
@@ -14,8 +14,8 @@ data_dir = paper_dir+'/data/'
 os.chdir(deft_dir)
 os.system('fac square-well-monte-carlo')
 
-def run_default(ww, ff, min_T, N, method):
-    out_fname = '%s-N%d-ff%.0f-ww%.0f' % (method, N, ff*100, ww*100)
+def run_default(ww, ff, min_T, N, method,seed):
+    out_fname = '%s-N%d-ff%.0f-ww%.0f-s%.3i' % (method, N, ff*100, ww*100, seed)
     iterations = 1e5*N*N
 
     if method == 'cfw':
@@ -23,8 +23,8 @@ def run_default(ww, ff, min_T, N, method):
                  os.path.abspath(data_dir + 'periodic-ww%.2f-ff%.2f-N%i'%(ww,ff,N) + \
                                  '-tmmc-golden-transitions.dat')
 
-    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --ww %g --ff %g --min_T %g --N %d --%s --iterations %d > %s.out 2>&1 &"
-           % (out_fname, ww, ff, min_T, N, method, iterations, data_dir+out_fname))
+    cmd = ("srun --mem=600 -J %s time nice -19 ./square-well-monte-carlo --ww %g --ff %g --min_T %g --N %d --%s --seed %i --iterations %d > %s.out 2>&1 &"
+           % (out_fname, ww, ff, min_T, N, method, seed, iterations, data_dir+out_fname))
     print(cmd)
     os.system(cmd)
 
@@ -33,5 +33,9 @@ ff = float(sys.argv[2])
 min_T = float(sys.argv[3])
 N = int(sys.argv[4])
 method = sys.argv[5]
+try:
+    seed = int(sys.argv[6])
+except:
+    seed = 0
 
-run_default(ww, ff, min_T, N, method)
+run_default(ww, ff, min_T, N, method, seed)
