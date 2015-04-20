60236c607d56b3883ec8da7c74f86a479c5ce012
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-19 23:34:40 -0700

    adding current histogram thesis and facfile to build it Tested on quipu. Took 0:25.83.

---
+++ papers/histogram/thesis/figs/square-well.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+plot_dims = (5,4)
+plot_scale = 0.7
+
+ww = 2.3
+r_max = 4
+v_max = 3
+v_min = -2
+
+top = 0.3
+bottom = -1
+
+plot_size = (plot_dims[0]*plot_scale,plot_dims[1]*plot_scale)
+<<<<<<< HEAD
+plt.figure(figsize=plot_size)
+
+plt.plot([1,1,ww,ww,r_max],[v_max,bottom,bottom,top,top],'k')
+plt.xlim(0,r_max)
+plt.ylim(v_min,v_max)
+
+plt.xticks([0,1,ww],['$0$','$\\sigma$','$\\lambda\\sigma$'])
+plt.yticks([top,bottom],['$0$','$-\epsilon$'])
+plt.xlabel('$r$')
+plt.ylabel('$v_{sw}$')
+plt.tight_layout()
+
+plt.savefig('figs/square-well.pdf')
+
