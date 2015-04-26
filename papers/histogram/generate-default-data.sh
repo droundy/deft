455a3705557e8b7961fe2de4d1e6a311ef184012
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-26 11:57:32 -0700

    adding hoards of new data

--- papers/histogram/generate-default-data.sh
+++ papers/histogram/generate-default-data.sh
@@ -6,13 +6,13 @@ ff=0.3
 ww=1.3
 min_T=0.2
 
-for N in `seq 5 30`; do
-  echo '    working on N='$N
-  for seed in 1; do
-    echo '  working on seed='$seed
-    for method in simple_flat tmmc oetmmc; do
-      #for method in cfw wang_landau; do
-      echo working on $method
+for N in `seq 27 30`; do
+  echo "\n\n\n    working on N=$N\n"
+  for seed in `seq 0 9`; do
+    echo "\n\n  working on seed=$seed\n"
+    #for method in simple_flat tmmc oetmmc; do
+    for method in cfw wang_landau; do
+      echo "\nworking on $method\n"
       python2 run-default.py $ww $ff $min_T $N $method $seed
     done
   done
--- papers/histogram/generate-golden-data.sh
+++ papers/histogram/generate-golden-data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-#set -ev
+set -ev
 
 ff=0.3
 ww=1.3
 min_T=0.1
 
-for N in 55 60 70 80 90 100; do
-    echo Working on N=$N
+for N in `seq 5 30`; do
+    echo "\nWorking on N=$N\n"
     python2 run-golden.py $ww $ff $min_T $N
 done
