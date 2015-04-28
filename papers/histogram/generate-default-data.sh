5151764c3a980543ce997fa21acf355315c72d2b
Michael A. Perlin <mika.perlin@gmail.com>
2015-04-28 01:19:08 -0700

    generate default data, accounting for the fact that not all golden simulations have finised

--- papers/histogram/generate-default-data.sh
+++ papers/histogram/generate-default-data.sh
@@ -6,14 +6,26 @@ ff=0.3
 ww=1.3
 min_T=0.2
 
-for N in `seq 27 30`; do
+min_N=5
+max_N=30
+max_golden_N=26
+
+for N in `seq $min_N $max_N`; do
   echo "\n\n\n    working on N=$N\n"
-  for seed in `seq 0 9`; do
+  for seed in `seq 18 24`; do
     echo "\n\n  working on seed=$seed\n"
-    #for method in simple_flat tmmc oetmmc; do
-    for method in cfw wang_landau; do
+
+    for method in simple_flat tmmc oetmmc; do
       echo "\nworking on $method\n"
       python2 run-default.py $ww $ff $min_T $N $method $seed
     done
+
+    if [ $N -le $max_golden_N ]; then
+      for method in cfw wang_landau; do
+        echo "\nworking on $method\n"
+        python2 run-default.py $ww $ff $min_T $N $method $seed
+      done
+    fi
+
   done
 done
