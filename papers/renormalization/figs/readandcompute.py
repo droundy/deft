8223f59864df1fe2e8594aaad51c99fa2d83de87
Brenden Vischer <vischerb@quipu.physics.oregonstate.edu>
2015-05-14 10:24:49 -0700

    Added in bare-bones ideal gas contribution to readandcompute in the renorm directory. Some bugs to fix still.

--- papers/renormalization/figs/readandcompute.py
+++ papers/renormalization/figs/readandcompute.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python2
 import numpy
+import math
 
 def T_u_cv_s_minT(fbase):
     max_T = 1.4
     T_bins = 1e3
     dT = max_T/T_bins
+    m = .25   # Need an appropriate scale for this - look at size of epsilon and sigma
+    V = 1000.0
+    N = 10.0
+
     T_range = numpy.arange(dT,max_T,dT)
     min_T = minT(fbase)
     # energy histogram file; indexed by [-energy,counts]
     e_hist = numpy.loadtxt(fbase+"-E.dat", ndmin=2)
     # weight histogram file; indexed by [-energy,ln(weight)]
     lnw_hist = numpy.loadtxt(fbase+"-lnw.dat", ndmin=2)
+
+
+    with open(fbase+"-E.dat") as file:
+        for line in file:
+            if("dimensions" in line):
+                V = (float(line.split()[-1]))**3
+            if(" N: " in line):
+                N = float(line.split()[-1])
+            break
+        
 
     energy = -e_hist[:,0] # array of energies
     lnw = lnw_hist[e_hist[:,0].astype(int),1] # look up the lnw for each actual energy
@@ -32,7 +47,8 @@ def T_u_cv_s_minT(fbase):
         # S = \sum_i^{microstates} P_i \log P_i
         # S = \sum_E D(E) e^{-\beta E} \log\left(\frac{e^{-\beta E}}{\sum_{E'} D(E') e^{-\beta E'}}\right)
         S[i] = sum(-dos_boltz*(-energy/T_range[i] - ln_dos_boltz.max() \
-                                       - numpy.log(Z[i])))/Z[i]
+                                       - numpy.log(Z[i])))/Z[i] \
+                                       + numpy.log((V/N)*(m*T_range[i]/(2*numpy.pi))**(.5)) + 5/2.0
         # Actually compute S(T) - S(T=\infty) to deal with the fact
         # that we don't know the actual number of eigenstates:
         S[i] -= S_inf
