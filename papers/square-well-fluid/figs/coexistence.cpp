f96cd3d4b66a6d5f6631acb0eb9adf3d7d12f4c4
David Roundy <roundyd@physics.oregonstate.edu>
2015-06-11 08:28:35 -0700

    several improvements to the still-buggy coexistence code Tested on darcy. Took 1:03.96.
    Tested on darcy. Took 0:56.65.

--- papers/square-well-fluid/figs/coexistence.cpp
+++ papers/square-well-fluid/figs/coexistence.cpp
@@ -1,7 +1,7 @@
 // Deft is a density functional package developed by the research
 // group of Professor David Roundy
 //
-// Copyright 2010 The Deft Authors
+// Copyright 2015 The Deft Authors
 //
 // Deft is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -20,7 +20,6 @@
 #include <sys/types.h>
 
 #include "new/HomogeneousSW_liquidFast.h"
-#include "version-identifier.h"
 #include "handymath.h"
 
 // The following tells fac how to run the executable to generate an
@@ -28,10 +27,13 @@
 /*--
 
 for ww in [1.3]:
-    self.rule('%s %g %g' % (exe,ww,kT), [exe],
+    self.rule('%s %g' % (exe,ww), [exe],
               [os.path.dirname(exe)[:-5]+'/data/coexistence/ww%g.dat' % ww])
 
 --*/
+
+const double dT = 0.01;
+
 const double epsilon = 1.0;
 const double radius = 1.0;
 const double sigma = 2*radius;
@@ -43,14 +45,15 @@ enum direction { MINIMIZE, MAXIMIZE };
 static inline bool is_improvement(enum direction d, double eold, double enew) {
   if (eold != eold) return true;
   if (d == MINIMIZE) {
-    return !(enew > eold);
+    return enew < eold;
   } else {
-    return !(enew < eold);
+    return enew > eold;
   }
 }
 
 static inline double energy(const HomogeneousSW_liquid &f, double n) {
   f.n() = n;
+  f.invalidate_cache();
   return f.energy();
 }
 
@@ -58,14 +61,21 @@ static inline double energy(const HomogeneousSW_liquid &f, double n) {
 // border, and nmid is a flexible estimate.  It's confusing, but it
 // also saves writing two very separate cases.
 double optimize(const HomogeneousSW_liquid &hf, enum direction d, double nlo, double nmid) {
+  if (d == MINIMIZE) {
+    printf("\n\nI am minimizing between %g and %g\n", nlo, nmid);
+  } else {
+    printf("\n\nI am maximizing between %g and %g\n", nlo, nmid);
+  }
   double nhi, emid, elo, ehi;
   elo = energy(hf, nlo);
   emid = energy(hf, nmid);
   if (is_improvement(d, elo, emid)) {
     // great, now we just need to find a high energy!
-    nhi = 3*nmid - nlo;
+    nhi = nmid + (nmid - nlo);
     ehi = energy(hf, nhi);
     while (is_improvement(d, emid, ehi)) {
+      printf("n is beyond: %10g %10g %10g\n", nlo, nmid, nhi);
+      printf("   energies: %10g %10g %10g\n\n", elo, emid, ehi);
       nlo = nmid;
       elo = emid;
       nmid = nhi;
@@ -77,6 +87,8 @@ double optimize(const HomogeneousSW_liquid &hf, enum direction d, double nlo, do
     do {
       ehi = emid;
       nhi = nmid;
+      printf("n is between: %10g %10g\n", nlo, nhi);
+      printf("    energies: %10g %10g\n", elo, ehi);
       nmid = 0.7*nlo + 0.3*nmid;
       emid = energy(hf, nmid);
     } while (is_improvement(d, emid, elo));
@@ -88,6 +100,8 @@ double optimize(const HomogeneousSW_liquid &hf, enum direction d, double nlo, do
     t = nlo; nlo = nhi; nhi = t;
     t = elo; elo = ehi; ehi = t;
   }
+  printf("n bracketed: %10g %10g %10g\n", nlo, nmid, nhi);
+  printf("   energies: %10g %10g %10g\n", elo, emid, ehi);
   do {
     if (nmid < 0.5*(nhi+nlo)) {
       const double nnew = 0.6*nhi + 0.4*nlo;
@@ -115,6 +129,8 @@ double optimize(const HomogeneousSW_liquid &hf, enum direction d, double nlo, do
       }
     }
   } while (fabs(nhi - nlo) > naccuracy);
+  printf("Final n: %10g\n", 0.5*(nhi+nlo));
+  printf(" energy: %10g\n", energy(hf, 0.5*(nhi+nlo)));
 
   return 0.5*(nhi+nlo);
 }
@@ -132,16 +148,26 @@ void identify_liquid_vapor(const HomogeneousSW_liquid &hf,
     // Find estimates for the liquid and vapor densities.
     *nv = optimize(hf, MINIMIZE, *middlen, 0);
     ev = energy(hf, *nv);
-    *nl = optimize(hf, MINIMIZE, *middlen, 10*(*middlen));
+    printf("  got nv: %10g\n", *nv);
+    printf("  energy: %10g\n", ev);
+    *nl = optimize(hf, MINIMIZE, *middlen, max(2*(*nl), 3*(*middlen)));
     el = energy(hf, *nl);
+    printf("  got nl: %10g\n", *nl);
+    printf("  energy: %10g\n", el);
     // Adjust the chemical potential to make the free energies at the
     // two minima equal.
-    hf.mu() += (ev - el)/(nv-nl);
+    hf.mu() += (ev - el)/(*nv - *nl);
     // Now we find the new middle point that maximizes the free
     // energy.
-    if (el > ev) *middlen = optimize(hf, MAXIMIZE, *nl, *nv);
-    else *middlen = optimize(hf, MAXIMIZE, *nv, *nl);
+    *middlen = optimize(hf, MAXIMIZE, *nl, *middlen);
     emid = energy(hf, *middlen);
+    printf("  got nm: %10g\n", *middlen);
+    printf("  energy: %10g\n", emid);
+
+    // for (double nn=*nv/2; nn < 1.1*(*nl); nn *= 1.2) {
+    //   printf("    %10g %10g\n", nn, energy(hf, nn));
+    // }
+
   } while (fabs(ev - el)/fabs(emid-el) > tol);
 }
 
@@ -151,7 +177,6 @@ int main(int argc, char **argv) {
     printf("usage: %s lambda\n", argv[0]);
     return 1;
   }
-  printf("git version: %s\n", version_identifier());
 
   sscanf(argv[1], "%lg", &lambda);
 
@@ -162,25 +187,38 @@ int main(int argc, char **argv) {
   sprintf(fname, "papers/square-well-fluid/data/coexistence/ww%g.dat", lambda);
   FILE *out = fopen(fname, "w");
   assert(out);
-  delete[] fname;
 
   double eta_middle = 0.3;
   double n_middle = eta_middle/(M_PI*uipow(sigma, 3)/6);
-  const double dff = 0.01, dT = 0.1;
-  for (double temp = 0.1; temp < 10; temp += dT) {
-    for (double ff = dff; ff < 0.7; ff += dff) {
-      HomogeneousSW_liquid hf;
-      hf.R() = radius;
-      hf.epsilon() = epsilon;
-      hf.kT() = temp;
-      hf.lambda() = lambda;
-      double nl, nv;
-      identify_liquid_vapor(hf, &n_middle, &nl, &nv);
-      printf("kT %g nv %g nm %g nl %g\n", temp, nl, n_middle, nv);
-      fprintf(out, "%g\t%g\t%g\t%g\n", temp, nl, n_middle, nv);
+  for (double temp = dT; temp < 5; temp += dT) {
+    HomogeneousSW_liquid hf;
+    hf.R() = radius;
+    hf.epsilon() = epsilon;
+    hf.kT() = temp;
+    hf.lambda() = lambda;
+    double nl, nv;
+    identify_liquid_vapor(hf, &n_middle, &nl, &nv);
+    printf("\n\n**********************************************\n");
+    printf("kT %g nl %g nm %g nv %g\n", temp, nl, n_middle, nv);
+    printf("**********************************************\n");
+    if (isnan(n_middle) || n_middle < nv || n_middle > nl) {
+      printf("Got craziness, I am quitting now.\n");
+      exit(0);
     }
+    fprintf(out, "%g\t%g\t%g\t%g\n", temp, nl, n_middle, nv);
+    fflush(out);
+
+    sprintf(fname, "papers/square-well-fluid/data/coexistence/ww%g-kT%g.dat",
+            lambda, temp);
+    FILE *ff = fopen(fname, "w");
+    assert(ff);
+    for (double nn=0.1*nv; nn<1.1*nl; nn *= 1.01) {
+      fprintf(ff, "%g\t%g\n", nn, energy(hf, nn));
+    }
+    fclose(ff);
   }
 
   fclose(out);
+  delete[] fname;
   return 0;
 }
---
+++ papers/square-well-fluid/figs/plot-coexistence.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2
+import matplotlib, sys
+if 'show' not in sys.argv:
+    matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+matplotlib.rc('text', usetex=True)
+
+import readandcompute
+
+ww = float(sys.argv[1])
+#arg ww = [1.3]
+kTslices = eval(sys.argv[2])
+#arg kTslices = [[1,2,3,4]]
+
+plt.figure()
+
+data = np.loadtxt('data/coexistence/ww%g.dat' % ww)
+plt.plot(data[:,0], data[:,1])
+plt.plot(data[:,0], data[:,2])
+plt.plot(data[:,0], data[:,3])
+
+#input: ['data/coexistence/ww%g-kT%g.dat' % (ww, kT) for kT in kTslices]
+for kT in kTslices:
+    fname = 'data/coexistence/ww%g-kT%g.dat' % (ww, kT)
+    ugh = np.loadtxt(fname)
+    plt.plot(kT + ugh[:,1] - ugh[:,1].min(), ugh[:,0], ':')
+
+plt.xlabel(r'$T$')
+plt.ylabel(r'$n$')
+
+plt.savefig('figs/coexistence-ww%g.pdf' % (ww))
+
+plt.show()
