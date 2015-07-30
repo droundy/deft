f2d8e4c41393bc323a66b267343bd822c170622f
Brenden Vischer <vischerb@elliot.physics.oregonstate.edu>
2015-07-29 22:07:10 -0700

    Started adding cache and overwrite functionality to the free energy script. Will implement for overall run script at a later date.

--- papers/renormalization/data/run-absolute.py
+++ papers/renormalization/data/run-absolute.py
@@ -4,6 +4,7 @@ from __future__ import division
 from math import pi       # REALLY don't need all of math
 import os, numpy, sys
 
+os.system("fac ../../../free-energy-monte-carlo")
 
 R=1
 
@@ -16,14 +17,18 @@ ww=1.3
 L=5
 #arg L = [5.0]
 
-Ns =list(xrange(0,2))  #list(xrange(0,4**(i+1)+1))
+Ns = list(xrange(0,2))
 #arg Ns = [range(2,10)]
 
+Overwrite = False
+if "--O" in sys.argv:  # Check for overwrite flag in arguments
+    Overwrite == True
+
+    
 for N in Ns:
-    dirname = 'scrunched/i%01d/N%03d/absolute/'
-    os.system('mkdir -p'+dirname)
-    filename = 'absolute-ww%4.2f-L%04.2f-N%03d'
-    filename+= '-g.dat'
+    dirname = 'scrunched/i%01d/N%03d/absolute' % (i,N)
+    os.system('mkdir -p '+dirname)
+    filename = 'absolute-ww%4.2f-L%04.2f-N%03d' % (ww,L,N)
     
     ffs = []
     success_ratios = []
@@ -36,23 +41,22 @@ for N in Ns:
     sim_iters = 1000000
     sc_period = int(max(10, 1*N*N/10))
     
-    
-    for j in xrange(steps):
-        if j!= 0 and j % 4 == 0:
-            step_size = step_size * 0.5
-        cmd = 'srun -J %s' % filename
-        cmd += ' ../../../free-energy-monte-carlo'
-        cmd += ' --ff %g' % ff
-        cmd += ' --sc_period %d' % sc_period
-        cmd += ' --iterations %d' % sim_iters
-        cmd += ' --filename %s' % filename
-        cmd += ' --data_dir %s' % dirname
-        cmd += ' --ff_small %g' % (ff+step_size)
-        cmd += ' --N %d' % N
-
-        print("Running with command: %s") % cmd
-        os.system(cmd)
-        #Not sure about the cacheing step... we'll see this
-        #tomorrow and talk about it
-
-        
+    if (not os.path.isfile(dirname+'/'+filename+'-g.dat'))  or \
+       all(os.path.isfile(dirname+'/'+filename+'-g.dat'), Overwrite == True):
+        for j in xrange(steps):
+            if j!= 0 and j % 4 == 0:
+                step_size = step_size * 0.5
+                cmd = 'srun -J %s' % filename
+                cmd += ' ../../../free-energy-monte-carlo'
+                cmd += ' --ff %g' % ff
+                cmd += ' --sc_period %d' % sc_period
+                cmd += ' --iterations %d' % sim_iters
+                cmd += ' --filename %s' % filename
+                cmd += ' --data_dir %s' % dirname
+                cmd += ' --ff_small %g' % (ff+step_size)
+                cmd += ' --N %d' % N
+                cmd += ' > %s/%s.out 2>&1 &' % (dirname, filename) 
+                print("Running with command: %s" % cmd)
+                os.system(cmd)
+                # cmd = ("../../../free-energy-monte-carlo --ff %g --sc_period %d --iterations %d --filename %s --data_dir %s --ff_small %g --N %d > %s/%s.out 2>&1" %
+                #       (ff,sc_period,sim_iters,filename,dirname,(ff+step_size), N, dirname, filename)
