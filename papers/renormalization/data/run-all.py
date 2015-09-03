70b74615f81fb80d30b086b31d62d908d2b328ef
David Roundy <roundyd@physics.oregonstate.edu>
2015-09-03 15:19:35 -0400

    fix bug Brenden told me about Tested on darcy. Took 1:48.63.

--- papers/renormalization/data/run-all.py
+++ papers/renormalization/data/run-all.py
@@ -18,19 +18,19 @@ Ns = eval(sys.argv[4])
 
 
 if '-O' in sys.argv:  # Check for overwrite flag in arguments
-    cmd_fe = 'python run-absolute.py %d %g %d "%s" -O' % (i,ww,L,Ns)
+    cmd_fe = 'python run-absolute.py %d %g %g "%s" -O' % (i,ww,L,Ns)
     print(cmd_fe)
     os.system(cmd_fe)
 
-    cmd_mc = "python run-monte-carlo.py %d %g %d '%s'" %(i,ww,L,Ns)
+    cmd_mc = "python run-monte-carlo.py %d %g %g '%s'" %(i,ww,L,Ns)
     print(cmd_mc)
     os.system(cmd_mc)
 else:
-    cmd_fe = 'python run-absolute.py %d %g %d "%s"' % (i,ww,L,Ns)
+    cmd_fe = 'python run-absolute.py %d %g %g "%s"' % (i,ww,L,Ns)
     print(cmd_fe)
     os.system(cmd_fe)
     
-    cmd_mc = "python run-monte-carlo.py %d %g %d '%s'" %(i,ww,L,Ns)
+    cmd_mc = "python run-monte-carlo.py %d %g %g '%s'" %(i,ww,L,Ns)
     print(cmd_mc)
     os.system(cmd_mc)
 
