47bf1f75eed95b44a24b8eb8dbb20455d62afd69
David Roundy <roundyd@physics.oregonstate.edu>
2015-11-12 14:05:16 -0500

    print handy messages at least every half hour Tested on darcy. Took 1:37.52.

--- src/Monte-Carlo/square-well.cpp
+++ src/Monte-Carlo/square-well.cpp
@@ -1491,7 +1491,7 @@ double sw_simulation::estimate_trip_time(int E1, int E2) {
 }
 
 bool sw_simulation::printing_allowed(){
-  const double max_time_skip = 60*60; // 1 hour
+  const double max_time_skip = 60*30; // 1/2 hour
   static double time_skip = 3; // seconds
   static int every_so_often = 0;
   static int how_often = 1;
