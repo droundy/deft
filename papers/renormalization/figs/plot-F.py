c9cd5f8670a3a49c7a01580b757ac6e6792763e9
vischerb <br.vischer@gmail.com>
2016-01-15 10:45:15 -0800

    started F versus T fixed eta plots Tests skipped!

--- papers/renormalization/figs/plot-F.py
+++ papers/renormalization/figs/plot-F.py
@@ -4,6 +4,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
+# This code potentially needs 
+
 matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 matplotlib.rc('text', usetex=True)
 
@@ -13,6 +15,8 @@ import readandcompute # needed for many functions, look at this if you haven't!
 if len(sys.argv) != 5:
     print 'useage: %s i ww L [Ns] RECURSION-LEVEL WELL-WIDTH LENGTH [SPHERES]' % sys.argv[0]
     exit(1)
+
+# Necessary constants and parameters 
 
 R = 1 # sphere radius
 
@@ -35,22 +39,26 @@ max_T = Ts[-1]
 
 styles = ['ro','bo','rx','bx','gx']
 
-plot_all = False
 
 if len(sys.argv) < 5:
+    # If no Ns are given, plot everything we have
     plot_all = True
 else:
+    # Choose plot type here. Might make this an argument, but seems unnecessary
+    plot_F_T = True
     Ns = eval(sys.argv[4])
     #arg Ns = [range(0,10)]
-
+ 
     
 if plot_all:
-    for T in set(Ts):
+    # Is this necessary?
+    for l in range(0. len(Ts)):
         eta, U, F, CV, S, min_T = readandcompute.eta_u_F_cv_s_minT('data/scrunched-ww%04.2f-L%04.2f' % (ww, L), T, max_T)
         plt.figure('F-all')
         plt.plot(eta, F, styles[j], label="$T=%02.1f$" % T)
+        plt.xlabel('$\eta$')
         
-else:
+elif plot_F_eta:
     for N in set(Ns):
         eta = N*(4*np.pi/3*R**3/(L*2**i)**3)
         for j in range(0,len(Ts)):            
@@ -61,12 +69,21 @@ else:
             plt.figure('F-Fix_i')
             if N == Ns[0]:
                 #hacky label magic
-                plt.plot(eta, F[q], styles[j%5], label="$T=%02.1f$" % Ts[j])
+                plt.plot(eta, F[q], styles[j%len(styles)], label="$T=%02.1f$" % Ts[j])
             else:
-                plt.plot(eta, F[q], styles[j%5])
-    plt.figure('F-Fix_i')
+                plt.plot(eta, F[q], styles[j%len(styles)])
+
+    plt.figure('F-eta')
+    plt.xlabel('$\eta$')
+elif plot_F_T:
+    etas = np.sort(map(lambda N: N*(4*np.pi/3*R**3/(L*2**i)**3), Ns))
+    for k in range(0,len(etas))
+        T, U, F, CV, S, min_T = readandcompute.T_u_F_cv_s_minT('data/scrunched-ww%04.2f-L%04.2f/i%01d/N%03d/data' % (ww, L,  i, Ns[k]), max_T)
+        plt.figure('F-T')
+        plt.plot(T, F, styles[j%len(styles)],label= "$\eta=%01.2f" % etas[k])
+        
 plt.title('Absolute free energies for $\lambda=%g$, $L=%g$ and $i=%d.$' % (ww, L, i))
-plt.xlabel('$\eta$')
+
 plt.ylabel('$F/N\epsilon$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0.2)
