540e5a8c4059ce10ae0ba8a209ae1bd80da93b93
vischerb <br.vischer@gmail.com>
2016-01-18 14:34:29 -0800

    finished F-T plotting script Tests skipped!

--- papers/renormalization/figs/plot-F.py
+++ papers/renormalization/figs/plot-F.py
@@ -37,8 +37,9 @@ max_T = Ts[-1]
 # Fixed T for plots
 
 
-styles = ['ro','bo','rx','bx','gx']
+styles = ['ro','bo','g-','c-']
 
+plot_all = plot_F_T = plot_F_eta = False
 
 if len(sys.argv) < 5:
     # If no Ns are given, plot everything we have
@@ -52,12 +53,12 @@ else:
     
 if plot_all:
     # Is this necessary?
-    for l in range(0. len(Ts)):
+    for l in range(0, len(Ts)):
         eta, U, F, CV, S, min_T = readandcompute.eta_u_F_cv_s_minT('data/scrunched-ww%04.2f-L%04.2f' % (ww, L), T, max_T)
         plt.figure('F-all')
         plt.plot(eta, F, styles[j], label="$T=%02.1f$" % T)
         plt.xlabel('$\eta$')
-        
+        output_filename = "figs/ww%02.0f-L%02.0f-F_i%01d_F-eta_all.pdf" % (ww*100, L, i)
 elif plot_F_eta:
     for N in set(Ns):
         eta = N*(4*np.pi/3*R**3/(L*2**i)**3)
@@ -72,22 +73,28 @@ elif plot_F_eta:
                 plt.plot(eta, F[q], styles[j%len(styles)], label="$T=%02.1f$" % Ts[j])
             else:
                 plt.plot(eta, F[q], styles[j%len(styles)])
-
+                
+    output_filename = "figs/ww%02.0f-L%02.0f-i%01d_F-eta.pdf" % (ww*100, L, i)
     plt.figure('F-eta')
     plt.xlabel('$\eta$')
+    
 elif plot_F_T:
     etas = np.sort(map(lambda N: N*(4*np.pi/3*R**3/(L*2**i)**3), Ns))
-    for k in range(0,len(etas))
+    for k in range(0,len(etas)):
         T, U, F, CV, S, min_T = readandcompute.T_u_F_cv_s_minT('data/scrunched-ww%04.2f-L%04.2f/i%01d/N%03d/data' % (ww, L,  i, Ns[k]), max_T)
         plt.figure('F-T')
-        plt.plot(T, F, styles[j%len(styles)],label= "$\eta=%01.2f" % etas[k])
+        plt.plot(T, F, styles[k%len(styles)],label= "$\eta=%01.2f$" % etas[k])
+    plt.xlabel('$T/\epsilon$')
+    plt.axvline(x=min_T)
+    output_filename = "figs/ww%02.0f-L%02.0f-i%01d_F-T.pdf" % (ww*100, L, i)
         
 plt.title('Absolute free energies for $\lambda=%g$, $L=%g$ and $i=%d.$' % (ww, L, i))
 
 plt.ylabel('$F/N\epsilon$')
 plt.legend(loc='best')
 plt.tight_layout(pad=0.2)
-output_filename = "figs/ww%02.0f-L%02.0f-F_i%01d_fixed_i.pdf" % (ww*100, L, i)
+
+
 plt.savefig(output_filename)
 
 
