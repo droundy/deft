31eaf35876f02b1b98382c6da423caab99126fda
David Roundy <roundyd@physics.oregonstate.edu>
2016-04-11 12:56:21 -0700

    bug fixes in gatherandcalculate

--- papers/renormalization/figs/gatherandcalculate.py
+++ papers/renormalization/figs/gatherandcalculate.py
@@ -5,74 +5,133 @@ import math
 import string
 import os
 
-def dos_energy(dbase, i):
+def lndos_energy_Ns(dbase):
     ln_dos = {}
     energy = {}
     ln_dos_hist = [] # need to initialize this correctly; how many elements?
+    Ns = []
     for N in range(2,100): #maybe require N as an argument?
-        fbase = '%s/%01d/N%03d/lv-data-dos.dat' % (dbase, i, N)
-        if os.path.isfile(fbase+"-dos.dat"):
-            ln_dos_hist = np.loadtext(fbase, ndmin=2)
+        fname = '%s/N%03d/lv-data-dos.dat' % (dbase, N)
+        if os.path.isfile(fname):
+            ln_dos_hist = np.loadtxt(fname, ndmin=2)
             ln_dos[N] = ln_dos_hist[:,1]
             energy[N] = ln_dos_hist[:,0] #slicing may be wrong, need to look at this harder. Also, if a file doesn't exist, index is no longer =N...
+            Ns.append(N)
         else:
             pass
-    return ln_dos, energy
+    return ln_dos, energy, np.array(Ns)
 
-def F_hardsphere(dbase, N):
-    fbase = dbase + '/absolute/'
-    F = 0
+def Sexc_hardsphere_Ns(dbase):
+    Ns = []
+    S = []
+    for N in range(2,100): # maybe go higher?
+        fbase = '%s/N%03d/absolute/' % (dbase, N)
+        if os.path.isfile(fbase+'00000.dat'):
+            try:
+                thisS = Sexc_hardsphere(dbase, N)
+                S.append(thisS)
+                Ns.append(N)
+            except:
+                print 'no data for N =', N
+        else:
+            print 'no such file as', fbase+'00000.dat'
+    return np.array(S), np.array(Ns)
+
+def Sexc_hardsphere(dbase, N):
+    fbase = '%s/N%03d/absolute/' % (dbase, N)
+    S = 0
     # loop over files in the ./absolute/ directory
-    for j in range(0, 400):
+    j = 0
+    while True:
         valid = 0
         total = 0
         ratio = 0
-        fname = fbase + '%05d' % (j)
+        fname = fbase + '%05d.dat' % (j)
         # if file exists, load the text from the file
-        if os.path.isfile(fname+'.dat'):
+        if os.path.isfile(fname):
             #open file and read in total valid and failed checks of small cell
-            with (fname+'.dat') open as file:
+            with open(fname) as file:
                 for line in file:
                     if 'valid small checks:' in line:
-                        valid = line.split()[-1]
-                    if 'total small checks:' in line:
-                        total = line.split()[-1]
-            # compute absolute F using read in data.
+                        valid = int(line.split()[-1])
+                    if 'total checks of small cell:' in line:
+                        total = int(line.split()[-1])
+            # compute absolute S using read in data.
             ratio = valid / total
-            F += -np.log(ratio)/ N
+            S += -np.log(ratio)/ N
+        else:
+            # we have found the last data file and can stop now
+            break
+        j += 1
+    return S
+
+def Uexc(ln_dos, energy, Ts):
+    Ns = ln_dos.keys()
+    U = np.zeros((len(Ts), len(Ns)))
+    for j in range(len(Ns)):
+        N = Ns[j]
+        for k in range(len(Ts)):
+            T = Ts[k]
+            ln_dos_boltz = ln_dos[N] - energy[N]/T
+            # Subtract of ln_dos_boltz.max() to keep dos_boltz reasonable
+            dos_boltz = np.exp(ln_dos_boltz - ln_dos_boltz.max())
+            U[k,j] = sum(energy[N]*dos_boltz)/sum(dos_boltz)
+    return U
+
+def Fexc(dbase, ln_dos, energy, Ts):
+    Ns = ln_dos.keys()
+    F = np.zeros((len(Ts), len(Ns)))
+    for j in range(len(Ns)):
+        N = Ns[j]
+        Sexc_HS = Sexc_hardsphere(dbase, N)
+        for k in range(len(Ts)):
+            T = Ts[k]
+            ln_dos_boltz = ln_dos[N] - energy[N]/T
+            # Subtract of ln_dos_boltz.max() to keep dos_boltz reasonable
+            dos_boltz = np.exp(ln_dos_boltz - ln_dos_boltz.max())
+            Z = sum(dos_boltz)
+            Zinf = sum(np.exp(ln_dos[N] - ln_dos_boltz.max()))
+            F[k,j] = -T*Sexc_HS - T*np.log(Z/Zinf)
     return F
 
-def U_F_S_ns(dbase, i):
-    # Main function; take input of data directory and return all possible quantities
-    ln_dos, energy = dos_energy(dbase, i)
-    Ns = eval(ln_dos.keys())
-    T_bins, dT, T_range = 1e3, 1/T_bins, np.arange(dT, 1, dT) # change Tmax # Generate array of temperatures
-    Z = np.zeros(len(T_range), len(Ns))
-    Zinf, U, F, S = (np.zeros_like(Z) for i in range(4))
+def Sexc(Uexc, Fexc, Ts):
+    S = np.zeros_like(Uexc)
+    for k in range(len(Ts)):
+        T = Ts[k]
+        S[k,:] = (Uexc[k,:] - Fexc[k,:])/T
+    return S
+
+# def U_F_S(dbase, i):
+#     # Main function; take input of data directory and return all possible quantities
+#     ln_dos, energy = dos_energy(dbase, i)
+#     Ns = eval(ln_dos.keys())
+#     T_bins, dT, T_range = 1e3, 1/T_bins, np.arange(dT, 1, dT) # change Tmax # Generate array of temperatures
+#     Z = np.zeros(len(T_range), len(Ns))
+#     Zinf, U, F, S = (np.zeros_like(Z) for i in range(4))
     
-    for j in range(len(Ns)): # Set iteration readability is great, but defining separate index seems sloppy...
-        N = Ns[j]
-        F_HS = F_hardsphere(dbase, N)
-        for k in range(len(T_range)):
-            # all computed quantities are excess, with the exception of final entropy (includes ideal gas)
-            T = T_range[k]
-            ln_dos_boltz = ln_dos[N] - energy[N]/T
-            dos_boltz = np.exp(ln_dos_boltz - ln_dos_boltz.max()) #overflow/underflow issues, need to keep lndos reasonable
-            Z[k,j] = sum(dos_boltz)  #indexed by [T, N]
-            U[k,j] = sum(energy[N]*dos_boltz)/Z[k,j]
-            F[k,j] = -T*np.log(Z[k,j]) fixme need max here? also ideal gas?
+#     for j in range(len(Ns)): # Set iteration readability is great, but defining separate index seems sloppy...
+#         N = Ns[j]
+#         F_HS = F_hardsphere(dbase, N)
+#         for k in range(len(T_range)):
+#             # all computed quantities are excess, with the exception of final entropy (includes ideal gas)
+#             T = T_range[k]
+#             ln_dos_boltz = ln_dos[N] - energy[N]/T
+#             dos_boltz = np.exp(ln_dos_boltz - ln_dos_boltz.max()) #overflow/underflow issues, need to keep lndos reasonable
+#             Z[k,j] = sum(dos_boltz)  #indexed by [T, N]
+#             U[k,j] = sum(energy[N]*dos_boltz)/Z[k,j]
+#             F[k,j] = -T*np.log(Z[k,j]) fixme need max here? also ideal gas?
 
-            # make absolute by equating entropy at T=inf. note that F_{ex,HS} \prop  S_{ex,HS}
-            # need to take ln of sum of e^{ln_dos} to find number of states
+#             # make absolute by equating entropy at T=inf. note that F_{ex,HS} \prop  S_{ex,HS}
+#             # need to take ln of sum of e^{ln_dos} to find number of states
             
-            S_SW = np.log(sum(np.exp(ln_dos[N]))) fixme deal with max of ln_dos
-            S_HS = F_HS
-            # F_{ex,HS} = -TS_HS; F_SW = U - TS_{ex,SW}
-            # lim_{T \rightarrow \infty} U_{ex, SW} = constant; \therefore F_{ex,SW}(T=\infty) = -TS_{ex, SW }
+#             S_SW = np.log(sum(np.exp(ln_dos[N]))) fixme deal with max of ln_dos
+#             S_HS = F_HS
+#             # F_{ex,HS} = -TS_HS; F_SW = U - TS_{ex,SW}
+#             # lim_{T \rightarrow \infty} U_{ex, SW} = constant; \therefore F_{ex,SW}(T=\infty) = -TS_{ex, SW }
             
-            S_SW -= (S_SW - S_HS)
-            F_SW = -T*(S_SW + 3/2*N)
-            # Now, F_{ex, SW} is the absolute free energy required for the configuration. So, make calculated F absolute!
-            F[k,j] -= (F[k,j] - F_SW)
+#             S_SW -= (S_SW - S_HS)
+#             F_SW = -T*(S_SW + 3/2*N)
+#             # Now, F_{ex, SW} is the absolute free energy required for the configuration. So, make calculated F absolute!
+#             F[k,j] -= (F[k,j] - F_SW)
 
-    return U, F, S
+#     return U, F, S
---
+++ papers/renormalization/figs/plot-new.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python2
+from __future__ import division
+import matplotlib, sys
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy
+import numpy as np
+
+matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+# matplotlib.rc('text', usetex=True)
+
+import gatherandcalculate
+
+all_colors = ['r','g','b','k','c','m','y']
+
+temperature_color = {}
+eta_color = {}
+linetype = {
+    1: '-',
+    2: '--',
+}
+
+L = 2.84
+ww = 1.3
+R = 1
+i_values = [1,2]
+next_color = 0
+for i in i_values:
+    print 'working on iteration', i
+    dbase = 'data/scrunched-ww%04.2f-L%04.2f/i%01d' % (ww, L,  i)
+    lndos, energy, Ns = gatherandcalculate.lndos_energy_Ns(dbase)
+
+    V = (L*2**i)**3
+    etas = Ns*4*np.pi/3*R**3/V
+
+    Ts = np.arange(0.5, 2.0, 0.05)
+    Uexc = gatherandcalculate.Uexc(lndos, energy, Ts)
+    Fexc = gatherandcalculate.Fexc(dbase, lndos, energy, Ts)
+    Sexc = gatherandcalculate.Sexc(Uexc, Fexc, Ts)
+
+    plt.figure('Fexc-T')
+    for j in range(len(Ns)):
+        modulo = 4*8**(i-1)
+        if Ns[j] % modulo == 0:
+            val = Ns[j] // modulo
+            print 'foo', Ns[j], val, etas[j]
+            c = all_colors[(Ns[j] // modulo) % len(all_colors)]
+            if i == i_values[0]:
+                plt.plot(Ts, Fexc[:,j]/V, linetype[i]+c, label=r'$\eta = %.2g$' % etas[j])
+            else:
+                plt.plot(Ts, Fexc[:,j]/V, linetype[i]+c)
+
+    plt.figure('Fexc-eta')
+    for k in range(0,len(Ts),5):
+        if Ts[k] in temperature_color:
+            plt.plot(etas, Fexc[k,:]/V, linetype[i]+temperature_color[Ts[k]])
+        else:
+            temperature_color[Ts[k]] = all_colors[next_color]
+            next_color = (next_color+1) % len(all_colors)
+            plt.plot(etas, Fexc[k,:]/V, linetype[i]+temperature_color[Ts[k]],
+                     label=r'$T = %g$' % Ts[k])
+
+    plt.figure('HS')
+    Sexcs, NNs = gatherandcalculate.Sexc_hardsphere_Ns(dbase)
+    print Sexcs
+    plt.plot(NNs*4*np.pi/3*R**3/V, 100*Sexcs/V, linetype[i]+'k')
+
+
+plt.figure('HS')
+plt.title('Excess hard-sphere entropy for $L=%g$' % (L))
+plt.xlabel('$\eta$')
+plt.ylabel(r'$S/V$')
+plt.legend(loc='best')
+plt.savefig("figs/Shs-vs-eta.pdf")
+
+plt.figure('Fexc-eta')
+plt.title('Absolute free energies for $\lambda=%g$, $L=%g$' % (ww,L))
+plt.xlabel('$\eta$')
+plt.ylabel(r'$F/\epsilon V$')
+plt.legend(loc='best')
+plt.savefig("figs/Fexc-vs-eta.pdf")
+
+plt.figure('Fexc-T')
+plt.title('Excess free energies for $\lambda=%g$, $L=%g$' % (ww, L))
+plt.xlabel('$kT/\epsilon$')
+plt.ylabel(r'$F/\epsilon V$')
+plt.legend(loc='best')
+plt.savefig("figs/Fexc-vs-T.pdf")
+
