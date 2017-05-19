3ba9f02ca165fb83e2ed2c58f54cc8679472fc12
Jordan Pommerenck <jordan@wentworth.physics.oregonstate.edu>
2017-05-19 09:24:51 -0700

    changed python file to plot tmi2 data

--- papers/histogram/figs/plot-sticky-wall.py
+++ papers/histogram/figs/plot-sticky-wall.py
@@ -46,8 +46,8 @@ lines = ['-', '--', ':',]
 
 first_method = True
 the_first_method = ''
-first_temperature = [True, True, True]
-methods = ['-tmi', '-toe', '-tmmc', ]
+first_temperature = [True, True, True, True]
+methods = ['-tmi2','tmi', '-toe', '-tmmc', ]
 for i in range(len(methods)):
     method = methods[i]
     fbase = 'data/lv/ww%.2f-ff%.2f-%gx%g%s' % (ww,ff,lenx,lenyz,method)
