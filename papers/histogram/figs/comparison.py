c2fb9a948e2cac6bfda4db626f6717db9273b500
Tanner Simpson <simpstan@oregonstate.edu>
2017-06-23 15:06:16 -0700

    Added more comparison plots including round trips

--- papers/histogram/figs/comparison.py
+++ papers/histogram/figs/comparison.py
@@ -13,7 +13,7 @@ filebase = sys.argv[3]
 
 ref = "data/" + reference
 try:
-    eref, lndosref, psref = readnew.e_lndos_ps(ref)
+    eref, lndosref, Nrt_ref = readnew.e_lndos_ps(ref)
 except:
     eref, lndosref = readnew.e_lndos(ref)
 maxref = readnew.max_entropy_state(ref)
@@ -21,6 +21,7 @@ maxref = readnew.max_entropy_state(ref)
 r = glob('data/%s-movie/*lndos.dat' % (filebase))
 
 iterations = numpy.zeros(len(r))
+Nrt_at_energy = numpy.zeros(len(r))
 maxentropystate = numpy.zeros(len(r))
 minimportantenergy = numpy.zeros(len(r))
 erroratenergy = numpy.zeros(len(r))
@@ -30,11 +31,12 @@ goodenoughenergy = numpy.zeros(len(r))
 
 for i,f in enumerate(sorted(r)):
     try:
-        e,lndos,ps = readnew.e_lndos_ps(f)
+        e,lndos,Nrt = readnew.e_lndos_ps(f)
     except:
         continue
     e,lndos = readnew.e_lndos(f)
     iterations[i] = readnew.iterations(f)
+    Nrt_at_energy[i] = Nrt[energy]
     maxentropystate[i] = readnew.max_entropy_state(f)
     minimportantenergy[i] = readnew.min_important_energy(f)
     doserror = lndos - lndos[maxref] - lndosref + lndosref[maxref]
@@ -47,26 +49,40 @@ for i,f in enumerate(sorted(r)):
     else:
         goodenoughenergy[i] = -minimportantenergy[i]
 print minimportantenergy[-1]
-print len(ps)
 
-plt.loglog(ps[ps > 0], iterations[ps > 0],
-             'ro',linewidth = 2,label = r'30x4 tmi3')
-plt.loglog(psref[ps > 0], iterations[ps > 0],
-             'bx',linewidth = 2,label = r'30x4 ref')
 
-plt.xlabel(r'$iterations$')
-plt.ylabel(r'$N_R(\epsilon)$')
-#plt.title(r'$D(\epsilon)$ for $\lambda=%g$, $\eta=%g$' % (ww, ff))
-plt.legend(loc='best')
-plt.tight_layout(pad=0.2)
+
+#~ plt.loglog(ps[ps > 0], iterations[ps > 0],
+             #~ 'ro',linewidth = 2,label = r'30x4 tmi3')
+#~ plt.loglog(psref[ps > 0], iterations[ps > 0],
+             #~ 'bx',linewidth = 2,label = r'30x4 ref')
+
+#~ plt.xlabel(r'$iterations$')
+#~ plt.ylabel(r'$N_R(\epsilon)$')
+#~ #plt.title(r'$D(\epsilon)$ for $\lambda=%g$, $\eta=%g$' % (ww, ff))
+#~ plt.legend(loc='best')
+#~ plt.tight_layout(pad=0.2)
 
 #plt.savefig('figs/sticky-wall-ww%.2f-ff%.2f-%gx%g%s-Error.pdf'
             #% (ww,ff,lenx,lenyz,'-tmi3'))
 
 plt.figure()
-plt.plot(iterations, erroratenergy, 'r.')
+plt.plot(iterations[Nrt_at_energy > 0], erroratenergy[Nrt_at_energy > 0], 'r.')
+plt.title('Error at energy %g' % energy)
 plt.xlabel('# iterations')
 plt.ylabel('error')
+
+plt.figure()
+plt.plot(iterations, Nrt_at_energy, 'r.')
+plt.title('Roundy Trips at energy %g' % energy)
+plt.xlabel('# iterations')
+plt.ylabel('Roundy Trips')
+
+plt.figure()
+plt.plot(Nrt_at_energy[Nrt_at_energy > 0], erroratenergy[Nrt_at_energy > 0], 'r.')
+plt.title('Error at energy %g' % energy)
+plt.xlabel('Roundy Trips')
+plt.ylabel('Error')
 
 plt.figure()
 plt.plot(iterations, goodenoughenergy, 'r.')
