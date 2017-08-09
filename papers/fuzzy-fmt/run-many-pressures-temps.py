5fa6ceb2b99d1b8f8aca4891b1fd7ae46704f6ba
Chris May <maychr@bennet.physics.oregonstate.edu>
2017-08-08 21:00:19 -0700

    Simplified plotting, and expanded run-new-soft

--- papers/fuzzy-fmt/run-many-pressures-temps.py
+++ papers/fuzzy-fmt/run-many-pressures-temps.py
@@ -3,25 +3,28 @@
 import shlex, subprocess, os
 import numpy as np
 
-# Thermodynamic characteristics
-fillFrac = 0.8
-temp = 0.2
+# What values do we tell the system to use.
+NDT = 1		# Number, Density, Temperature On
+NVT = 0		# Number, Volume, Temperature Off
+DVT = 0		# Density, Volume, Temperature Off
 
 ## System Size Determination
+spheres = 108 # Number of Spheres
 lenx = 10
 leny = 10
 lenz = 10
+lenxy = 10
+
+# Wall Existence
 wallx = 0
 wally = 0
 wallz = 0
 
+#
+
 # Simulation Characteristics
-iters = 1000000
+iters = 10000000
 dr = 0.1
-
-# Directory and Filename Information
-direc_name = "CHRIS"
-
 
 ffmin = 0.1
 ffmax = 1.1
@@ -32,22 +35,25 @@ tempmax = 3.5
 dtemp = 0.5
 
 
-tempFF = np.arange(ffmin,ffmax,dff)
-tempTemp = np.arange(tempmin,tempmax,dtemp)
-
 try:
 	os.makedirs('data')
 except:
 	pass
 
-for fillFrac in tempFF:
-	for temp in tempTemp:
-		filename = "ff-"+str(fillFrac)+"_temp-"+str(temp)
-		command_line = "sbatch -o data/{}.out -c 1 -n 1 -J {} ./run-new-soft.py --density {} --temp {}".format(
-		                  filename, filename, fillFrac, temp)
+for density in np.arange(ffmin,ffmax,dff):
+	for temp in np.arange(tempmin,tempmax,dtemp):
+		filename = "ff-"+str(density)+"_temp-"+str(temp)
+		command_line = "sbatch -o data/{}.out -c 1 -n 1 -J {}"\
+		" ./run-new-soft.py --density {} --temp {} --number {}"\
+		" --NDT {} --NVT {} --DVT {} --lenx {} --leny {} --lenz {}"\
+		" --wallx {} --wally {} --wallz {}"\
+		" --iters {} --dr {}".format(
+		                  filename, filename,
+		                  density, temp, spheres,
+						  NDT, NVT, DVT, lenx, leny, lenz,
+						  wallx, wally, wallz,
+						  iters, dr)
 		args = shlex.split(command_line)
-		
-		#print args
 		p = subprocess.call(args)
 
 
