9494c2d9efb91ac9153d53a9b1a6fbb7bb6665bc
David Roundy <roundyd@physics.oregonstate.edu>
2017-10-05 13:26:25 -0700

    improve and move the format-code.py script

--- src/format-code.py
+++ src/format-code.py
@@ -2,10 +2,10 @@
 
 import os, glob
 
-if os.system('git diff --exit-code .'):
+if os.system('git diff --stat --exit-code .'):
     print('There are uncommitted changes in this directory.  Commit these before formatting!')
     exit(0)
 
-for f in glob.glob('*.cpp') + glob.glob('*/*.cpp'):
+for f in glob.iglob('**/*.cpp', recursive=True):
     print('formatting', f)
     os.system('astyle --style=google --indent=spaces=2 %s' % f)
