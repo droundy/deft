4f7a06378c17c5f502b808226bf0deae50e56fd6
Kirstie <kirstie@physics>
2017-10-25 10:23:58 -0700

    still working on new-melting_script_argpar.py...

--- papers/fuzzy-fmt/new-melting_script_argpar.py
+++ papers/fuzzy-fmt/new-melting_script_argpar.py
@@ -3,10 +3,7 @@
 import os
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
-
-
 
 parser = argparse.ArgumentParser(description='Variables for running mew-melting.')
 parser.add_argument('--n', metavar='n', type=float, nargs='+',
@@ -30,7 +27,7 @@ args=parser.parse_args()
 #print args
 
 n=args.n
-print "n is", n 
+print "density is", n
 temp=args.t
 print "temp is", temp
 nstart=args.nstart
@@ -49,38 +46,34 @@ print "tstep is", tstep
 
 print "DONE"
 
+for i in range(0,len(n)):
+    print "n[i] is", n[i]
+   
+
 
 ##-----------------------------
-#if len(sys.argv) < 3:
-#    print "Usage: T dfv dgw [DENSITIES TO COMPUTE]..."  #no longer needed?
-#    exit(1)
-
-##make these loops over T and rdensities?
-#temp=sys.argv[1]    
-#rdensities=sys.argv[2:]  #if enter by command line and not by hand
-#print rdensities
 #temp_num=float(temp)
 
 ##print "Do you want to save default directory deft/papers/fuzzy-fmt/crystalization before it is over-written?"
 ##os.system('rm ')  #ASK-remove data directory? - might not want to do this!
 
-##rdensities=[0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]  #if enter by hand and not by command line
-##rdensities=[0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]  #if enter by hand and not by command line
 #num_rd=len(rdensities)-1 
 
 ##ASK-change rdensities strings from command line input to float numbers
 #densities = np.array([float(d) for d in rdensities])
+#if n=none
+#    print (nend-nstart)/nstep
+#    densities=
+#    else
+    #densities = np.array([float(d) for d in n])  #??
+
   
 #for i in range(len(densities)):
-   ##if enter by hand and not by command line...
-    ##rdensity=densities[i]  
-    ##print 'running with', rdensity     
-    ##os.system('figs/new-melting.mkdat --kT 2 --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (rdensity))    
-   ##if enter by command line and not by hand...
     #rdensity_num=densities[i]   
-    #print 'running with', rdensity_num  
-    #os.system('figs/new-melting.mkdat --kT %g --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (temp_num, rdensity_num))
-      
+    #print 'running with', rdensity_num 
+#    print densities[i] 
+    #os.system('figs/new-melting.mkdat --kT %g --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (temp, rdensity_num)
+    #os.system('figs/new-melting.mkdat --kT %g --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (temp, densities[i]))   ??   
 
 ##NOTE: lattice_constant will be divided by gwstep     
    
