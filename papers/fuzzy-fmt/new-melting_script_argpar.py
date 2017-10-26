d73edbb8599ff3c6e08ed671115030bee8da3dc3
Kirstie <kirstie@physics>
2017-10-25 19:02:13 -0700

    new-melting_script_argpar.py functional but not done

--- papers/fuzzy-fmt/new-melting_script_argpar.py
+++ papers/fuzzy-fmt/new-melting_script_argpar.py
@@ -8,8 +8,8 @@ import sys
 parser = argparse.ArgumentParser(description='Variables for running mew-melting.')
 parser.add_argument('--n', metavar='n', type=float, nargs='+',
                     help='reduced density(s) (default: 1.2)')
-parser.add_argument('--t', metavar='t', type=float, 
-                    help='temperature (default: 2)')
+parser.add_argument('--t', metavar='t', type=float, nargs='+',
+                    help='temperature(s) (default: 2)')
 parser.add_argument('--nstart', metavar='n_start', type=float, 
                     help='starting reduced density (default: 0.2)')
 parser.add_argument('--nend', metavar='n_end', type=float, 
@@ -24,56 +24,52 @@ parser.add_argument('--tstep', metavar='ntstep', type=float,
                     help='step temperature by (default: 1.0)')
 
 args=parser.parse_args()
-#print args
+print args
 
-n=args.n
-print "density is", n
-temp=args.t
-print "temp is", temp
+densities=args.n
+temperatures=args.t
 nstart=args.nstart
-print "nstart is", nstart
 nend=args.nend
-print "nend is", nend
 nstep=args.nstep
-print "nstep is", nstep
 tstart=args.tstart
-print "tstart is", tstart
 tend=args.tend
-print "tend is", tend
 tstep=args.tstep
-print "tstep is", tstep
 
+if args.nstart and args.n:
+    print "Error - EITHER enter --n OR --nstart, --nend, and --nstep"
+if args.tstart and args.t:
+    print "Error - EITHER enter --t OR --tstart, --tend, and --tstep"
+    
+if args.nstart:
+    densities = np.arange(nstart, nend+nstep, nstep, float)
+    
+if args.tstart:
+    temperatures = np.arange(tstart, tend+tstep, tstep, float)
+
+for i in range(0,len(temperatures)):
+    print temperatures[i]
+    for j in range(0,len(densities)):
+        print densities[j]
+        #os.system('figs/new-melting.mkdat --kT %g --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (temperatures[i], denstities[j])) 
+        
+        
+#print "Running with densities: ", densities
+#print "length of densities=", len(densities)
+#print "Running with temperatures: ", temperatures
+#print "length of temperatures=", len(temperatures)
+#print "nstart is", nstart 
+#print "nend is", nend 
+#print "nstep is", nstep
+#print "tstart is", tstart
+#print "tend is", tend 
+#print "tstep is", tstep     
 
 print "DONE"
 
-for i in range(0,len(n)):
-    print "n[i] is", n[i]
-   
-
-
-##-----------------------------
-#temp_num=float(temp)
+##----------------------------------------------------------------------
 
 ##print "Do you want to save default directory deft/papers/fuzzy-fmt/crystalization before it is over-written?"
 ##os.system('rm ')  #ASK-remove data directory? - might not want to do this!
-
-#num_rd=len(rdensities)-1 
-
-##ASK-change rdensities strings from command line input to float numbers
-#densities = np.array([float(d) for d in rdensities])
-#if n=none
-#    print (nend-nstart)/nstep
-#    densities=
-#    else
-    #densities = np.array([float(d) for d in n])  #??
-
-  
-#for i in range(len(densities)):
-    #rdensity_num=densities[i]   
-    #print 'running with', rdensity_num 
-#    print densities[i] 
-    #os.system('figs/new-melting.mkdat --kT %g --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (temp, rdensity_num)
-    #os.system('figs/new-melting.mkdat --kT %g --rd %g --fvstart 0.0 --fvend 1.0 --fvstep 0.2 --gwstart 0.01 --gwstep 10' % (temp, densities[i]))   ??   
 
 ##NOTE: lattice_constant will be divided by gwstep     
    
