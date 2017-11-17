fba2b202ae6952528ce8717e69559c74b68b6841
Kirstie <kirstie@physics>
2017-11-17 09:00:22 -0800

    calculate diff in free energy rather than crystal free energy in downhill simplex

--- papers/fuzzy-fmt/figs/new-melting.cpp
+++ papers/fuzzy-fmt/figs/new-melting.cpp
@@ -303,10 +303,11 @@ void display_simplex(double simplex_fe[3][3]) {
   printf("\n");
 }
 
+
 void evaluate_simplex(double temp, double reduced_density, double simplex_fe[3][3], char *data_dir, double dx, bool verbose) {
   for (int k=0; k<3; k++) {
     data dhill_data=find_energy(temp, reduced_density, simplex_fe[k][0], simplex_fe[k][1], data_dir, dx, verbose);
-    simplex_fe[k][2]=dhill_data.cfree_energy_per_atom;
+    simplex_fe[k][2]=dhill_data.diff_free_energy_per_atom;
     printf("simplex_fe[%i][2]=%g\n", k, simplex_fe[k][2]);
   }
 }
@@ -331,16 +332,16 @@ point_fe reflect_simplex(double temp, double reduced_density, double simplex_fe[
   reflected.fv=simplex_fe[0][0]+simplex_fe[1][0]-simplex_fe[2][0];
   reflected.gw=simplex_fe[0][1]+simplex_fe[1][1]-simplex_fe[2][1];
   data reflect=find_energy(temp, reduced_density, reflected.fv, reflected.gw, data_dir, dx, verbose);
-  reflected.fe=reflect.cfree_energy_per_atom;
+  reflected.fe=reflect.diff_free_energy_per_atom;
   return reflected;
 }
 
 point_fe extend_simplex(double temp, double reduced_density, double simplex_fe[3][3], char *data_dir, double dx, bool verbose) {
   point_fe extended;
-  extended.fv=(3/2.0)*(simplex_fe[0][0]+simplex_fe[1][0])-2.0*simplex_fe[2][0];
-  extended.gw=(3/2.0)*(simplex_fe[0][1]+simplex_fe[1][1])-2.0*simplex_fe[2][1];
+  extended.fv=(3/2.0)*(simplex_fe[0][0]+simplex_fe[1][0])-(2.0*simplex_fe[2][0]);
+  extended.gw=(3/2.0)*(simplex_fe[0][1]+simplex_fe[1][1])-(2.0*simplex_fe[2][1]);
   data extend=find_energy(temp, reduced_density, extended.fv, extended.gw, data_dir, dx, verbose);
-  extended.fe=extend.cfree_energy_per_atom;
+  extended.fe=extend.diff_free_energy_per_atom;
   return extended;
 }
 
@@ -354,13 +355,13 @@ points_fe contract_simplex(double temp, double reduced_density, double simplex_f
   contracted.out.gw=((3/4.0)*(simplex_fe[0][1]+simplex_fe[1][1]))-((1/2.0)*(simplex_fe[2][1]));
   printf("contracted.out.fv=%g, contracted.out.gw=%g\n", contracted.out.fv, contracted.out.gw);   //debug
   data contract_out=find_energy(temp, reduced_density, contracted.out.fv, contracted.out.gw, data_dir, dx, verbose);
-  contracted.in.fe=contract_out.cfree_energy_per_atom;
+  contracted.in.fe=contract_out.diff_free_energy_per_atom;
 
   contracted.in.fv=((1/4.0)*(simplex_fe[0][0]+simplex_fe[1][0]))-((1/2.0)*(simplex_fe[2][0]));
   contracted.in.gw=((1/4.0)*(simplex_fe[0][1]+simplex_fe[1][1]))-((1/2.0)*(simplex_fe[2][1]));
   printf("contracted.in.fv=%g, contracted.in.gw=%g\n", contracted.in.fv, contracted.in.gw);   //debug
   data contract_in=find_energy(temp, reduced_density, contracted.in.fv, contracted.in.gw, data_dir, dx, verbose);
-  contracted.in.fe=contract_in.cfree_energy_per_atom;
+  contracted.in.fe=contract_in.diff_free_energy_per_atom;
   return contracted;
 }
 
@@ -370,12 +371,12 @@ points_fe shrink_simplex(double temp, double reduced_density, double simplex_fe[
   shrunken.out.fv=(1/2.0)*(simplex_fe[0][0] + simplex_fe[1][0]);   //using in/out so don't have to make another structure
   shrunken.out.gw=(1/2.0)*(simplex_fe[0][1] + simplex_fe[1][1]);
   shrunken.out.fe=find_energy(temp, reduced_density, shrunken.out.fv, shrunken.out.gw,
-                              data_dir, dx, verbose).cfree_energy_per_atom;
+                              data_dir, dx, verbose).diff_free_energy_per_atom;
 
   shrunken.in.fv=(1/2.0)*(simplex_fe[0][0] + simplex_fe[2][0]);
   shrunken.in.gw=(1/2.0)*(simplex_fe[0][1] + simplex_fe[2][1]);
   shrunken.in.fe=find_energy(temp, reduced_density, shrunken.in.fv, shrunken.in.gw,
-                             data_dir, dx, verbose).cfree_energy_per_atom;
+                             data_dir, dx, verbose).diff_free_energy_per_atom;
   return shrunken;
 }
 
