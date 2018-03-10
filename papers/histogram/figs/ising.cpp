9e4cbbe41dea0c55ee66cac2b35ace2efbb67be7
Jordan Pommerenck <jordan@wentworth.physics.oregonstate.edu>
2018-03-09 16:56:44 -0800

    added random generator and lattice.

--- papers/histogram/figs/ising.cpp
+++ papers/histogram/figs/ising.cpp
@@ -9,8 +9,56 @@
 #include <errno.h>
 #include "handymath.h"
 #include "vector3d.h"
+#include "random"
 
 #include "version-identifier.h"
+
+// ---------------------------------------------------------------------
+// Define "Constants" -- set from arguments then unchanged
+// ---------------------------------------------------------------------
+
+double E = 0;  // Define our initial system energy.
+double M = 0;  // Define our initial system magnetization.
+double iterations = 0; 
+double J = 0;  // Define our spin coupling.
+const int N = 25;  // Define number of spin sites.
+// user should be able to define minT? 
+
+// ---------------------------------------------------------------------
+// Mersenne Twister Engine -- build the random seed generator
+// ---------------------------------------------------------------------
+
+//the random device that will seed the generator
+std::random_device seeder;
+// The Mersenne twister engine which has a distinct advantage
+// over rand().  Only for c++11 compilers. 
+std::mt19937 engine(seeder());
+std::uniform_int_distribution<int> spin_dist(-1, 1);
+
+// ---------------------------------------------------------------------
+// Generate 2D Spin Lattice 
+// ---------------------------------------------------------------------
+
+int spin_lattice[N][N];
+int spin_number = spin_dist(engine); // currently either -1,0,+1
+
+//inline generate_spin_lattice() {
+  //for (int i = 0; i < 2; i++) {
+    //for (int j = 0; j < 2; j++) {
+      //spin_lattice[i][j] = spin_number;
+    //}
+  //}
+  //return spin_lattice;
+//}
+
+// ---------------------------------------------------------------------
+// Flip a spin site randomly
+// ---------------------------------------------------------------------
+
+std::uniform_int_distribution<int> X(0, N);
+std::uniform_int_distribution<int> Y(0, N);
+
+//int spin_site_flip[X][Y];
 
 static double took(const char *name) {
   assert(name); // so it'll count as being used...
@@ -32,8 +80,9 @@ static double took(const char *name) {
   return exp(ceil(log(seconds)));
 }
 
-
 int main(int argc, const char *argv[]) {
   took("Starting program");
   printf("version: %s\n",version_identifier());
+  printf("lets randomize: %d\n", spin_number);
 }
+
