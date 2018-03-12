6c92c0863e8cbb9a0ae9128bbedeee8623612fb3
Jordan Pommerenck <jordan@wentworth.physics.oregonstate.edu>
2018-03-12 16:44:49 -0700

    added basic spin flip functions to ising.

--- papers/histogram/figs/ising.cpp
+++ papers/histogram/figs/ising.cpp
@@ -19,46 +19,87 @@
 
 double E = 0;  // Define our initial system energy.
 double M = 0;  // Define our initial system magnetization.
-double iterations = 0; 
 double J = 0;  // Define our spin coupling.
+
 const int N = 25;  // Define number of spin sites.
-// user should be able to define minT? 
+const int Q = 2;   // Define number of spin states.
+
+double minT = 0; // Define minimum Temperature.
+
+// ---------------------------------------------------------------------
+// ising.h files -- this could likely go into an ising.h file
+// ---------------------------------------------------------------------
+
+struct ising_simulation {
+  long iteration;   // the current iteration number
+
+  int spin_number;        // the current spin number
+  int site_numberI;
+  int site_numberJ;
+  int spin_lattice[N][N]; // the size of the spin system
+  
+  void flip_a_spin(); // attempt to flip a spin
+  void initialize_spin_lattice(); // generate the spin lattice
+  void random_site_flip(); // flip the spin of a random site
+};
 
 // ---------------------------------------------------------------------
 // Mersenne Twister Engine -- build the random seed generator
 // ---------------------------------------------------------------------
 
-//the random device that will seed the generator
-std::random_device seeder;
-// The Mersenne twister engine which has a distinct advantage
-// over rand().  Only for c++11 compilers. 
-std::mt19937 engine(seeder());
-std::uniform_int_distribution<int> spin_dist(-1, 1);
+// ising_simulation methods
+
+void ising_simulation::flip_a_spin() {
+  //the random device that will seed the generator
+  std::random_device seeder;
+  // The Mersenne twister engine which has a distinct advantage
+  // over rand().  Only for c++11 compilers. 
+  std::mt19937 engine(seeder());
+  std::uniform_int_distribution<int> spin_dist(1, Q);
+
+  spin_number = spin_dist(engine); // currently ranges from 1 --> Q
+
+  if (spin_number > Q/2.0) {
+    spin_number = spin_number - (Q + 1);
+  }
+}
 
 // ---------------------------------------------------------------------
 // Generate 2D Spin Lattice 
 // ---------------------------------------------------------------------
 
-int spin_lattice[N][N];
-int spin_number = spin_dist(engine); // currently either -1,0,+1
+void ising_simulation::initialize_spin_lattice() {
+  assert (Q%2 == 0);  // terminate program if Q is odd!
 
-//inline generate_spin_lattice() {
-  //for (int i = 0; i < 2; i++) {
-    //for (int j = 0; j < 2; j++) {
-      //spin_lattice[i][j] = spin_number;
-    //}
-  //}
-  //return spin_lattice;
-//}
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      flip_a_spin();
+      spin_lattice[i][j] = spin_number;
+    }
+  }
+}
 
 // ---------------------------------------------------------------------
 // Flip a spin site randomly
 // ---------------------------------------------------------------------
 
-std::uniform_int_distribution<int> X(0, N);
-std::uniform_int_distribution<int> Y(0, N);
+void ising_simulation::random_site_flip() {
+    std::random_device seeder;
+    std::mt19937 engine(seeder());
+    std::uniform_int_distribution<int> site_distI(1, N);
+    std::uniform_int_distribution<int> site_distJ(1, N);
+    
+    site_numberI = site_distI(engine); // currently ranges from 1 --> N
+    site_numberJ = site_distJ(engine); // currently ranges from 1 --> N
+    
+    flip_a_spin();
 
-//int spin_site_flip[X][Y];
+    spin_lattice[site_numberI][site_numberJ] = spin_number;
+}
+
+// ---------------------------------------------------------------------
+// Initialize Main
+// ---------------------------------------------------------------------
 
 static double took(const char *name) {
   assert(name); // so it'll count as being used...
@@ -83,6 +124,5 @@ static double took(const char *name) {
 int main(int argc, const char *argv[]) {
   took("Starting program");
   printf("version: %s\n",version_identifier());
-  printf("lets randomize: %d\n", spin_number);
 }
 
