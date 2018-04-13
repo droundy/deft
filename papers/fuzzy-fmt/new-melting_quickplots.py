e8963b55bd9f2b23d2d7abebdca537d10a801e37
Kirstie <kirstie@physics>
2018-04-13 08:52:49 -0700

    added --dx option in new-melting_quickplots.py

--- papers/fuzzy-fmt/new-melting_quickplots.py
+++ papers/fuzzy-fmt/new-melting_quickplots.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2
-#Run this program from /deft/papers/fuzzy-fmt by entering ./new-melting_quickplots.py  --kT value --n value  --d directory
+#Run this program from /deft/papers/fuzzy-fmt by entering ./new-melting_quickplots.py  --kT 2 --n 1.2  --d directory --dx 0.001
 #Generates 8 plot.dat files and plots
 
 import os
@@ -16,23 +16,29 @@ parser.add_argument('--kT', metavar='temperature', type=float,
                     help='create plot at this temperature')
 parser.add_argument('--n', metavar='reduced_density', type=float,
                     help='create plot at this reduced density')
+parser.add_argument('--dx', metavar='dx', type=float,
+                    help='dx')
 
 args=parser.parse_args()
 directory=args.d
 kT=args.kT
 n=args.n
 
+dx=0.01
+if args.dx:
+    dx=args.dx
+
 #Try values of n: 0.95, 1.08, 1.2  and  kT: 1, 2, 20, 200
 
 for gwidth in [0.001, 0.01, 0.1, .2]: 
-    os.system('figs/new-melting.mkdat --kT %g --n %g --d %s_kT%g_n%g_gw%g --fvstart 0 --fvend .9 --fvstep 0.01 --gw %g  --dx 0.01' % (kT, n, directory, kT, n, gwidth, gwidth))
-    os.system('cat %s_kT%g_n%g_gw%g/*alldat.dat >> %s_kT%g_n%g_gw%g/plot.dat' % (directory, kT, n, gwidth, directory, kT, n, gwidth))
-    os.system('./new-melting_anyplot_script.py %s_kT%g_n%g_gw%g --ftemp %g --ydiff --xfv --ptname n%g_gw%g' % (directory, kT, n, gwidth, kT, n, gwidth))
-    os.system('cp %s_kT%g_n%g_gw%g/plot_DiffFEvsfv_kT%g_n%g_gw%g.png %s_plots/plot_DiffFEvsfv_kT%g_n%g_gw%g.png' % (directory, kT, n, gwidth, kT, n, gwidth, directory, kT, n, gwidth))
+    os.system('figs/new-melting.mkdat --kT %g --n %g --d %s_kT%g_n%g_gw%g_dx%g --fvstart 0 --fvend .9 --fvstep 0.01 --gw %g  --dx %g' % (kT, n, directory, kT, n, gwidth, dx, gwidth, dx))
+    os.system('cat %s_kT%g_n%g_gw%g_dx%g/*alldat.dat >> %s_kT%g_n%g_gw%g_dx%g/plot.dat' % (directory, kT, n, gwidth, dx, directory, kT, n, gwidth, dx))
+    os.system('./new-melting_anyplot_script.py %s_kT%g_n%g_gw%g_dx%g --ftemp %g --ydiff --xfv --ptname n%g_gw%g_dx%g' % (directory, kT, n, gwidth, dx, kT, n, gwidth, dx))
+    os.system('cp %s_kT%g_n%g_gw%g_dx%g/plot_DiffFEvsfv_kT%g_n%g_gw%g_dx%g.png %s_plots/plot_DiffFEvsfv_kT%g_n%g_gw%g_dx%g.png' % (directory, kT, n, gwidth, dx, kT, n, gwidth, dx, directory, kT, n, gwidth, dx))
 
 for fv in [0, 0.01, 0.1, 0.2]:
-    os.system('figs/new-melting.mkdat --kT %g --n %g --d %s_kT%g_n%g_fv%g --fv %g --gwstart .001 --gwend .3 --gwstep .005  --dx 0.01' % (kT, n, directory, kT, n, fv, fv))
-    os.system('cat %s_kT%g_n%g_fv%g/*alldat.dat >> %s_kT%g_n%g_fv%g/plot.dat' % (directory, kT, n, fv, directory, kT, n, fv))
-    os.system('./new-melting_anyplot_script.py %s_kT%g_n%g_fv%g --ftemp %g --ydiff --xgw --ptname n%g_fv%g' % (directory, kT, n, fv, kT, n, fv))
-    os.system('cp %s_kT%g_n%g_fv%g/plot_DiffFEvsgw_kT%g_n%g_fv%g.png %s_plots/plot_DiffFEvsgw_kT%g_n%g_fv%g.png' % (directory, kT, n, fv, kT, n, fv, directory, kT, n, fv))
+    os.system('figs/new-melting.mkdat --kT %g --n %g --d %s_kT%g_n%g_fv%g_dx%g --fv %g --gwstart .001 --gwend .3 --gwstep .005  --dx %g' % (kT, n, directory, kT, n, fv, dx, fv, dx))
+    os.system('cat %s_kT%g_n%g_fv%g_dx%g/*alldat.dat >> %s_kT%g_n%g_fv%g_dx%g/plot.dat' % (directory, kT, n, fv, dx, directory, kT, n, fv, dx))
+    os.system('./new-melting_anyplot_script.py %s_kT%g_n%g_fv%g_dx%g --ftemp %g --ydiff --xgw --ptname n%g_fv%g_dx%g' % (directory, kT, n, fv, dx, kT, n, fv, dx))
+    os.system('cp %s_kT%g_n%g_fv%g_dx%g/plot_DiffFEvsgw_kT%g_n%g_fv%g_dx%g.png %s_plots/plot_DiffFEvsgw_kT%g_n%g_fv%g_dx%g.png' % (directory, kT, n, fv, dx, kT, n, fv, dx, directory, kT, n, fv, dx))
 
