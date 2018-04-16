fd1b281a5f187d6959eff85f7579d2a17d9dfa94
Felix Tyson <tysonf@oregonstate.edu>
2018-04-16 14:42:49 -0700

    scratch prev approach; new expr trait system skeleton

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,4 +1,3 @@
-//! This is the deft crate!
 
 extern crate internment;
 extern crate tinyset;
@@ -6,128 +5,52 @@ extern crate tinyset;
 use tinyset::{Map64, Fits64};
 use internment::Intern;
 
-/// This is an expression.
-///
-/// It implements `Copy`, making it efficient to use.
-#[derive(Debug, Hash, Clone, PartialEq, Eq)]
-pub struct Expr<T: ExprKind + Cpp<T>> {
-    inner: Intern<ExprOp<T>>,
+pub trait InnerExpr: 'static + Send + Eq + std::hash::Hash {
+    fn cpp(&self) -> String;
 }
 
-impl<T: ExprKind + Cpp<T>> Copy for Expr<T> {}
+pub struct Expr<T: InnerExpr> {
+    inner: Intern<T>,
+}
 
-impl<T: ExprKind + Cpp<T>> Fits64 for Expr<T> {
-    fn to_u64(self) -> u64 {
-        self.inner.to_u64()
-    }
-    unsafe fn from_u64(x: u64) -> Self {
-        Expr { inner: Intern::from_u64(x), }
+impl<T: InnerExpr> Expr<T> {
+    fn cpp(&self) -> String {
+        self.inner.cpp()
     }
 }
 
-pub trait Cpp<T: ExprKind + Cpp<T>> {
-    fn cpp(op: Expr<T>, assignto: &'static str) -> String;
-}
-
-pub trait ExprKind: PartialEq + Eq + Clone + std::fmt::Debug {
-    type SpecificOps: std::hash::Hash + PartialEq + Eq + Clone + std::fmt::Debug;
-}
-
-#[derive(Debug, Hash)]
-enum ExprOp<T: ExprKind + Cpp<T>> {
-    Add(CommutativeMap<T>),
-    ScalarMul(Scalar, Expr<T>),
-    Specific(T::SpecificOps),
-}
-
-#[derive(PartialEq, Eq, Clone, Debug, Hash)]
-struct Scalar;
-impl ExprKind for Scalar { type SpecificOps = ScalarOps; }
-#[derive(Hash, PartialEq, Eq, Clone, Debug)]
-enum ScalarOps {
-    Mul(CommutativeMap<Scalar>),
-    Exp(Expr<Scalar>),
-    Log(Expr<Scalar>),
-}
-impl Cpp<Scalar> for Scalar {
-    fn cpp(op: Expr<Scalar>, assignto: &'static str) -> String {
-        match &*op.inner {
-            &ExprOp::Add(ref map) => { unimplemented!() },
-            &ExprOp::Specific(ref spec_op) =>
-                match spec_op {
-                    &ScalarOps::Mul(ref map) => { unimplemented!() },
-                    otherwise => { unimplemented!() },
-                },
-            otherwise => { unimplemented!() }
-        }
+impl<T: InnerExpr> From<T> for Expr<T> {
+    fn from(inner: T) -> Self {
+        Expr { inner: Intern::new(inner) }
     }
 }
 
-#[derive(Debug, Clone)]
-struct CommutativeMap<T: ExprKind + Cpp<T>> {
-    inner: Map64<Expr<T>, f64>,
+#[derive(PartialEq, Eq, Hash)]
+enum Scalar {
+    Var(&'static str),
+    Add(
+        // unimplemented!()
+    ),
 }
 
-impl<T: ExprKind + Cpp<T>> CommutativeMap<T> {
-    fn new() -> Self {
-        CommutativeMap { inner: Map64::new(), }
-    }
-
-    fn as_vec(&self) -> Vec<(u64, u64)> {
-        self.inner.iter().map(|(k, &v)| (k.inner.to_u64(), v as u64)).collect()
-    }
-
-    // This lets you create a `CommutativeMap` without wasting time checking if
-    // the first term was already there.
-    fn from_pair(k: Expr<T>, v: f64) -> Self {
-        let mut map = CommutativeMap::new();
-        map.inner.insert(k, v);
-        map
-    }
-
-    fn insert(&mut self, k: Expr<T>, v: f64) {
-        let v = self.inner.get(&k).unwrap_or(&0.0) + v;
-        if v == 0.0 {
-            self.inner.remove(&k);
-        } else {
-            self.inner.insert(k, v);
-        }
-    }
-
-    fn union(&mut self, other: &Self) {
-        for (k, &v) in other.inner.iter() {
-            self.insert(k, v);
-        }
-    }
-
-    // This is used for negation, subtraction, reciprocals, division.
-    fn negate(&self) -> Self {
-        let mut neg = CommutativeMap::new();
-        for (k, &v) in self.inner.iter() {
-            neg.inner.insert(k, -v);
-        }
-        neg
-    }
+impl InnerExpr for Scalar {
+    fn cpp(&self) -> String { unimplemented!() }
 }
 
-impl<T: ExprKind + Cpp<T>> std::hash::Hash for CommutativeMap<T> {
-    fn hash<H: std::hash::Hasher>(&self, hasher: &mut H) {
-        let mut terms = self.as_vec();
-        terms.sort();
-        for (k, v) in terms {
-            k.hash(hasher);
-            v.hash(hasher);
-        }
-    }
+impl std::ops::Add for Scalar {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self { unimplemented!() }
 }
 
-impl<T: ExprKind + Cpp<T>> PartialEq for CommutativeMap<T> {
-    fn eq(&self, other: &Self) -> bool {
-        let (mut lhs, mut rhs) = (self.as_vec(), other.as_vec());
-        lhs.sort();
-        rhs.sort();
-        lhs == rhs
-    }
+trait ClosedAdd {
+    fn add(&self, other: &Self) -> Self;
 }
 
-impl<T: ExprKind + Cpp<T>> Eq for CommutativeMap<T> {}
+impl<T: InnerExpr + ClosedAdd> std::ops::Add for Expr<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        ((*self.inner).add(&*other.inner)).into()
+    }
+}
