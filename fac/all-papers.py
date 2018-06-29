659aa95bedc027a06e830531f456382014244d8b
David Roundy <roundyd@physics.oregonstate.edu>
2018-06-29 15:18:14 -0700

    add embryonic 2d grand paper

--- fac/all-papers.py
+++ fac/all-papers.py
@@ -4,7 +4,7 @@ import os
 
 papers = """
    histogram pair-correlation contact hard-sphere-free-energy
-   square-well-fluid
+   square-well-fluid grand
    fuzzy-fmt water-saft polyhedra electrostatics renormalization
 """.split()
 
