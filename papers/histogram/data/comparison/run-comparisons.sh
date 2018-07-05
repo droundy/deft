09d08e215c093082aeaa557a114748770f455bf9
David Roundy <roundyd@physics.oregonstate.edu>
2018-07-05 13:48:00 -0700

    add simple shell script to automate running of comparison.py

---
+++ papers/histogram/data/comparison/run-comparisons.sh
@@ -0,0 +1,10 @@
+
+python figs/comparison.py 0 s000/periodic-ww1.30-ff0.10-N50-tmmc-golden-movie/002100 s000/periodic-ww1.30-ff0.10-N50
+
+python figs/comparison.py 0 s000/periodic-ww1.30-ff0.20-N50-tmi3-golden-movie/001090-lndos.dat s000/periodic-ww1.30-ff0.20-N50 allmethods
+
+python figs/comparison.py 0 s000/periodic-ww1.30-ff0.30-N50-tmmc-golden-movie/001050 s000/periodic-ww1.30-ff0.30-N50
+
+
+python figs/comparison.py 0 s000/periodic-ww1.30-ff0.30-N500-tmmc-golden-movie/000324-lndos.dat s000/periodic-ww1.30-ff0.30-N500 allmethods
+
