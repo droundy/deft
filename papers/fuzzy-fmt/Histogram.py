88d96fc143774695dca8cca327b22794376e750e
Kirstie <kirstie@physics>
2018-10-11 15:47:40 -0700

    Added argparse and print data out statement to Histogram.py; Added #NAN print out statement in new-melting.cpp so last line of data printed out would not mess up script files when there is a NAN; Added seed to print out data in new-melting.cpp; Replaced data format in histogram_relerror.dat

--- papers/fuzzy-fmt/Histogram.py
+++ papers/fuzzy-fmt/Histogram.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python2
-#Run from /deft/papers/fuzzy-fmt by entering ./Histogram.py
+#Run from /deft/papers/fuzzy-fmt by entering ./Histogram.py [filename.dat] --gw [] --mcconstant [] --seeds []
 
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+import argparse
+import os
+import sys
 
-thisdata = np.loadtxt("histogram_relerror.dat")
+parser = argparse.ArgumentParser(description='Creates a plot.', epilog="stuff...")
 
-x=thisdata[:,6]
+parser.add_argument('filedat', metavar='histogram datafile', type=str,
+                    help='data file used to create histogram') 
+parser.add_argument('--gw', metavar='gwidth', type=float,
+                    help='gwidth') 
+parser.add_argument('--mcconstant', metavar='mcconstant', type=float,
+                    help='mc_constant') 
+parser.add_argument('--seeds', metavar='seeds', type=float,
+                    help='number of seeds ran') 
+                                        
+args=parser.parse_args()
+
+thisdata = np.loadtxt(args.filedat)
+
+x=thisdata[:,9]
 
 error_range = max(x.max(), -x.min())
 _,bins,_ = plt.hist(x,range=(-error_range, error_range), facecolor='blue')
 plt.hist(x[x<0],bins,facecolor='green')
-plt.title(r'%.1f%% $\pm$ %.1f%% negative' %
+plt.title(r'%.1f%% $\pm$ %.1f%% negative  %s' %
           (100.0*len(x[x<0]) / float(len(x)),
-           100.0*len(x[x<0]) / float(len(x))/np.sqrt(len(x))))
+           100.0*len(x[x<0]) / float(len(x))/np.sqrt(len(x)), args.filedat))
+percent_neg=100.0*len(x[x<0]) / float(len(x))
+print('%s %g %g %g    #%.1f%% +- %.1f%% negative  %s' %
+          (percent_neg, args.gw, args.mcconstant, args.seeds, 100.0*len(x[x<0]) / float(len(x)),
+           100.0*len(x[x<0]) / float(len(x))/np.sqrt(len(x)), args.filedat))
 x_label="relerror"
-print(bins)
+#print(bins)
 plt.xlabel(x_label)
+plt.savefig(args.filedat+".png" )
 plt.show()
+
--- papers/fuzzy-fmt/figs/new-melting.cpp
+++ papers/fuzzy-fmt/figs/new-melting.cpp
@@ -721,6 +721,7 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
           printf("position is: %g %g %g\n", r.x, r.y, r.z);
           printf("n0 = %g\nn1 = %g\nn2=%g\nn3=%.17g = 1+%g\n", n_0, n_1, n_2, n_3, n_3-1);
           printf("phi1 = %g\nphi2 = %g\nphi3=%g\n", phi_1, phi_2, phi_3);
+          printf("             #NAN!\n"); //
           data data_out;
           data_out.diff_free_energy_per_atom=0;
           data_out.cfree_energy_per_atom=0;
@@ -844,8 +845,8 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
   printf("run time is %g hours\n", run_time/60/60);
   //printf("mc_constant=%ld, mc_prefactor=%ld\n", mc_constant, mc_prefactor); // HERE!
   
-  printf("\n#dx\tmc-error\tphi1\tphi2\tphi3\tFtot\tFideal\t\tFEdiff\tmean_n3\t\trelerr\t\tmc_con\tmc_pf\tgw\tmin\n");
-  printf("%g\t%g\t\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%ld\t%ld\t%g\t%g\n",
+  printf("\n#dx\tmc-error\tphi1\tphi2\tphi3\tFtot\tFideal\t\tFEdiff\tmean_n3\t\trelerr\t\tmc_con\tmc_pf\tgw\tseed\tmin\n");
+  printf("%g\t%g\t\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%ld\t%ld\t%g\t%g\t%g\n",
          dx_input,
          MC_ERROR,
          total_phi_1/primitive_cell_volume,
@@ -859,6 +860,7 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
          mc_constant,
          mc_prefactor,
          gwidth,
+         seed,
          run_time/60);
    if (my_experiment > 0) {
    printf("My experiment\n"); 
