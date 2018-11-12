6d12c3c2370d713ba97ae2a9502f16e009acca14
Kirstie <kirstie@physics>
2018-11-12 05:22:59 -0800

    modified descriptions on command line arguments, fixed system command input for kT and n, modified gw values for running script

--- papers/fuzzy-fmt/nm_FE_vs_gw_generate_data.py
+++ papers/fuzzy-fmt/nm_FE_vs_gw_generate_data.py
@@ -12,20 +12,20 @@ parser = argparse.ArgumentParser(description='Creates a plot.', epilog="stuff...
 
 #parser.add_argument('filedat', metavar='data filename', type=str,
 #                    help='name of datafile to store FE vs gw data') 
-parser.add_argument('--kT', metavar='reduced temperature', type=float,
+parser.add_argument('--kT', metavar='temperature', type=float,
                     help='reduced temperature - required') 
-parser.add_argument('--n', metavar='reduced density', type=float,
+parser.add_argument('--n', metavar='density', type=float,
                     help='reduced density - required') 
-parser.add_argument('-fv', metavar='fraction of vacancies', type=float,
+parser.add_argument('-fv', metavar='vacancies', type=float,
                     help='fraction of vacancies')  
 parser.add_argument('--dx', metavar='dx', type=float,
                     help='scaling dx') 
 #parser.add_argument('--mcerror', metavar='mc_error', type=float,
 #                    help='mc_error') 
-parser.add_argument('--mcconstant', metavar='mc_constant', type=int,
-                    help='mc_constant') 
-parser.add_argument('--mcprefactor', metavar='mc_prefactor', type=int,
-                    help='mc_prefactor')
+parser.add_argument('--mcconstant', metavar='const', type=int,
+                    help='monte carlo integration mc_constant') 
+parser.add_argument('--mcprefactor', metavar='prefac', type=int,
+                    help='monte carlo integration mc_prefactor')
 parser.add_argument('--seeds', metavar='seeds', type=int,
                     help='number of seeds ran') 
                                         
@@ -69,12 +69,13 @@ else :
 #if args.filedat:    
 #    datafile=args.filedat+'.dat'
 
-for gw in [.4, .5, .6, .7, .8, .9]:
-#for gw in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3]: 
+#for gw in [0.1, 0.2, 0.3]:
+for gw in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4]: 
+#for gw in [.5, .6, .7, .8, .9]:
   print "gw=%g" % (gw)
   for seed in range(1, seeds+1):   
     print "seed=%g" % (seed)
-    os.system('figs/new-melting.mkdat --kT 2 --n 1.3 --gw %g  --fv 0 --dx %g  --mc-error .001   --mc-constant %g --mc-prefactor %g --seed %g | tail -n 2   >> FE_vs_gw_kT%g_n%g_fv0_dx%g_mcerror0.001_mcconstant%g_mcprefactor%g.dat' % (gw, dx, mcconstant, mcprefactor, seed, kT, n, dx, mcconstant, mcprefactor)) 
+    os.system('figs/new-melting.mkdat --kT %g --n %g --gw %g  --fv 0 --dx %g  --mc-error .001   --mc-constant %g --mc-prefactor %g --seed %g | tail -n 2   >> FE_vs_gw_kT%g_n%g_fv0_dx%g_mcerror0.001_mcconstant%g_mcprefactor%g.dat' % (kT, n, gw, dx, mcconstant, mcprefactor, seed, kT, n, dx, mcconstant, mcprefactor)) 
 
     
 
