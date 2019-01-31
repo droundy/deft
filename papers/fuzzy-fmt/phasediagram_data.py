671785b52ba892f6e7f388955347270d3f812dc8
Kirstie <kirstie@physics>
2019-01-30 18:37:26 -0800

    plot-pressure.py: added code to find liquid and crystal densities at the transistion point; phasediagram.py: new file to create phasenew.dat with temperature, pressure and densities at transistion point; plot-phasediagram.py: modified for new phasenew.dat file format and cleanups

---
+++ papers/fuzzy-fmt/phasediagram_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+#NOTE: Run this plot script from directory deft/papers/fuzzy-fmt 
+#with comand ./phasediagram_data.py 
+
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import os, glob
+import argparse
+
+for kT in np.arange(0.05, 1.2, 0.05):
+    #print (kT)
+    #cmd = 'rq run -J plot-pressure.py --kT %g >> phasenew.dat' % (kT)
+    cmd = './plot-pressure.py --kT %g >> phasenew.dat' % (kT)
+    print(cmd)
+    os.system(cmd)
--- papers/fuzzy-fmt/plot-phasediagram.py
+++ papers/fuzzy-fmt/plot-phasediagram.py
@@ -15,35 +15,24 @@ parser.add_argument('filedat', metavar='datafile', type=str,
 args=parser.parse_args()
 
 thisdata = np.loadtxt(args.filedat)
-kT=thisdata[:,0]   
-n_homogeneous=thisdata[:,1]
-n_crystal=thisdata[:,2]  
-P=thisdata[:,3]
+kT=thisdata[:,0]
+#phasenew.dat:   
+n_homogeneous=thisdata[:,2]
+n_crystal=thisdata[:,3]  
+P=thisdata[:,1]
 
-#print(len(kT))
-
-#a=0
-#b=-2
-#if b<0 :
-#  print b
-#else : print a
-
+#phase.dat:
+#n_homogeneous=thisdata[:,1]
+#n_crystal=thisdata[:,2]  
+#P=thisdata[:,3]
 
 
 #Temperature vs Density Phase Diagram
-plt.plot(n_homogeneous, kT, label='liquid', color='red')
-plt.plot(n_crystal, kT, label='solid', color='blue')
-
-#for i in range(len(kT)-1):
-#   plt.plot([n_homogeneous[i], n_crystal[i]],[kT[i],kT[i]], color='gray', lw=2)
-#   plt.plot([0, n_homogeneous[i]], [kT[i],kT[i]], color='red')
-#   plt.plot([0, n_homogeneous[5]], [kT[5],kT[5]], color='red')  #FIX!
-#   plt.plot([n_crystal[i], n_crystal[len(kT)-1]], [kT[i],kT[i]], color='blue')
-   
-plt.fill_betweenx(kT, 0, n_homogeneous, color='red')  
-plt.fill_betweenx(kT, n_homogeneous, n_crystal, color='gray') 
-plt.fill_betweenx(kT, n_crystal, 1, color='blue')  
-   
+plt.plot(n_homogeneous, kT,  '.-', label='liquid', color='red')
+plt.plot(n_crystal, kT,  '.-', label='solid', color='blue')
+#plt.fill_betweenx(kT, 0, n_homogeneous, color='red')  
+#plt.fill_betweenx(kT, n_homogeneous, n_crystal, color='gray') 
+#plt.fill_betweenx(kT, n_crystal, 1.5, color='blue')  
 plt.title("Temperature vs Density")
 plt.legend(loc='best')
 plt.xlabel('Density')
@@ -55,12 +44,10 @@ plt.figure()
 plt.fill_between(kT, 0*P, P, color='red')
 plt.fill_between(kT, P, P+100, color='blue')
 plt.plot(kT, P, color='black')
-# for i in range(len(kT)):
-#    plt.plot([0, kT[i]], [P[i],P[i]], color='darkblue')
-#    plt.plot([kT[i], kT[len(kT)-1]], [P[i],P[i]], color='deepskyblue')
 plt.ylim(0, 40)
 plt.xlim(kT.min(), kT.max())
 plt.title("Pressure vs Temperature")
 plt.xlabel('kT')
 plt.ylabel('Pressure')
+
 plt.show()
