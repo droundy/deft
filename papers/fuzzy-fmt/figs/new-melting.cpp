f90b9bfbdcb40810577142c48e7a2d53798bb3f4
Kirstie <kirstie@physics>
2019-02-14 13:09:23 -0800

    changed tensorw to use_tensor_weight and ...tara.dat to ...tensor.dat

--- papers/fuzzy-fmt/figs/new-melting.cpp
+++ papers/fuzzy-fmt/figs/new-melting.cpp
@@ -32,7 +32,7 @@ double seed=1;
 long mc_prefactor=50000;
 long mc_constant=100;
 int my_experiment =0;   //set to 1 to run my experiment
-bool tensorw=false;
+bool use_tensor_weight=false;
 
 char *free_energy_output_file = 0;
 
@@ -713,7 +713,7 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
         double phi_1 = -n_0*1.0*log(1.0-1.0*n_3);
         double phi_2 = (n_1*n_2 - nv_1.dot(nv_2))/(1-n_3);
         double phi_3;
-        if (tensorw) {
+        if (use_tensor_weight) {
           //double traceof_nm_2squared =  nm_2.x.x*nm_2.x.x +
           //                              nm_2.y.y*nm_2.y.y +
           //                              nm_2.z.z*nm_2.z.z +
@@ -898,8 +898,8 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
     char *alldat_filedescriptor = new char[1024];
     sprintf(alldat_filedescriptor, "kT%5.3f_n%05.3f_fv%04.2f_gw%04.3f",
             temp, reduced_density, fv, gwidth);
-    if (tensorw)  {
-      sprintf(alldat_filename, "%s/%s-alldat_tara.dat", data_dir, alldat_filedescriptor);
+    if (use_tensor_weight)  {
+      sprintf(alldat_filename, "%s/%s-alldat_tensor.dat", data_dir, alldat_filedescriptor);
     } else {
       sprintf(alldat_filename, "%s/%s-alldat.dat", data_dir, alldat_filedescriptor);
     }
@@ -909,12 +909,12 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
     FILE *newmeltoutfile = fopen(alldat_filename, "w");
     if (newmeltoutfile) {
       fprintf(newmeltoutfile, "# git  version: %s\n", version_identifier());
-      fprintf(newmeltoutfile, "#kT\tn\tfv\tgwidth\thFE/atom\tcFE/atom\tFEdiff/atom\tlat_const\tNsph\tdx\tmcconstant\tmcprefactor\tmcerror\tmcseed\ttime(h)\ttarazona\n");
+      fprintf(newmeltoutfile, "#kT\tn\tfv\tgwidth\thFE/atom\tcFE/atom\tFEdiff/atom\tlat_const\tNsph\tdx\tmcconstant\tmcprefactor\tmcerror\tmcseed\ttime(h)\ttensor\n");
       fprintf(newmeltoutfile, "%g\t%g\t%g\t%g\t%g\t%g\t      %g\t\t%g\t\t%g\t%g\t%li\t%li\t%g\t%g\t%g\t%d\n",
               temp, reduced_density, fv, gwidth, hfree_energy_per_atom,
               cfree_energy_per_atom, data_out.diff_free_energy_per_atom,
               lattice_constant, reduced_num_spheres, dx_input, mc_constant, mc_prefactor, MC_ERROR, seed,
-              run_time/60/60, tensorw);
+              run_time/60/60, use_tensor_weight);
       fclose(newmeltoutfile);
     } else {
       printf("Unable to open file %s!\n", alldat_filename);
@@ -927,7 +927,7 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
   if (free_energy_output_file) {
     FILE *f = fopen(free_energy_output_file, "a");
     fprintf(f, "\n# git  version: %s\n", version_identifier());
-    fprintf(f, "#dx\tmc-error\tphi1\tphi2\tphi3\tFtot\tFideal\t\t\tFEdiff\t\tmean_n3\t\t\trelerr\t\t\tmc_con\tmc_pf\tgw\t\tfv\tkT\tn\tseed\tmin\ttarazona\n");
+    fprintf(f, "#dx\tmc-error\tphi1\tphi2\tphi3\tFtot\tFideal\t\t\tFEdiff\t\tmean_n3\t\t\trelerr\t\t\tmc_con\tmc_pf\tgw\t\tfv\tkT\tn\tseed\tmin\ttensor\n");
     fprintf(f, "%g\t%g\t\t%g\t%g\t%g\t%g\t%g\t\t%g\t\t%g\t\t%g\t\t%ld\t%ld\t%g\t%g\t%g\t%g\t%g\t%g\t%d\n",
             dx_input,
             MC_ERROR,
@@ -947,7 +947,7 @@ data find_energy_new(double temp, double reduced_density, double fv, double gwid
             reduced_density,
             seed,
             run_time/60,
-            tensorw);
+            use_tensor_weight);
     fclose(f);
   }
   if (my_experiment > 0) {
@@ -1532,7 +1532,7 @@ int main(int argc, const char **argv) {
     {"mc-constant", '\0', POPT_ARG_LONG | POPT_ARGFLAG_SHOW_DEFAULT, &mc_constant, 0, "Monte-Carlo seed", "LONG"},   //temporary - delete later!
 
     /*** Tensor Weight OPTION ***/
-    {"ten", '\0', POPT_ARG_NONE | POPT_ARGFLAG_SHOW_DEFAULT, &tensorw, 0, "Include Tensor weight in SFMT", "BOOLEAN"},
+    {"tensor", '\0', POPT_ARG_NONE | POPT_ARGFLAG_SHOW_DEFAULT, &use_tensor_weight, 0, "Include Tensor weight in SFMT", "BOOLEAN"},
 
     /*** PARAMETERS DETERMINING OUTPUT FILE DIRECTORY AND NAMES ***/
     // Where to save the free energy info
@@ -1752,8 +1752,8 @@ int main(int argc, const char **argv) {
 
     //Create bestdataout filename (to be used if we are looping)
     char *bestdat_filename = new char[1024];
-    if (tensorw)  {
-      sprintf(bestdat_filename, "%s/kT%05.3f_n%05.3f_best_tara.dat", data_dir, temp, reduced_density);
+    if (use_tensor_weight)  {
+      sprintf(bestdat_filename, "%s/kT%05.3f_n%05.3f_best_tensor.dat", data_dir, temp, reduced_density);
     } else {
       sprintf(bestdat_filename, "%s/kT%05.3f_n%05.3f_best.dat", data_dir, temp, reduced_density);
     }
@@ -1764,10 +1764,10 @@ int main(int argc, const char **argv) {
     if (newmeltbest) {
       fprintf(newmeltbest, "# git version: %s\n", version_identifier());
 
-      fprintf(newmeltbest, "#kT\tn\tfv\tgwidth\thFE/atom\tbest_cFE/atom\tbest_FEdiff/atom\tbest_lat_const\tNsph\tdx\tmcerror\tmcseed\thFE/volume\tbest_cFE/volume\tmcconstant\tmcprefactor\ttarazona\n");
+      fprintf(newmeltbest, "#kT\tn\tfv\tgwidth\thFE/atom\tbest_cFE/atom\tbest_FEdiff/atom\tbest_lat_const\tNsph\tdx\tmcerror\tmcseed\thFE/volume\tbest_cFE/volume\tmcconstant\tmcprefactor\ttensor\n");
       fprintf(newmeltbest, "%g\t%g\t%g\t%g\t%g\t%g\t\t%g\t\t%g\t\t%g\t%g\t%g\t%g\t%g\t%g\t%li\t%li\t%d\n", temp, reduced_density, best_fv, best_gwidth,
               best_cfree_energy-best_energy_diff, best_cfree_energy, best_energy_diff,
-              best_lattice_constant, 1-fv, dx, MC_ERROR, seed, hfree_energy_pervol, cfree_energy_pervol, mc_constant, mc_prefactor, tensorw);    //Nsph=1-fv for parallepiped
+              best_lattice_constant, 1-fv, dx, MC_ERROR, seed, hfree_energy_pervol, cfree_energy_pervol, mc_constant, mc_prefactor, use_tensor_weight);    //Nsph=1-fv for parallepiped
       fclose(newmeltbest);
     } else {
       printf("Unable to open file %s!\n", bestdat_filename);
@@ -1803,8 +1803,8 @@ int main(int argc, const char **argv) {
 
     //Create bestdataout filename (to be used if we are looping)
     char *bestdat_filename = new char[1024];
-    if (tensorw) {
-      sprintf(bestdat_filename, "%s/kT%05.3f_n%05.3f_best_tara.dat", data_dir, temp, reduced_density);
+    if (use_tensor_weight) {
+      sprintf(bestdat_filename, "%s/kT%05.3f_n%05.3f_best_tensor.dat", data_dir, temp, reduced_density);
     } else {
       sprintf(bestdat_filename, "%s/kT%05.3f_n%05.3f_best.dat", data_dir, temp, reduced_density);
     }
@@ -1814,10 +1814,10 @@ int main(int argc, const char **argv) {
     FILE *newmeltbest = fopen(bestdat_filename, "w");
     if (newmeltbest) {
       fprintf(newmeltbest, "# git version: %s\n", version_identifier());
-      fprintf(newmeltbest, "#kT\tn\tfv\tgwidth\thFE/atom\tbest_cFE/atom\tbest_FEdiff/atom\tbest_lat_const\tNsph\tdx\tmcerror\tmcseed\thFE/volume\tbest_cFE/volume\tmcconstant\tmcprefactor\ttarazona\n");
+      fprintf(newmeltbest, "#kT\tn\tfv\tgwidth\thFE/atom\tbest_cFE/atom\tbest_FEdiff/atom\tbest_lat_const\tNsph\tdx\tmcerror\tmcseed\thFE/volume\tbest_cFE/volume\tmcconstant\tmcprefactor\ttensor\n");
       fprintf(newmeltbest, "%g\t%g\t%g\t%g\t%g\t%g\t\t%g\t\t%g\t\t%g\t%g\t%g\t%g\t%g\t%g\t%li\t%li\t%d\n", temp, reduced_density, best_fv, best_gwidth,
               best_cfree_energy-best_energy_diff, best_cfree_energy, best_energy_diff,
-              best_lattice_constant, 1-fv, dx, MC_ERROR, seed, hfree_energy_pervol, cfree_energy_pervol, mc_constant, mc_prefactor, tensorw);    //Nsph=1-fv for parallepiped
+              best_lattice_constant, 1-fv, dx, MC_ERROR, seed, hfree_energy_pervol, cfree_energy_pervol, mc_constant, mc_prefactor, use_tensor_weight);    //Nsph=1-fv for parallepiped
       fclose(newmeltbest);
     } else {
       printf("Unable to open file %s!\n", bestdat_filename);
