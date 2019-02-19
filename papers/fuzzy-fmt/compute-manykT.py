08ff53054d381e40708dd8f00ee6674d002fe439
Kirstie <kirstie@physics>
2019-02-18 18:24:20 -0800

    new program runs fig/new-melting.cpp for many temperatures at a given density

---
+++ papers/fuzzy-fmt/compute-manykT.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python2
+
+#This program runs figs/new-melting.cpp for many different temperatures for a given denisty
+#ie. figs/new-melting.mkdat --kT .5 --n 1 --gwstart .01 --gwend .2 --gwstep .01 --fv 0 --dx 0.5 --mcerror 0.001 --mcconstant 5 --mcprefactor 50000
+
+#NOTE: Run this plot script from directory deft/papers/fuzzy-fmt 
+#with comand ./compute-manykT.py --n [density] --tensor(optional)
+#For list of the many other options enter ./compute-manykT.py --help
+
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Generates data for plots.')
+
+parser.add_argument('--n', metavar='density', type=float,
+                    help='reduced density - REQUIRED')
+parser.add_argument('--minkT', type=float,
+                    help='min temperature', default=0.01)
+parser.add_argument('--maxkT', type=float,
+                    help='max temperature', default=2)
+parser.add_argument('--dkT', type=float,
+                    help='change of temperature', default=0.05)
+
+parser.add_argument('--dgw', type=float,
+                    help='change in gw', default=0.01)
+parser.add_argument('--maxgw', type=float,
+                    help='max gw', default=0.2)
+parser.add_argument('--mingw', type=float,
+                    help='min gw', default=0.01)
+
+parser.add_argument('--fv', metavar='vacancies', type=float,
+                    help='fraction of vacancies - Default 0')
+parser.add_argument('--gw', metavar='width', type=float,
+                    help='width of Gaussian - Default 0.01')
+parser.add_argument('--dx', metavar='dx', type=float,
+                    help='scaling dx - Default 0.5')
+parser.add_argument('--mcerror', metavar='mc_error', type=float,
+                    help='monte carlo mc_error - Default 0.001')
+parser.add_argument('--mcconstant', metavar='const', type=int,
+                    help='monte carlo integration mc_constant - Default 5')
+parser.add_argument('--mcprefactor', metavar='prefac', type=int,
+                    help='monte carlo integration mc_prefactor - Default 50000')
+                    
+parser.add_argument('--tensor', action='store_true',
+                    help='use tensor weight')
+
+args=parser.parse_args()
+
+n=args.n
+
+if args.dkT:
+    dkT=args.dkT
+else :
+    dkT=0.05
+    
+if args.maxkT:
+    maxkT=args.maxkT
+else :
+    maxkT=2   #erf model deviates too much for temperatures higher than this (I think)
+    
+if args.minkT:
+    minkT=args.minkT
+else :
+    minkT=.05  
+    
+if args.mingw:
+    mingw=args.mingw
+else :
+    mingw=0.01    
+
+if args.dgw:
+    dgw=args.dgw
+else :
+    dgw=0.01
+
+if args.maxgw:
+    maxgw=args.maxgw
+else :
+    maxgw=0.2  
+    
+if args.mingw:
+    mingw=args.mingw
+else :
+    mingw=0.01
+
+if args.fv:
+    fv=args.fv
+else :
+    fv=0
+
+if args.dx:
+    dx=args.dx
+else :
+    dx=.5
+
+if args.mcerror:
+    mcerror=args.mcerror
+else :
+    mcerror=0.001
+
+if args.mcconstant:
+    mcconstant=args.mcconstant
+else :
+    mcconstant=5
+    
+if args.mcprefactor:
+    mcprefactor=args.mcprefactor
+else :
+    mcprefactor=50000
+    
+if args.tensor:    
+    for kT in np.arange(args.minkT, args.maxkT, dkT):
+        cmd = 'rq run -J manykT-kT%g-n%g-tensor' % (kT, n)
+        cmd += ' figs/new-melting.mkdat --kT %g --n %g' % (kT, n)
+        cmd += ' --gwstart %g --gwend %g --gwstep %g' % (mingw, maxgw, dgw)
+        cmd += ' --fv %g --dx %g' % (fv, dx)
+        cmd += ' --mc-error %g --mc-constant %g --mc-prefactor %g' % (mcerror, mcconstant, mcprefactor)
+        #cmd += ' --filename isotherm-kT-%g_tensor.dat' % kT
+        cmd += ' --tensor'
+        print(cmd)
+        os.system(cmd)
+else :
+    for kT in np.arange(args.minkT, args.maxkT, dkT):
+        cmd = 'rq run -J manykT-kT%g-n%g' % (kT, n)
+        cmd += ' figs/new-melting.mkdat --kT %g --n %g' % (kT, n)
+        cmd += ' --gwstart %g --gwend %g --gwstep %g' % (mingw, maxgw, dgw)
+        cmd += ' --fv %g --dx %g' % (fv, dx)
+        cmd += ' --mc-error %g --mc-constant %g --mc-prefactor %g' % (mcerror, mcconstant, mcprefactor)
+        #cmd += ' --filename isotherm-kT-%g.dat' % kT
+        print(cmd)
+        os.system(cmd)
