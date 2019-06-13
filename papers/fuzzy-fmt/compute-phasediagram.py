1d58b266db880c21068c650a85a3dcfc9c1fdd2e
Kirstie Finster <kirstie@knightley.physics.oregonstate.edu>
2019-06-13 15:28:02 -0700

    set tensor max gw value to 0.2 (which is also the value for non-tensor)

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -54,7 +54,7 @@ for kT in kTs:
         run_new_melting(kT, n, 0.001, 0.001, 0.001, avoid_rq=True)    #temp, density, gw_start, gw_end, gw_step
     for n in np.arange(0.69, 1.2, 0.02):    #crystal
         if args.tensor:
-            run_new_melting(kT, n, 0.01, 0.1, 0.01)    #temp, density, gw_start, gw_end, gw_step
+            run_new_melting(kT, n, 0.01, 0.2, 0.01)    #temp, density, gw_start, gw_end, gw_step
         else:
             run_new_melting(kT, n, 0.01, 0.2, 0.01)
     #for n in np.arange(0.01, 1.11, 0.01):
