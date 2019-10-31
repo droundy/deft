5bd9b32c000f906638b49b5c2a0bc8d4bea8370d
David Roundy <roundyd@physics.oregonstate.edu>
2019-10-31 12:11:47 -0700

    compare a few ways to compute B_2

--- papers/fuzzy-fmt/figs/xi-from-alpha-b2.py
+++ papers/fuzzy-fmt/figs/xi-from-alpha-b2.py
@@ -3,9 +3,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import erf, iv
+from scipy.integrate import quad
 
 def alpha(T):
     return (2/(1+np.sqrt(T*np.log(2))))**(1./6)
+
+def B2_erf_numerical(Xi, T) :
+    alpha=np.cbrt(np.sqrt((2/(1+np.sqrt(T*np.log(2))))))
+    r = np.linspace(0, 10*alpha, 10000)
+    f=(1.0/2)*(erf((r-alpha)/Xi)-1)
+    return -0.5*(4*np.pi*r**2*r[1]*f).sum()
+B2_erf_numerical = np.vectorize(B2_erf_numerical)
+
+def B2_erf_integrand(r, KbT, pXi) :
+    alpha=np.cbrt(np.sqrt((2/(1+np.sqrt(KbT*np.log(2))))))
+    erf_mayer_function=(1.0/2)*(erf((r-alpha)/pXi)-1)
+    return (-1.0/2)*(4*np.pi)*(erf_mayer_function)*r*r
+def B2_erf_with_quad(Xi, T):
+    val = quad(B2_erf_integrand, 0, np.inf, args=(T, Xi))
+    # print('quad says', val)
+    return val[0]
 
 def b2(xi, T):
     a = alpha(T)
@@ -32,17 +49,23 @@ def b2wca(T):
     f[0] = 1
     return -0.5*(4*np.pi*r**2*r[1]*f).sum()
 
-xi = np.linspace(0, 1, 1000)
+xi = np.linspace(0.0001, 1, 1000)
 
 for T in [0.1, 0.5,1, 200]:
     plt.figure()
-    plt.plot(xi, b2(xi, T), label=r'$\Xi$')
-    plt.axvline(alpha(T))
-    plt.axhline(b2wca(T), color='g')
-    plt.axhline(b2wca_bessel(T), color='r')
+    plt.plot(xi, b2(xi, T), label=r'$B_2(\Xi)$')
+    plt.plot(xi, B2_erf_numerical(xi, T), 'b--', label=r'$B_2(\Xi)$ numerical')
+    with_quad = np.zeros_like(xi)
+    for j in range(len(xi)):
+        with_quad[j] = B2_erf_with_quad(xi[j], T)
+    plt.plot(xi, with_quad, 'c:', label=r'$B_2(\Xi)$ quad')
+    plt.axvline(alpha(T), color='xkcd:purple', linestyle=':', label=r'$\alpha$')
+    plt.axhline(b2wca(T), color='g', label=r'$B_2(WCA)$')
+    # plt.axhline(b2wca_bessel(T), color='r')
     plt.xlabel(r'$\Xi$')
     plt.ylabel(r'$B_2$')
-    plt.title(T)
+    plt.title('T={}'.format(T))
+    plt.legend(loc='best')
 
 
     # r = np.linspace(0.01, rmax, 1000)
@@ -50,13 +73,13 @@ for T in [0.1, 0.5,1, 200]:
     # plt.plot(r, f_wca(r, T))
     #plt.plot(r, Vwca(r))
 
-T = np.arange(0.5, 2.0, 0.1)
-plt.figure()
-b2 = np.zeros_like(T)
-b2b = np.zeros_like(T)
-for i in range(len(T)):
-    b2[i] = b2wca(T[i])
-    b2b[i] = b2wca_bessel(T[i])
-plt.plot(T, b2)
-plt.plot(T, b2b)
+# T = np.arange(0.5, 2.0, 0.1)
+# plt.figure()
+# b2 = np.zeros_like(T)
+# b2b = np.zeros_like(T)
+# for i in range(len(T)):
+#     b2[i] = b2wca(T[i])
+#     b2b[i] = b2wca_bessel(T[i])
+# plt.plot(T, b2)
+# plt.plot(T, b2b)
 plt.show()
