aa4774c16210a49eface8bff034bb9698276d288
Kirstie Finster <kirstie@knightley.physics.oregonstate.edu>
2019-11-07 14:30:15 -0800

    cleanups

--- papers/fuzzy-fmt/figs/xi-fromB2.cpp
+++ papers/fuzzy-fmt/figs/xi-fromB2.cpp
@@ -9,14 +9,8 @@
 double sigma=1;
 double epsilon=1;
 
-double rmax_wca = pow(2.0,1.0/6);   //rmax_wca=1.122462048
-
 double alpha(double T) {
     return pow(sqrt((2.0/(1+sqrt(T*log(2))))),1.0/3);
-}
-
-double rmax_erf(double T) { 
-    return 10*alpha(T);
 }
 
 double Vwca(double r) {
@@ -27,10 +21,11 @@ double f_wca(double r, double T){   //WCA mayer function
     return exp(-Vwca(r)/T) - 1;
 }
 
-double B2_wca(double T) {   
+double B2_wca(double T) {
     long i=0;
     long num_points =10000;
     double r;
+    double rmax_wca = pow(2.0,1.0/6);   //rmax_wca=1.122462048
     double dr=rmax_wca/num_points;
     double f_sum=0;
     for (; i<num_points; i++) {
@@ -45,25 +40,8 @@ double B2_erf(double Xi, double T) {
     return B2_erf;
 }
 
-//double f_erf(double r, double xi, double T){   //erf mayer function
-    //return (0.5)*(erf((r-alpha(T))/(xi/sqrt(2)))-1);
-//}
 
-//double B2_erf(double xi, double T) {   
-    //long i=0;
-    //long num_points =10000;
-    //double r;
-    //double dr=rmax_erf(T)/num_points;
-    //double f_sum=0;
-    //for (; i<num_points; i++) {
-      //r=dr*i+ 0.0000000000001;
-      //f_sum = f_sum + (-0.5)*(4*M_PI*r*r*dr*f_erf(r, xi, T)); 
-    //}
-    //return f_sum;
-//}
-
-
-double find_Xi(double T) {      
+double find_Xi(double T) {
     double B2wca = B2_wca(T);
     double xi_lo = 0;
     double xi_hi = 1;
@@ -78,9 +56,6 @@ double find_Xi(double T) {
       } 
     } while (xi_hi - xi_lo > 0.000000001); 
     printf("B2_erf mid=%g\n",B2_erf(xi_mid, T));  
-    printf("B2_erf hi =%g, xi_hi=%g\n",B2_erf(xi_hi, T), xi_hi);
-    printf("B2_erf lo =%g, xi_lo=%g\n",B2_erf(xi_hi, T), xi_lo);
-    printf("B2_erf=%g, xi=0.0457\n",B2_erf(0.0457, T));
     return xi_mid;  
 }
 
@@ -88,10 +63,8 @@ int main() {
     long i=1;
     double T;
     double Xi_at_T=0;
-    printf("rmax_wca=%g\n",rmax_wca); 
     for (; i<12; i++) {
       T=0.1*i; 
-      //printf("rmax_erf=%g\n",rmax_erf(T)); 
       printf("T=%g\n", T);   
       Xi_at_T = find_Xi(T);
       printf("Xi=%g\n\n", Xi_at_T);
@@ -99,3 +72,29 @@ int main() {
 
     return 0;
 }
+
+
+//B2_erf integrated----------------
+
+//double f_erf(double r, double xi, double T){   //erf mayer function
+    //return (0.5)*(erf((r-alpha(T))/(xi/sqrt(2)))-1);
+//}
+
+//double B2_erf(double xi, double T) {    //This also appears to be working! matched David's program at T=0.1
+    //long i=0;
+    //long num_points =10000;
+    //double r;
+    //double rmax_erf=10*alpha(T);
+    //double dr=rmax_erf(T)/num_points;
+    //double f_sum=0;
+    //for (; i<num_points; i++) {
+      //r=dr*i+ 0.0000000000001;
+      //f_sum = f_sum + (-0.5)*(4*M_PI*r*r*dr*f_erf(r, xi, T)); 
+    //}
+    //return f_sum;
+//}
+
+
+
+
+
