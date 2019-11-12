d83b9933bff67aacdf416f41b22052a04b4979a6
David Roundy <roundyd@physics.oregonstate.edu>
2019-11-12 12:45:51 -0800

    try to figure out what Poulaine et al. actually did, requires a script

---
+++ papers/histogram/figs/count-wla-moves.py
@@ -0,0 +1,20 @@
+# This script computes the number of iterations in the Poulaine paper
+
+import numpy as np
+
+lnf = 1
+
+M = 50
+N = 100
+
+iters = 0
+
+for main in range(0,M):
+    for sub in range(0,N):
+        lnf *= 0.9
+        iters += 1.0/np.sqrt(lnf)
+        print('   substage: ', lnf, 'gaining', 1.0/np.sqrt(lnf), '-> total', iters)
+    lnf /= 0.9**(N-1)
+    print('returning to', lnf)
+
+print('finally', '%e' % iters)
