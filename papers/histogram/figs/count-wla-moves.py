ab42361840e9d5fabbb9c04b51e1eca57b32f156
David Roundy <roundyd@physics.oregonstate.edu>
2019-11-12 12:48:33 -0800

    clarify Poulaine paragraph

--- papers/histogram/figs/count-wla-moves.py
+++ papers/histogram/figs/count-wla-moves.py
@@ -12,7 +12,7 @@ iters = 0
 for main in range(0,M):
     for sub in range(0,N):
         lnf *= 0.9
-        iters += 1.0/np.sqrt(lnf)
+        iters += 1e4/np.sqrt(lnf)
         print('   substage: ', lnf, 'gaining', 1.0/np.sqrt(lnf), '-> total', iters)
     lnf /= 0.9**(N-1)
     print('returning to', lnf)
