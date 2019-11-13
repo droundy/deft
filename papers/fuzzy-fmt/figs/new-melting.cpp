e314102595c8dd28ca6d97e569edd71c15a911a3
David Roundy <roundyd@physics.oregonstate.edu>
2019-11-13 11:30:50 -0800

    fix bug in new-melting.cpp

--- papers/fuzzy-fmt/figs/new-melting.cpp
+++ papers/fuzzy-fmt/figs/new-melting.cpp
@@ -119,8 +119,8 @@ double B2_wca(double T) {
 }
 
 double B2_erf(double Xi, double T) {
-    double B2_erf=(M_PI/3)*((pow(alpha(T), 3) + 1.5*alpha(T)*pow(Xi,2))*(1+erf(alpha(T)/Xi)) + 1/pow(M_PI,0.5)*(pow(alpha(T),2)*Xi + pow(Xi,3))*exp(-pow((alpha(T)/Xi),2)));
-    return B2_erf;
+  double alpha = find_alpha(T);
+  return (M_PI/3)*((pow(alpha, 3) + 1.5*alpha*pow(Xi,2))*(1+erf(alpha/Xi)) + 1/pow(M_PI,0.5)*(pow(alpha,2)*Xi + pow(Xi,3))*exp(-pow((alpha/Xi),2)));
 }
 
 
