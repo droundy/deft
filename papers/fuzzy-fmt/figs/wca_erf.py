67227deaf943374b103a20ac28e8b5b4679bd2b2
Kirstie Finster <kirstie@knightley.physics.oregonstate.edu>
2019-11-25 11:53:36 -0800

    fixed error in calling find_xi.Xi

--- papers/fuzzy-fmt/figs/wca_erf.py
+++ papers/fuzzy-fmt/figs/wca_erf.py
@@ -1,12 +1,12 @@
 from __future__ import division
 import numpy as np
 from scipy.special import erfinv
-import find_xi.py
+import find_xi
 
 def parameters(kT, sigma = 1, eps = 1):
     alpha = ( 2.0/(1.0 + np.sqrt(np.log(2)*kT/eps)) )**(1.0/6)
     #Xi = alpha/(6*np.sqrt(np.pi))/(np.log(2) + np.sqrt(np.log(2)*eps/kT))
-    Xi = find_xi(alpha, kT)
+    Xi = find_xi.Xi(alpha, kT)
 
     diameter = 2**(1.0/6)*sigma
     return alpha, Xi, diameter
