e6216286d8809508b966abccfd37d5f15b88fc41
Kirstie Finster <kirstie@knightley.physics.oregonstate.edu>
2019-11-27 08:30:02 -0800

    Added a comment.

--- papers/fuzzy-fmt/figs/find_xi.py
+++ papers/fuzzy-fmt/figs/find_xi.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function
 #This program finds xi for a given T by setting the second virial coefficient
 #computed with V_wca equal to the second virial coefficient computed with V_erf.
 #It is referenced by wca_erf.py which is referenced by potential-plot.py
+#and by w2-comparison.py.
 
 from scipy import special
 from scipy.special import erf
