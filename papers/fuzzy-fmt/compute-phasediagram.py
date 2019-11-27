fb3386a4ffdd1f26b7246ef14d14c24089ceb213
Kirstie Finster <kirstie@knightley.physics.oregonstate.edu>
2019-11-27 08:52:49 -0800

    Edited for higher kT

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -46,17 +46,18 @@ def run_new_melting(kT, n, gwstart, gwend, gwstep, fv=0, dx=0.5, seed=1,
     print(cmd)
     assert(os.system(cmd) == 0)
 
-kTs = np.arange(2, 0.3, -0.1)   # kT less than 0.4 don't have solutions
+#kTs = np.arange(2, 0.3, -0.1)   # kT less than 0.4 don't have solutions
+kTs = np.arange(20, 2, -2) 
 #kTs = np.arange(1.15, 0.7, -0.05)
 #kTs=np.append(kTs, 0.01)
 for kT in kTs:
-    # for n in np.arange(0.01, 0.59, 0.02):   #homogeneous fluid
-        # run_new_melting(kT, n, 0.001, 0.001, 0.001, avoid_rq=True)    #temp, density, gw_start, gw_end, gw_step
-    # for n in np.arange(0.59, 1.2, 0.02):    #crystal
-        # if args.tensor:
-            # run_new_melting(kT, n, 0.01, 0.2, 0.01)    #temp, density, gw_start, gw_end, gw_step
-        # else:
-            # run_new_melting(kT, n, 0.01, 0.2, 0.01)
+    for n in np.arange(0.01, 0.59, 0.02):   #homogeneous fluid
+        run_new_melting(kT, n, 0.001, 0.001, 0.001, avoid_rq=True)    #temp, density, gw_start, gw_end, gw_step
+    for n in np.arange(0.59, 1.2, 0.02):    #crystal
+        if args.tensor:
+            run_new_melting(kT, n, 0.01, 0.2, 0.01)    #temp, density, gw_start, gw_end, gw_step
+        else:
+            run_new_melting(kT, n, 0.01, 0.2, 0.01)
     # for n in np.arange(0.01, 0.59, 0.02):
         # run_new_melting(kT, n, 0.01, 0.2, 0.01)
     for n in np.arange(1.22, 1.42, 0.02):
