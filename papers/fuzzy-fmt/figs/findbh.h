64a7ef464ced0cbd66d73c7688d2daa49a0f34c8
David Roundy <roundyd@physics.oregonstate.edu>
2020-03-12 09:54:18 -0700

    clean up code in find*.h in hope of finding a bug

--- papers/fuzzy-fmt/figs/findbh.h
+++ papers/fuzzy-fmt/figs/findbh.h
@@ -4,18 +4,20 @@ static const double Rmax = 2*sigma/pow(2.0,5.0/6.0); // this is the distance for
 
 static inline double Vwca(double r) {
   const double power_6 = uipow(sigma/r, 6);
-  // if (r > Rmax) return 0;
+  if (r > Rmax) return 0;
   return 4*epsilon*(sqr(power_6) - power_6) + epsilon;
 }
 
 static inline double R_BH(const double kT) {
-  const int N = 100; // 1000000;
+  const int N = 10000; // 1000000;
   double bh_diameter = 0;
   const double dr = Rmax/N;
   const double beta = 1.0/kT;
   for (double r_cur=dr/2; r_cur < Rmax; r_cur += dr) {
     bh_diameter += (1 - exp(-beta*Vwca(r_cur)))*dr;
   }
+  printf("barker_henderson diameter at T=%g is %g\n", kT, bh_diameter);
+  printf("barker_henderson B2 = %g\n", 2*M_PI/3*uipow(bh_diameter,3));
   return bh_diameter/2;
 }
 
--- papers/fuzzy-fmt/figs/findxi.h
+++ papers/fuzzy-fmt/figs/findxi.h
@@ -1,8 +1,7 @@
 #pragma once
 
 static inline double find_alpha(double temp) {
-  const double sigma=1;  //sigma must be 1 - changing it would invalidate other equations in the program!
-  const double epsilon=1;
+  assert(sigma == 1.0); //sigma must be 1 - changing it would invalidate other equations in the program?
   return sigma*pow(2/(1+sqrt((temp*log(2))/epsilon)),1.0/6);
 }
 
@@ -17,7 +16,7 @@ static inline double find_alpha(double temp) {
 
 //Find Xi(T)---------------------------------------------------
 static inline double Vwca(double r) {
-    return 4.0*(pow(r,-12.0) - pow(r,-6.0)) +1;
+    return 4.0*epsilon*(pow(r/sigma,-12.0) - pow(r/sigma,-6.0)) + epsilon;
 }
 
 static inline double f_wca(double r, double T){   //WCA mayer function
@@ -25,22 +24,20 @@ static inline double f_wca(double r, double T){   //WCA mayer function
 }
 
 static inline double B2_wca(double T) {
-    long i=0;
     long num_points =10000;
-    double r;
-    double rmax_wca = pow(2.0,1.0/6);   //rmax_wca=1.122462048
-    double dr=rmax_wca/num_points;
+    double Rmax = sigma*pow(2.0,1.0/6);   //rmax_wca=1.122462048
+    double dr = Rmax/num_points;
     double f_sum=0;
-    for (; i<num_points; i++) {
-      r=dr*(i+0.5);
-      f_sum = f_sum + (-0.5)*(4*M_PI*r*r*dr*f_wca(r, T)); 
+    for (double r=dr/2; r < Rmax; r += dr) {
+      f_sum = f_sum + (-0.5)*(4*M_PI*r*r*dr*f_wca(r, T));
     }
     return f_sum;
 }
 
 static inline double B2_erf(double Xi, double T) {
   double alpha = find_alpha(T);
-  return (M_PI/3)*((pow(alpha, 3) + 1.5*alpha*pow(Xi,2))*(1+erf(alpha/Xi)) + 1/pow(M_PI,0.5)*(pow(alpha,2)*Xi + pow(Xi,3))*exp(-pow((alpha/Xi),2)));
+  return (M_PI/3)*((uipow(alpha, 3) + 1.5*alpha*uipow(Xi,2))*(1+erf(alpha/Xi))
+                   + 1/sqrt(M_PI)*(uipow(alpha,2)*Xi + uipow(Xi,3))*exp(-uipow((alpha/Xi),2)));
 }
 
 
@@ -52,7 +49,7 @@ static inline double find_Xi(double T) {
   }
   double B2wca = B2_wca(T);
   double xi_lo = 0;
-  double xi_hi = 1;
+  double xi_hi = sigma;
   double xi_mid;
   // printf("B2wca=%g\n", B2wca);
   do {
@@ -66,6 +63,10 @@ static inline double find_Xi(double T) {
   // printf("B2_erf mid=%g\n",B2_erf(xi_mid, T));
   last_T = T;
   last_Xi = xi_mid;
+  printf("our alpha at T=%g is %g\n", T, find_alpha(T));
+  printf("our Xi at T=%g is %g\n", T, xi_mid);
+  printf("our B2 = %g\n", B2_erf(xi_mid, T));
+  printf("correct B2 = %g\n", B2_wca(T));
   return xi_mid;
 }
 //END Find Xi(T)---------------------------------------------------
@@ -75,8 +76,8 @@ static inline double find_Xi(double T) {
 static inline HomogeneousSFMTFluid sfmt_homogeneous(double n, double T) {
   HomogeneousSFMTFluid hf;
   hf.Xi() = find_Xi(T);
-  hf.sigma() = 1;
-  hf.epsilon() = 1;   //energy constant in the WCA fluid
+  hf.sigma() = sigma;
+  hf.epsilon() = epsilon;   //energy constant in the WCA fluid
   hf.kT() = T;
   hf.n() = n;
   hf.mu() = 0;
@@ -88,8 +89,8 @@ static inline HomogeneousSFMTFluid sfmt_homogeneous(double n, double T) {
 static inline SFMTFluidVeff sfmt_inhomogeneous(double T, double Lx, double Ly, double Lz, double dx) {
   SFMTFluidVeff hf(Lx, Ly, Lz, dx);
   hf.Xi() = find_Xi(T);
-  hf.sigma() = 1;
-  hf.epsilon() = 1;   //energy constant in the WCA fluid
+  hf.sigma() = sigma;
+  hf.epsilon() = epsilon;   //energy constant in the WCA fluid
   hf.kT() = T;
   hf.mu() = 0;
   return hf;
