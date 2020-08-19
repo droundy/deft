3495bcb3fea4cce57424fde4657bc636c0aa715b
kirstie <kirstie@praises.bluesky.net>
2020-08-19 13:51:19 -0700

    findxi.h: fixed error in function Vwca by adding the line 'if (r > Rmax) return 0;', also made other parts of the program more similiar to findbh.h, and cleaned up comments.

--- papers/fuzzy-fmt/figs/findxi.h
+++ papers/fuzzy-fmt/figs/findxi.h
@@ -1,44 +1,31 @@
 #pragma once
 
-//sigma must be 1 - changing it would invalidate other equations in the program?
 const double epsilon = 1.0;
-const double sigma = 1.0;
+const double sigma = 1.0;  //sigma must be 1, changing it would invalidate other equations
 
-static inline double find_alpha(double temp) {
-  return sigma*pow(2/(1+sqrt((temp*log(2))/epsilon)),1.0/6);
+static inline double find_alpha(double T) {                    //note: T means kT
+  return sigma*pow(2/(1+sqrt((T*log(2))/epsilon)),1.0/6);
 }
 
+static const double Rmax = 2*sigma/pow(2.0,5.0/6.0); // this is the distance for which the potential is zero beyond it.
 
-//Old Xi derived from derivatives (referenced here if want to revert to old Xi for comparison)--------
- //static inline double find_Xi(double kT) {
-   //double xi = find_alpha(kT)/(6*uipow(M_PI,0.5))/(log(2) + uipow(log(2)*epsilon/kT,0.5));  //check syntex!
-   //printf("our alpha at T=%g is %g\n", kT, find_alpha(kT));
-   //printf("our Xi at T=%g is %g\n", kT, xi);
-   //return xi;
- //}
-//END old Xi(T)---------------------------------------------------------------------------------------
-
-
-
-static inline double Vwca(double r) {
-    return 4.0*epsilon*(pow(r/sigma,-12.0) - pow(r/sigma,-6.0)) + epsilon;
+static inline double Vwca(double r) { 
+  const double power_6 = uipow(sigma/r, 6);
+  if (r > Rmax) return 0;
+  return 4*epsilon*(sqr(power_6) - power_6) + epsilon;
 }
 
-static inline double f_wca(double r, double T){   //WCA mayer function
-    return exp(-Vwca(r)/T) - 1;
-}
+//Find Xi(T) by matching second virial coefficeints B2 for WCA and Erf potentials: 
 
-//Find Xi(T)---------------------------------------------------
-
-static inline double B2_wca(double T) {
-    long num_points =10000;
-    double Rmax = sigma*pow(2.0,1.0/6);   //rmax_wca=1.122462048
-    double dr = Rmax/num_points;
-    double f_sum=0;
-    for (double r=dr/2; r < Rmax; r += dr) {
-      f_sum = f_sum + (-0.5)*(4*M_PI*r*r*dr*f_wca(r, T));
-    }
-    return f_sum;
+static inline double B2_wca(double T) {   
+  const int N = 10000; // 1000000;
+  double f_sum=0;
+  const double dr = Rmax/N;
+  const double beta = 1.0/T;
+  for (double r_cur=dr/2; r_cur < Rmax; r_cur += dr) {
+  f_sum += -4*M_PI*r_cur*r_cur*dr*(exp(-beta*Vwca(r_cur)) - 1);
+  }
+  return f_sum/2;
 }
 
 static inline double B2_erf(double Xi, double T) {
@@ -46,7 +33,6 @@ static inline double B2_erf(double Xi, double T) {
   return (M_PI/3)*((uipow(alpha, 3) + 1.5*alpha*uipow(Xi,2))*(1+erf(alpha/Xi))
                    + 1/sqrt(M_PI)*(uipow(alpha,2)*Xi + uipow(Xi,3))*exp(-uipow((alpha/Xi),2)));
 }
-
 
 static inline double find_Xi(double T) {
   static double last_T = 0.0;
@@ -58,7 +44,6 @@ static inline double find_Xi(double T) {
   double xi_lo = 0;
   double xi_hi = sigma;
   double xi_mid;
-  //putStrLn("B2wca=%g\n", B2wca);   //DEBUG
   do {
     xi_mid = 0.5*(xi_hi + xi_lo);
     if (B2_erf(xi_mid, T) > B2wca) {
@@ -67,7 +52,6 @@ static inline double find_Xi(double T) {
       xi_lo = xi_mid;
     }
   } while (xi_hi - xi_lo > 0.000000001);
-  //putStrLn("B2_erf mid=%g\n",B2_erf(xi_mid, T));   //DEBUG
   last_T = T;
   last_Xi = xi_mid;
   printf("our alpha at T=%g is %g\n", T, find_alpha(T));
@@ -76,9 +60,14 @@ static inline double find_Xi(double T) {
   printf("correct B2 = %g\n", B2_wca(T));
   return xi_mid;
 }
-//END Find Xi(T)---------------------------------------------------
 
-
+//Old Xi derived from derivatives (referenced here if want to revert to old Xi for comparison)
+ //static inline double find_Xi(double T) {
+   //double xi = find_alpha(T)/(6*uipow(M_PI,0.5))/(log(2) + uipow(log(2)*epsilon/T,0.5));
+   //printf("our alpha at T=%g is %g\n", T, find_alpha(T));
+   //printf("our Xi at T=%g is %g\n", T, xi);
+   //return xi;
+ //}
 
 static inline HomogeneousSFMTFluid sfmt_homogeneous(double n, double T) {
   HomogeneousSFMTFluid hf;
@@ -90,8 +79,6 @@ static inline HomogeneousSFMTFluid sfmt_homogeneous(double n, double T) {
   hf.mu() = 0;
   return hf;
 }
-
-
 
 static inline SFMTFluidVeff sfmt_inhomogeneous(double T, double Lx, double Ly, double Lz, double dx) {
   SFMTFluidVeff hf(Lx, Ly, Lz, dx);
