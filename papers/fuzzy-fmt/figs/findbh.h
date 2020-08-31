82fd255e72ade5e50efc347ecf08894023b813b6
Kirstie Finster <finsterk@oregonstate.edu>
2020-08-31 13:52:09 -0700

    figs/findbh.h: making findbh.h similar to findxi.h

--- papers/fuzzy-fmt/figs/findbh.h
+++ papers/fuzzy-fmt/figs/findbh.h
@@ -1,6 +1,10 @@
 #pragma once
 
+const double epsilon = 1.0;
+const double sigma = 1.0;  //sigma must be 1, changing it would invalidate other equations
+
 static const double Rmax = 2*sigma/pow(2.0,5.0/6.0); // this is the distance for which the potential is zero beyond it.
+const double radius = Rmax/2.0;
 
 static inline double Vwca(double r) {
   const double power_6 = uipow(sigma/r, 6);
@@ -8,15 +12,15 @@ static inline double Vwca(double r) {
   return 4*epsilon*(sqr(power_6) - power_6) + epsilon;
 }
 
-static inline double R_BH(const double kT) {
-  const int N = 10000; // 1000000;
+static inline double R_BH(const double T) {          //note: T means kT
+  const int N = 10000; // 1000000; ask David which to use 10000 or 1000000 (original)
   double bh_diameter = 0;
   const double dr = Rmax/N;
-  const double beta = 1.0/kT;
+  const double beta = 1.0/T;
   for (double r_cur=dr/2; r_cur < Rmax; r_cur += dr) {
     bh_diameter += (1 - exp(-beta*Vwca(r_cur)))*dr;
   }
-  printf("barker_henderson diameter at T=%g is %g\n", kT, bh_diameter);
+  printf("barker_henderson diameter at T=%g is %g\n", T, bh_diameter);
   printf("barker_henderson B2 = %g\n", 2*M_PI/3*uipow(bh_diameter,3));
   return bh_diameter/2;
 }
@@ -29,7 +33,8 @@ static inline HomogeneousWhiteBearFluid bh_homogeneous(double n, double T) {
 
   hf.R() = rad_bh;
   hf.kT() = T;
-  hf.n() = n;
+  hf.n() = n;   //ASK! should this be replaced with line below??
+  //hf.n() = reduced_density*pow(2,-5.0/2.0);   //OLD line in new-bh-radial-wca.cpp -ASK 
   hf.mu() = 0;
   return hf;
 }
