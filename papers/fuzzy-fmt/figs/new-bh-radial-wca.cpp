559ae8ab9065878b3d33ef9b129e599a271e7961
Kirstie Finster <finsterk@oregonstate.edu>
2020-08-31 13:57:03 -0700

    figs/new-bh-radial-wca.cpp: moved things around and to findbh.h to make these programs similar to new-radial-wca.cpp and findxi.h

--- papers/fuzzy-fmt/figs/new-bh-radial-wca.cpp
+++ papers/fuzzy-fmt/figs/new-bh-radial-wca.cpp
@@ -24,16 +24,13 @@
 #include "new/Minimize.h"
 #include "version-identifier.h"
 
+#include "findbh.h"
+
 // Here we set up the lattice.
 double zmax = 16;
 double ymax = zmax;
 double xmax = zmax;
 double dx = 0.05;
-const double epsilon = 1.0;
-const double radius = 1.0;
-const double R = 2*radius;
-const double sigma = R*pow(2,-1.0/6.0);
-const int N = 1000000;
 
 static void took(const char *name) {
   static clock_t last_time = 0;
@@ -42,19 +39,6 @@ static void took(const char *name) {
   printf("\t\t%s took %g seconds\n", name, (t-last_time)/double(CLOCKS_PER_SEC));
   fflush(stdout);
   last_time = t;
-}
-
-double R_BH(const double kT) {
-  printf("kT for R_BH is %g.\n", kT);
-  double bh_diameter = 0;
-  const double dr = R/N;
-  const double beta = 1.0/kT;
-  printf("Beta is %g.\n", beta);
-  for (double r_cur=dr/2; r_cur < R; r_cur += dr) {
-    bh_diameter += (1 - exp(-beta*(4*epsilon*(uipow(sigma/r_cur,12)
-                                   - uipow(sigma/r_cur,6)) + epsilon)))*dr;
-  }
-  return bh_diameter/2;
 }
 
 void run_minimization(double reduced_density,WhiteBearFluidVeff *f, double kT) {
@@ -72,17 +56,18 @@ void run_minimization(double reduced_density,WhiteBearFluidVeff *f, double kT) {
   printf("| Working on rho* = %4g and kT = %4g |\n", reduced_density, kT);
   printf("========================================\n");
   do {
-    //f->run_finite_difference_test("SFMT", 0, 100*min.recent_stepsize());
+    //f->run_finite_difference_test("BH", 0, 100*min.recent_stepsize());
 
     took("Doing the minimization step");
 
     const int Nz = f->Nz();
-    Vector Vext = f->Vext();
+    Vector Vext = f->Vext();  
     Vector r = f->get_r();
     Vector n = f->get_n();
     f->get_Fideal(); // FIXME this is a hokey trick to make dV be defined
     Vector n3 = f->get_n3();
 
+    printf("The length of the vectors is %d", n.get_size());
     FILE *o = fopen(fname, "w");
     if (!o) {
       fprintf(stderr, "error creating file %s\n", fname);
@@ -127,38 +112,25 @@ int main(int argc, char **argv) {
   sscanf(argv[1], "%lg", &reduced_density);
   sscanf(argv[2], "%lg", &temp);
 
-  HomogeneousWhiteBearFluid hf;
-  printf("dx is %g\n", dx);
-
-  double rad_bh = R_BH(temp);
-  printf("rad is %g\n", rad_bh);
-
-  hf.R() = rad_bh;
-  hf.kT() = temp;
-  hf.n() = reduced_density*pow(2,-5.0/2.0);
-  printf("dividing by sigma = %g\n", sigma);
-  printf("eta is %g\n", hf.n()*uipow(radius,3)*M_PI*4/3);
+  HomogeneousWhiteBearFluid hf = bh_homogeneous(reduced_density, temp);
+  printf("dividing by sigma = %g\n", sigma);    //OLD - ASK
+  printf("eta is %g\n", hf.n()*uipow(radius,3)*M_PI*4/3);    //OLD - ASK
   hf.mu() = 0;
   hf.mu() = hf.d_by_dn(); // set mu based on derivative of hf
   printf("bulk energy is %g\n", hf.energy());
+  //hf.printme("XXX:");
   printf("cell energy should be %g\n", hf.energy()*dx*dx*dx);
 
-  WhiteBearFluidVeff f(xmax, ymax, zmax, dx);
-  f.R() = hf.R();
-  f.kT() = hf.kT();
+  WhiteBearFluidVeff f = bh_inhomogeneous(temp, xmax, ymax, zmax, dx);
   f.mu() = hf.mu();
   f.Vext() = 0;
   f.Veff() = -temp*log(hf.n());
-
   {
     const int Ntot = f.Nx()*f.Ny()*f.Nz();
     const Vector r = f.get_r();
     for (int i=0; i<Ntot; i++) {
       const double Vmax = 100*temp;
-      f.Vext()[i] = 4*epsilon*(uipow(sigma/r[i], 12) - uipow(sigma/r[i], 6)) + epsilon;
-      if (r[i] > R) {
-        f.Vext()[i] = 0;
-      }
+      f.Vext()[i] = Vwca(r[i]);
       if (!(f.Vext()[i] < Vmax)) f.Vext()[i] = Vmax;
 
       if (f.Vext()[i] > 0) {
