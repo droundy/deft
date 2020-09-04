b2f8e14077012e8ed8fd36597ab926272baca5af
Kirstie Finster <finsterk@oregonstate.edu>
2020-09-04 09:41:47 -0700

    changed data directory to phase-diagram7

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -37,18 +37,18 @@ def run_new_melting(kT, n, gwstart, gwend, gwstep, fv=0, dx=0.5, seed=1,
     cmd += ' --fv %g --dx %g --seed %g' % (fv, dx, seed)
     cmd += ' --mc-error %g --mc-constant %g --mc-prefactor %g' % (mcerror, mcconstant, mcprefactor)
     if args.tensor:
-        cmd += ' --d newdata_tensor/phase-diagram'   #once new results are compared to the old, remove the "2" at the end of phase-diagram
+        cmd += ' --d newdata_tensor/phase-diagram7'   #once new results are compared to the old, remove the "2" at the end of phase-diagram
         cmd += ' --tensor'
     else:
         #cmd += ' --d data/phase-diagram'
-        cmd += ' --d newdata/phase-diagram' #once new results are compared to the old, remove the "2" at the end of phase-diagram
+        cmd += ' --d newdata/phase-diagram7' #once new results are compared to the old, remove the "2" at the end of phase-diagram
     cmd += ' --filename %s.dat' % name
     print(cmd)
     assert(os.system(cmd) == 0)
 
 #kTs = np.arange(2, 0.3, -0.1)   # kT less than 0.4 don't have solutions
 #kTs = np.arange(20, 2, -2) 
-kTs = np.arange(40, .1, -1) 
+kTs = np.arange(40, .1, -2) 
 #kTs = np.arange(200, 20, -20)
 #kTs = np.arange(.05, .005, -0.02)
 #kTs = np.arange(3, 0.05, -0.1)
