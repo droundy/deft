1dece69b7da9dae64a709152ca1cf9d266c24690
Kirstie Finster <finsterk@oregonstate.edu>
2020-09-03 19:54:15 -0700

    modified temp and densities to run, and edited comment

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -5,7 +5,7 @@
 #(use compute-isotherm.py to run figs/new-melting.cpp for many different densities based on user input)
 #ie. figs/new-melting.mkdat --kT 0.5 --n 1.06 --gwstart 0.01 --gwend 0.2 --gwstep 0.01 --fv 0.01 --dx 0.5 --mc-error 0.001 --mc-constant 5 --mc-prefactor 50000 --filename isotherm-kT-0.5_tensor.dat --tensor
 
-#NOTE: Run this script from directory deft/papers/fuzzy-fmt with command python3 ./compute-phasediagram.py --tensor(optional)
+#NOTE: Run this script from directory deft/papers/fuzzy-fmt with command ./compute-phasediagram.py --tensor(optional)
 #Currently, data is stored in the direction deft/papers/fuzzy-fmt/newdata_tensor/phase-diagram5
 
 
@@ -48,7 +48,7 @@ def run_new_melting(kT, n, gwstart, gwend, gwstep, fv=0, dx=0.5, seed=1,
 
 #kTs = np.arange(2, 0.3, -0.1)   # kT less than 0.4 don't have solutions
 #kTs = np.arange(20, 2, -2) 
-kTs = np.arange(.5, .1, -.5) 
+kTs = np.arange(5, .1, -5) 
 #kTs = np.arange(200, 20, -20)
 #kTs = np.arange(.05, .005, -0.02)
 #kTs = np.arange(3, 0.05, -0.1)
@@ -63,7 +63,7 @@ for kT in kTs:
             # run_new_melting(kT, n, 0.01, 0.2, 0.01)    #temp, density, gw_start, gw_end, gw_step
         # else:
             # run_new_melting(kT, n, 0.01, 0.2, 0.01)
-    for n in np.arange(.6, 1.4, 0.2):
+    for n in np.arange(0.6, 1.8, 0.02):
          #run_new_melting(kT, n, 0.001, 0.001, 0.001) #super fast for homogeneous at the high temps I'm running
          run_new_melting(kT, n, 0.01, 0.2, 0.01)
     # for n in np.arange(1.22, 1.42, 0.02):
