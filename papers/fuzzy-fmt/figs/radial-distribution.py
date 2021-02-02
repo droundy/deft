6c7e585b2ffd141ab562aebae4af792a48791ec7
David Roundy <daveroundy@gmail.com>
2021-02-02 11:44:02 -0800

    tweak radial distribution plot to use new data

--- papers/fuzzy-fmt/figs/radial-distribution.py
+++ papers/fuzzy-fmt/figs/radial-distribution.py
@@ -49,6 +49,7 @@ def plot_radial(reduced_density, temps):
     ylabel('$g(r)$')
     legend()
     xlim(0.5, 3)
+    ylim(ymin=0)
 
 # input: ['figs/mcfcc-%04.4f-%.4f.dat.gradial' % (0.6, temp) for temp in [10.0, 5.0, 2.5, 1.0, 0.1]]
 # input: ['figs/new-data/radial-wca-%06.4f-%04.2f.dat' % (temp, 0.6) for temp in [10.0, 5.0, 2.5, 1.0, 0.1]]
@@ -58,13 +59,10 @@ def plot_radial(reduced_density, temps):
 # input: ['figs/new-data/radial-bh-wca-%06.4f-%04.2f.dat' % (temp, 1.0) for temp in [10.0, 5.0, 2.5]]
 figure(figsize=(9, 14.5))
 subplot(2, 1, 1)
-#plot_radial(60, [10, 5.0, 2.5, 1.0, 0.1])
-plot_radial(60, [10, 1.0])
-#plot_radial(60, [10])
+plot_radial(60, [10, 5.0, 2.5, 1.0])
 
 subplot(2, 1, 2)
-#plot_radial(100, [10.0, 5.0, 2.5])
-plot_radial(100, [10, 2.5])
+plot_radial(100, [10.0, 5.0, 2.5])
 
 plt.tight_layout();
 savefig('figs/radial-distribution.pdf', bbox_inches=0)
