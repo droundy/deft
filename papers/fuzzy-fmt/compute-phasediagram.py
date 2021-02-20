ae9259f9bc80be4a027e4cf50e0332a2d1aed434
Kirstie Finster <finsterk@oregonstate.edu>
2021-02-20 10:43:27 -0800

    removed tensor argument

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -3,9 +3,9 @@
 #This program runs figs/new-melting.cpp for many different densities for input parameters specified within this program
 #It is used to reproduce data, which is why the input paratmeters are specified within the program, and are not intended to be changed.
 #(use compute-isotherm.py to run figs/new-melting.cpp for many different densities based on user input)
-#ie. figs/new-melting.mkdat --kT 0.5 --n 1.06 --gwstart 0.01 --gwend 0.2 --gwstep 0.01 --fv 0.01 --dx 0.5 --mc-error 0.001 --mc-constant 5 --mc-prefactor 50000 --d (directory) --filename isotherm-kT-0.5_tensor.dat --tensor
+#ie. figs/new-melting.mkdat --kT 0.5 --n 1.06 --gwstart 0.01 --gwend 0.2 --gwstep 0.01 --fv 0.01 --dx 0.5 --mc-error 0.001 --mc-constant 5 --mc-prefactor 50000 --d newdata_tensor/phase-diagram4 --filename isotherm-kT-0.5_tensor.dat
 
-#NOTE: Run this script from directory deft/papers/fuzzy-fmt with command ./compute-phasediagram.py --tensor(optional)
+#NOTE: Run this script from directory deft/papers/fuzzy-fmt with command ./compute-phasediagram.py
 #Currently, data is stored in the direction deft/papers/fuzzy-fmt/newdata_tensor/phase-diagram
 
 
@@ -17,18 +17,10 @@ import os
 import argparse
 import array as arr
 
-parser = argparse.ArgumentParser(description='Creates data for phase diagrams.')
-
-parser.add_argument('--tensor', action='store_true',
-                    help='use tensor weight')
-args=parser.parse_args()
-
 def run_new_melting(kT, n, gwstart, gwend, gwstep, fv=0, dx=0.5, seed=1,
                     mcerror=1e-3, mcconstant=5, mcprefactor=50000,
                     avoid_rq=False):
     name = 'nm-kT_%g-n_%g' % (kT, n)
-    if args.tensor:
-        name = 'tensor-'+name
     cmd = 'rq run -J %s' % name
     if avoid_rq:
         cmd = ''
@@ -36,12 +28,7 @@ def run_new_melting(kT, n, gwstart, gwend, gwstep, fv=0, dx=0.5, seed=1,
     cmd += ' --gwstart %g --gwend %g --gwstep %g' % (gwstart, gwend, gwstep)
     cmd += ' --fv %g --dx %g --seed %g' % (fv, dx, seed)
     cmd += ' --mc-error %g --mc-constant %g --mc-prefactor %g' % (mcerror, mcconstant, mcprefactor)
-    if args.tensor:
-        cmd += ' --d newdata_tensor/phase-diagram4'   #once new results are compared to the old, remove the "2" at the end of phase-diagram
-        cmd += ' --tensor'
-    else:
-        #cmd += ' --d data/phase-diagram'
-        cmd += ' --d newdata/phase-diagram4' #once new results are compared to the old, remove the "2" at the end of phase-diagram
+    cmd += ' --d newdata_tensor/phase-diagram4'   #once new results are compared to the old, remove the "2" at the end of phase-diagram
     cmd += ' --filename %s.dat' % name
     print(cmd)
     assert(os.system(cmd) == 0)
