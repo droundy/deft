5394f24d04338894f932db3bc1de7471fbb37148
Kirstie Finster <finsterk@oregonstate.edu>
2021-02-25 23:10:20 -0800

    more densities

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -37,7 +37,7 @@ def run_new_melting(kT, n, gwstart, gwend, gwstep, fv=0, dx=0.5, seed=1,
 #kTs = np.arange(200, 20, -20)
 #kTs = np.arange(.05, .005, -0.02)
 ###kTs = np.arange(3, 0.05, -0.1)  #for small plot in paper
-kTs = np.arange(0.4, 0.05, -4.1)  #for small plot in paper
+kTs = np.arange(0.5, 0.05, -4.1)  #for small plot in paper
 #kTs = np.arange(200, 100, -20)
 #kTs = np.arange(1.15, 0.7, -0.05)
 #kTs=np.append(kTs, 0.01)
@@ -56,6 +56,6 @@ for kT in kTs:
         #run_new_melting(kT, n, 0.01, 0.2, 0.01)
     #for n in np.arange(1.82, 2.8, 0.2):
         #run_new_melting(kT, n, 0.01, 0.2, 0.01)
-    for n in np.arange(0.01, 0.29, 0.02):    #crystal                                      #for small plot in paper
+    for n in np.arange(0.01, 0.59, 0.02):    #crystal                                      #for small plot in paper
         run_new_melting(kT, n, gwstart=0.01, gwend=0.2, gwstep=0.01)  
 
