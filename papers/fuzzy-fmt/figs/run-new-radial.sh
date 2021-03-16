9111ddec5b3bc5ab19c6a5e0c71379630c0534d2
Kirstie Finster <finsterk@oregonstate.edu>
2021-03-16 09:14:43 -0700

    removed one run

--- papers/fuzzy-fmt/figs/run-new-radial.sh
+++ papers/fuzzy-fmt/figs/run-new-radial.sh
@@ -32,7 +32,7 @@ rq run -J radialbh-1-2.5 --mem 88 -o new-data/radial-bh-wca-1.0000-2.50.out ./ne
 
 #Run new-radial-lj.cpp 
 rq run -J radiallj-6-10 --mem 88 -o new-data/radial-lj-0.71-0.83890.out ./new-radial-lj.mkdat 0.83890 0.71 
-rq run -J radiallj-6-5 --mem 88 -o new-data/radial-lj-2.48-0.957.out ./new-radial-lj.mkdat 0.957 2.48     
+#rq run -J radiallj-6-5 --mem 88 -o new-data/radial-lj-2.48-0.957.out ./new-radial-lj.mkdat 0.957 2.48     
 rq run -J radiallj-6-2.5 --mem 88 -o new-data/radial-lj-wca-1.235-0.5844.out ./new-radial-lj.mkdat 0.5844 1.235  
 rq run -J radiallj-6-1 --mem 88 -o new-data/radial-lj-2.48-1.095.out ./new-radial-lj.mkdat 1.095 2.48     
 
