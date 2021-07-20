695d3df852f5be4998e080485f9632ec795fd4e0
Kirstie Finster <finsterk@oregonstate.edu>
2021-07-20 14:25:53 -0700

    deleted old alpha and xi stuff

--- papers/fuzzy-fmt/figs/find_xi.py
+++ papers/fuzzy-fmt/figs/find_xi.py
@@ -8,11 +8,6 @@ from scipy.special import erf
 import numpy as np
 import math
 
-#Old Xi derived from derivatives (referenced here if want to revert to old Xi for comparison)------
-#def Xi(alpha, kT, sigma = 1, eps = 1):
-#   return alpha/(6*np.sqrt(np.pi))/(np.log(2) + np.sqrt(np.log(2)*eps/kT))
-#--------------------------------------------------------------------------------------------------
-
 #Compute B2_erf analytically (default method): 
 def B2_erf_analytical(alpha, Xi, T) :
     return np.pi/3*((alpha**3 + 1.5*alpha*Xi**2)*(1+erf(alpha/Xi)) + 1/np.sqrt(np.pi)*(alpha**2*Xi + Xi**3)*np.exp(-(alpha/Xi)**2))
@@ -74,35 +69,5 @@ def find_alpha(T, sigma = 1, eps = 1):
             alpha_lo = alpha_mid
     print('Alpha is', alpha_mid, 'B2wca is', B2wca)
     return alpha_mid
-	   
-
-# #Find Xi at a given  T (that is, kT)
-# def Xi(alpha, T, sigma = 1, eps = 1):
-    # B2wca = B2_wca_numerical(T)   #use with B2 wca numerical method  (default)
-    # xi_lo = 0
-    # xi_hi = sigma
-    # while xi_hi - xi_lo > 0.000001:
-        # xi_mid = 0.5*(xi_hi + xi_lo)
-        # if B2_erf_analytical(alpha, xi_mid, T) > B2wca:    #use with B2 erf analytical method (default)
-            # xi_hi = xi_mid
-        # else:
-            # xi_lo = xi_mid
-    # print('Xi is', xi_mid)
-    # return xi_mid
-
-# # #CHECK---------------------------------------------------------------------
-# #Find Xi at a given T   
-# T=0.2575
-# alpha=(2.0/(1+np.sqrt(T*np.log(2))))**(1.0/6)
-# B2wca = B2_wca_numerical(T)   #use with B2 wca numerical method  (default)
-# xi_lo = 0
-# xi_hi = 1
-# while xi_hi - xi_lo > 0.000001:
-    # xi_mid = 0.5*(xi_hi + xi_lo)
-    # if B2_erf_analytical(alpha, xi_mid, T) > B2wca:    #use with B2 erf analytical method (default)
-        # xi_hi = xi_mid
-    # else:
-        # xi_lo = xi_mid
-# Xi_at_T=xi_mid
-# print(Xi_at_T, T)
+    
 
