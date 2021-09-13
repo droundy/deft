ee822b59aa6ff473cee235d299295600ef7863c6
Kirstie Finster <finsterk@oregonstate.edu>
2021-09-13 11:20:00 -0700

    No changes to the program, edited comments only

--- papers/fuzzy-fmt/compute-phasediagram.py
+++ papers/fuzzy-fmt/compute-phasediagram.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
-#This program runs figs/new-melting.cpp for many different densities for input parameters specified within this program
-#It is used to reproduce data, which is why the input paratmeters are specified within the program, and are not intended to be changed.
-#(use compute-isotherm.py to run figs/new-melting.cpp for many different densities based on user input)
-#ie. figs/new-melting.mkdat --kT 0.5 --n 1.06 --gwstart 0.01 --gwend 0.2 --gwstep 0.01 --fv 0 --dx 0.5 --mc-error 0.001 --mc-constant 5 --mc-prefactor 50000 --d newdata_tensor/phase-diagram4 --filename isotherm-kT-0.5_tensor.dat
+#This program runs figs/new-melting.cpp for many different densities for 
+#input parameters specified within this program.
+#It is used to reproduce data, which is why the input paratmeters are 
+#specified within the program, and are not intended to be changed.
+#(Use compute-isotherm.py to run figs/new-melting.cpp for many different 
+#densities and temperatures that are not fixed to reproduce published data.)
+#ie. figs/new-melting.mkdat --kT 0.5 --n 1.06 --gwstart 0.01 --gwend 0.2 
+#    --gwstep 0.01 --fv 0 --dx 0.5 --mc-error 0.001 --mc-constant 5 
+#    --mc-prefactor 50000 --d newdata_tensor/phase-diagram4 
+#    --filename isotherm-kT-0.5_tensor.dat
 
-#NOTE: Run this script from directory deft/papers/fuzzy-fmt with command ./compute-phasediagram.py
+#NOTE: Run this script from directory deft/papers/fuzzy-fmt 
+#      with command ./compute-phasediagram.py
 
 import numpy as np
 import matplotlib.mlab as mlab
@@ -34,6 +41,8 @@ kTs = np.arange(3, 0.05, -0.1)  #for small plot in paper
 
 for kT in kTs:
     for n in np.arange(0.01, 1.53, 0.02):   #homogeneous fluid
-        #run_new_melting(kT, n, gwstart=0.001, gwend=0.001, gwstep=0.001, avoid_rq=True)  #super fast for homogeneous free energy (not used for paper)
-        run_new_melting(kT, n, gwstart=0.01, gwend=0.2, gwstep=0.01)                      #for small plot in paper
+		#To run super fast for homogeneous free energy (not used for paper) use:
+        #run_new_melting(kT, n, gwstart=0.001, gwend=0.001, gwstep=0.001, avoid_rq=True) 
+        #To produce small plot in paper use: 
+        run_new_melting(kT, n, gwstart=0.01, gwend=0.2, gwstep=0.01)                      
 
