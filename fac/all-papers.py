471b64ff88ca60c4d0d5abfc7f6f49dbda679e53
David Roundy <roundyd@physics.oregonstate.edu>
2021-11-02 13:49:02 -0700

    avoid building roth thesis, which was taking forever

--- fac/all-papers.py
+++ fac/all-papers.py
@@ -21,17 +21,6 @@ for paper in papers:
 
 figs_done = set()
 
-for project in ['thesis-roth']:
-  print("| python3 latex.py papers/"+project+"/project.tex")
-  print("< config.py")
-  print("> ../papers/"+project+"/.project.tex.fac")
-  print("c .pyc\n")
-
-  print("* cd ../papers/%s && python3 ../../fac/figs.py figs/*.py > .figs.fac"
-        % project)
-  print("> ../papers/"+project+"/.figs.fac")
-  print("c .pyc\n")
-
 other_texs = """
    thesis-vischer/Thesis.tex
    histogram/sad.tex
