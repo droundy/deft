7f5cae6bbd06badf838e8d1cc7622ff5028ff91f
Kirstie Finster <finsterk@oregonstate.edu>
2021-11-08 15:43:38 -0800

    just want one data point at high accuracy

--- papers/fuzzy-fmt/figs/compute-phasediagram.py
+++ papers/fuzzy-fmt/figs/compute-phasediagram.py
@@ -45,9 +45,12 @@ for kT in kTs:
         # It seems that for low temperature and high density we need to run a more
         # accurate and precise Monte Carlo, otherwise we end up seeing n_3 > 1 and get
         # NaNs.
-        if kT > 0.501 or kT < 0.499 and n > 0.94 and n < 1.15:
-            pass # for now, skip these simulations that we already ran.
-            # run_new_melting(kT, n, gwstart=0.01, gwend=0.2, gwstep=0.01)
-        else:
-            # We want high accuracy for this one!
+        #if kT > 0.501 or kT < 0.499 and (1/n) > 1.15  or (1/n) < 0.94:
+        if 0.499 < kT < 0.501 and 0.94 < (1/n) < 1.15:
+	    # We want high accuracy for this one!
             run_new_melting(kT, n, gwstart=0.01, gwend=0.07, gwstep=0.01, mcerror=1e-4)
+            #print(kT, 1/n)
+        else:
+            pass # for now, skip these simulations that we already ran.
+            #run_new_melting(kT, n, gwstart=0.01, gwend=0.2, gwstep=0.01)
+
