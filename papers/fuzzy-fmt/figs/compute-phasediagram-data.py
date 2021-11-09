1c322bcde6bd5d6153ef55570b742192c5d23ce0
Kirstie Finster <finsterk@oregonstate.edu>
2021-11-09 12:46:44 -0800

    took out unused import statements

--- papers/fuzzy-fmt/figs/compute-phasediagram-data.py
+++ papers/fuzzy-fmt/figs/compute-phasediagram-data.py
@@ -15,8 +15,6 @@
 from __future__ import print_function, division
 
 import numpy as np
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
 import os, glob
 import argparse
 import sys
