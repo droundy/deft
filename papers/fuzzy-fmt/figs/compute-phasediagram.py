92dfeb76c1222d5ba83e6488f2be4f6b3fb6a59e
Kirstie Finster <finsterk@oregonstate.edu>
2022-03-07 07:16:18 -0800

    finishing more points that didn't run earlier

--- papers/fuzzy-fmt/figs/compute-phasediagram.py
+++ papers/fuzzy-fmt/figs/compute-phasediagram.py
@@ -52,9 +52,9 @@ for kT in kTs:
         #if 0.499 < kT < 0.501 and 0.89 < (1/n) < 1.3:   #for small plot in paper
         if 0.499 < kT < 0.501 and 0.89 < (1/n) < 1:    #trouble data points
 	    # We want high accuracy for this one!
-            run_new_melting(kT, n, seed=1, gwstart=0.01, gwend=0.11, gwstep=0.01, mcerror=1e-4) #trouble data points
+            run_new_melting(kT, n, gwstart=0.01, gwend=0.11, gwstep=0.01, mcerror=1e-4) #trouble data points
         else:
             #pass # for now, skip these simulations that we already ran.
-            run_new_melting(kT, n, seed=1, gwstart=0.01, gwend=0.2, gwstep=0.01)
+            run_new_melting(kT, n, gwstart=0.01, gwend=0.2, gwstep=0.01)
             #run_new_melting(kT, n, seed, gwstart=0.01, gwend=0.2, gwstep=0.01, mcerror=1e-4)
 
