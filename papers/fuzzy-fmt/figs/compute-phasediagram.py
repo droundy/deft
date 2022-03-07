498718948ab4edfe07ee6496758d8e35c03df558
Kirstie Finster <finsterk@oregonstate.edu>
2022-03-07 07:40:16 -0800

    finishing more points that didn't run earlier

--- papers/fuzzy-fmt/figs/compute-phasediagram.py
+++ papers/fuzzy-fmt/figs/compute-phasediagram.py
@@ -36,14 +36,17 @@ def run_new_melting(kT, n, seed, gwstart, gwend, gwstep, fv=1e-4, dx=0.5,
 #kTs = np.arange(3, 0.05, -3.5)  #only do kT=3
 kTs = np.arange(0.5, 0.45, -0.1)  #only do kT=0.5
 #kTs = np.arange(3, 0.05, -2.5)  #only do kT=3 and kT=0.5 
-#densities = np.arange(0.01, 1.53, 0.02)
-densities = np.arange(1.01, 1.12, 0.02) #only do trouble points
-#densities = np.arange(1.09, 1.1, 0.02)
-#seeds = (1,2,3,4,5)
-seeds = (1,4,5)
+kTs = np.arange(0.6, 0.55, -0.1)  #only do kT=0.5
 
-for seed in seeds:
-  for kT in kTs:
+densities = np.arange(0.01, 1.53, 0.02)
+#densities = np.arange(1.01, 1.12, 0.02) #only do trouble points
+#densities = np.arange(1.09, 1.1, 0.02)
+
+#seeds = (1,2,3,4,5)
+#seeds = (1,4,5)
+
+#for seed in seeds:
+for kT in kTs:
     for n in densities:
         # It seems that for low temperature and high density we need to run a more
         # accurate and precise Monte Carlo, otherwise we end up seeing n_3 > 1 and get
@@ -52,9 +55,9 @@ for seed in seeds:
         #if 0.499 < kT < 0.501 and 0.89 < (1/n) < 1.3:   #for small plot in paper
         if 0.499 < kT < 0.501 and 0.89 < (1/n) < 1:    #trouble data points
 	    # We want high accuracy for this one!
-            run_new_melting(kT, n, seed, gwstart=0.01, gwend=0.11, gwstep=0.01, mcerror=1e-4) #trouble data points
+            run_new_melting(kT, n, seed=1, gwstart=0.01, gwend=0.11, gwstep=0.01, mcerror=1e-4) #trouble data points
         else:
             #pass # for now, skip these simulations that we already ran.
-            run_new_melting(kT, n, seed, gwstart=0.01, gwend=0.2, gwstep=0.01)
+            run_new_melting(kT, n, seed=1, gwstart=0.01, gwend=0.2, gwstep=0.01)
             #run_new_melting(kT, n, seed, gwstart=0.01, gwend=0.2, gwstep=0.01, mcerror=1e-4)
 
